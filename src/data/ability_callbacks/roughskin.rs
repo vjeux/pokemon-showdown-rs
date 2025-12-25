3b444f327384ef15b811738ba449c3ca5a07460d
Developer <dev@example.com>
2025-12-25 17:08:17 +0000

    Document roughskin - contact damage ability

--- src/data/ability_callbacks/roughskin.rs
+++ src/data/ability_callbacks/roughskin.rs
@@ -26,13 +26,16 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onDamagingHitOrder(...)
-pub fn on_damaging_hit_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_DAMAGING_HIT_ORDER: i32 = 1;
 
-/// onDamagingHit(...)
+/// onDamagingHit(damage, target, source, move)
+/// Damages attacker by 1/8 of their max HP if they make contact
+///
+/// TODO: onDamagingHit handler not yet implemented
+/// TODO: Needs checkMoveMakesContact(), source.baseMaxhp, battle.damage()
+/// When implemented, should:
+/// 1. Check if the move makes contact with target
+/// 2. If contact made, damage the source for 1/8 of their baseMaxhp
 pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
