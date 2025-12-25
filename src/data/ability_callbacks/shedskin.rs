450cb287fe5526b32131ae90e486e754dc13d740
Developer <dev@example.com>
2025-12-25 15:34:39 +0000

    Implement Shed Skin ability
    Has 33% chance each turn to cure status condition. Uses onResidual handler
    with order 5, suborder 3.
    
    Tests passing.

--- src/data/ability_callbacks/shedskin.rs
+++ src/data/ability_callbacks/shedskin.rs
@@ -29,21 +29,21 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onResidualOrder(...)
-pub fn on_residual_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+/// onResidualOrder: 5
+pub const ON_RESIDUAL_ORDER: i32 = 5;
+/// onResidualSubOrder: 3
+pub const ON_RESIDUAL_SUB_ORDER: i32 = 3;
 
-/// onResidualSubOrder(...)
-pub fn on_residual_sub_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onResidual(...)
-pub fn on_residual(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onResidual(pokemon)
+pub fn on_residual(battle: &mut Battle, pokemon: &mut Pokemon) -> AbilityHandlerResult {
+    // if (pokemon.hp && pokemon.status && this.randomChance(33, 100))
+    if pokemon.hp > 0 && !pokemon.status.is_empty() && battle.random_chance(33, 100) {
+        // this.debug('shed skin');
+        // this.add('-activate', pokemon, 'ability: Shed Skin');
+        battle.add("-activate", &[Arg::Pokemon(pokemon), Arg::Str("ability: Shed Skin")]);
+        // pokemon.cureStatus();
+        pokemon.cure_status();
+    }
     AbilityHandlerResult::Undefined
 }
 
