85af560a1cbc6e7de261cd94580707c2018bd76a
Developer <dev@example.com>
2025-12-25 18:09:50 +0000

    Implement moldbreaker - Shows message on switch-in and makes moves ignore target abilities

--- src/data/ability_callbacks/moldbreaker.rs
+++ src/data/ability_callbacks/moldbreaker.rs
@@ -28,26 +28,17 @@ use super::{AbilityHandlerResult, Status, Effect};
 
 /// onStart(pokemon)
 /// Shows message on switch-in
-///
-/// TODO: onStart handler not yet implemented
-/// When implemented, should:
-/// 1. Add '-ability' message for Mold Breaker
-pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_start(battle: &mut Battle, pokemon: &Pokemon) -> AbilityHandlerResult {
     // this.add('-ability', pokemon, 'Mold Breaker');
+    battle.add("-ability", &[Arg::Pokemon(pokemon), Arg::Str("Mold Breaker")]);
     AbilityHandlerResult::Undefined
 }
 
 /// onModifyMove(move)
 /// Makes moves ignore target abilities
-///
-/// TODO: onModifyMove handler not yet implemented
-/// TODO: Needs move.ignoreAbility field
-/// When implemented, should:
-/// 1. Set move.ignoreAbility = true
-pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_modify_move(move_: &mut MoveDef, _pokemon: &Pokemon) -> AbilityHandlerResult {
     // move.ignoreAbility = true;
+    move_.ignores_ability = true;
     AbilityHandlerResult::Undefined
 }
 
