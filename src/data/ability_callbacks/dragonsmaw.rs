03f673e3b77458549755617e54933a14a75918e4
Developer <dev@example.com>
2025-12-25 13:34:57 +0000

    Implement dragonsmaw ability - boosts Dragon-type moves by 1.5x

--- src/data/ability_callbacks/dragonsmaw.rs
+++ src/data/ability_callbacks/dragonsmaw.rs
@@ -34,27 +34,30 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyAtkPriority(...)
-pub fn on_modify_atk_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onModifyAtkPriority: 5
+pub const ON_MODIFY_ATK_PRIORITY: i32 = 5;
+/// onModifySpAPriority: 5
+pub const ON_MODIFY_SPA_PRIORITY: i32 = 5;
+
+/// onModifyAtk(atk, attacker, defender, move)
+/// Boosts Dragon-type moves by 1.5x
+pub fn on_modify_atk(_atk: u32, _attacker: &Pokemon, _defender: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (move.type === 'Dragon')
+    if move_.move_type == "Dragon" {
+        // return this.chainModify(1.5);
+        return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
+    }
     AbilityHandlerResult::Undefined
 }
 
-/// onModifyAtk(...)
-pub fn on_modify_atk(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onModifySpAPriority(...)
-pub fn on_modify_sp_a_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onModifySpA(...)
-pub fn on_modify_sp_a(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onModifySpA(atk, attacker, defender, move)
+/// Boosts Dragon-type moves by 1.5x
+pub fn on_modify_sp_a(_spa: u32, _attacker: &Pokemon, _defender: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (move.type === 'Dragon')
+    if move_.move_type == "Dragon" {
+        // return this.chainModify(1.5);
+        return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
+    }
     AbilityHandlerResult::Undefined
 }
 
