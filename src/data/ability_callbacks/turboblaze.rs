6bc549e13c30f14451300abf954e82a44eecfad5
Developer <dev@example.com>
2025-12-25 17:35:02 +0000

    Document turboblaze - Makes moves ignore target abilities

--- src/data/ability_callbacks/turboblaze.rs
+++ src/data/ability_callbacks/turboblaze.rs
@@ -26,15 +26,28 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
+/// onStart(pokemon)
+/// Announces the ability when Pokemon enters battle
+///
+/// TODO: onStart handler needs Pokemon parameter
+/// When implemented, should:
+/// 1. Add message: this.add('-ability', pokemon, 'Turboblaze')
 pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onModifyMove(...)
+/// onModifyMove(move)
+/// Makes moves ignore target's abilities (same as Teravolt)
+///
+/// TODO: onModifyMove handler needs mutable MoveDef to set move.ignoreAbility
+/// Currently onModifyMove takes &MoveDef (immutable reference)
+/// When implemented, should:
+/// 1. Set move.ignoreAbility = true
 pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
+    // Requires mutable MoveDef to implement move.ignoreAbility = true
+    // See teravolt.rs and battlebond.rs for similar issue
     AbilityHandlerResult::Undefined
 }
 
