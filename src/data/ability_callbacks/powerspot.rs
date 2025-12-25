0b2c821adc582c8715b123f397b8ec54ddec92fb
Developer <dev@example.com>
2025-12-25 16:56:22 +0000

    Document powerspot - boosts ally moves by 1.3x

--- src/data/ability_callbacks/powerspot.rs
+++ src/data/ability_callbacks/powerspot.rs
@@ -27,13 +27,15 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onAllyBasePowerPriority(...)
-pub fn on_ally_base_power_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_ALLY_BASE_POWER_PRIORITY: i32 = 22;
 
-/// onAllyBasePower(...)
+/// onAllyBasePower(basePower, attacker, defender, move)
+/// Boosts ally move power by 1.3x
+///
+/// TODO: onAllyBasePower handler not yet implemented
+/// TODO: Needs effectState.target, attacker comparison
+/// When implemented, should:
+/// 1. If attacker is not the ability holder, return chainModify(5325, 4096) for 1.3x boost
 pub fn on_ally_base_power(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
