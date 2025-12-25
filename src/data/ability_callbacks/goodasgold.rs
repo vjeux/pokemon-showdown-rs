9a952214c8b8d535346b68f680bcf6dc2c76a999
Developer <dev@example.com>
2025-12-25 13:38:57 +0000

    Implement goodasgold ability - immune to Status moves

--- src/data/ability_callbacks/goodasgold.rs
+++ src/data/ability_callbacks/goodasgold.rs
@@ -26,9 +26,14 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryHit(...)
-pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onTryHit(target, source, move)
+/// Immune to Status moves from other Pokemon
+pub fn on_try_hit(target: &Pokemon, source: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (move.category === 'Status' && target !== source)
+    if move_.category == MoveCategory::Status && target.position != source.position {
+        // return null;
+        return AbilityHandlerResult::Null;
+    }
     AbilityHandlerResult::Undefined
 }
 
