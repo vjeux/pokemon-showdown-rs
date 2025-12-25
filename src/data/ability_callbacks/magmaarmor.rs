bd7840d6a8bd8855d23e0aae02b735da9bc509bb
Developer <dev@example.com>
2025-12-25 14:22:55 +0000

    Implement magmaarmor ability
    - onUpdate: Automatically cures freeze (frz) status
    - onImmunity: Returns False to grant immunity to freeze
    
    Matches JS implementation 1-to-1 from data/abilities.ts

--- src/data/ability_callbacks/magmaarmor.rs
+++ src/data/ability_callbacks/magmaarmor.rs
@@ -29,15 +29,23 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onUpdate(...)
-pub fn on_update(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onUpdate(pokemon)
+pub fn on_update(battle: &mut Battle, pokemon: &mut Pokemon) -> AbilityHandlerResult {
+    // if (pokemon.status === 'frz')
+    if pokemon.status.as_str() == "frz" {
+        // this.add('-activate', pokemon, 'ability: Magma Armor');
+        battle.add("-activate", &[Arg::Pokemon(pokemon), Arg::Str("ability: Magma Armor")]);
+        // pokemon.cureStatus();
+        pokemon.cure_status();
+    }
     AbilityHandlerResult::Undefined
 }
 
-/// onImmunity(...)
-pub fn on_immunity(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onImmunity(type, pokemon)
+pub fn on_immunity(immunity_type: &str, _pokemon: &Pokemon) -> AbilityHandlerResult {
+    // if (type === 'frz') return false;
+    if immunity_type == "frz" {
+        return AbilityHandlerResult::False;
+    }
     AbilityHandlerResult::Undefined
 }
-
