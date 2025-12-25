c13afd29d01657e4e12dfd1b9773f3c4f288d8ec
Developer <dev@example.com>
2025-12-25 14:27:32 +0000

    Implement speedboost ability
    - onResidual: Boosts Speed by 1 stage at end of each turn (after first turn)
      - Only activates if active_turns > 0
    
    Matches JS implementation 1-to-1 from data/abilities.ts

--- src/data/ability_callbacks/speedboost.rs
+++ src/data/ability_callbacks/speedboost.rs
@@ -27,21 +27,18 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onResidualOrder(...)
-pub fn on_residual_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onResidualOrder: 28
+pub const ON_RESIDUAL_ORDER: i32 = 28;
+/// onResidualSubOrder: 2
+pub const ON_RESIDUAL_SUB_ORDER: i32 = 2;
+
+/// onResidual(pokemon)
+pub fn on_residual(battle: &mut Battle, pokemon: &Pokemon) -> AbilityHandlerResult {
+    // if (pokemon.activeTurns)
+    if pokemon.active_turns > 0 {
+        // this.boost({ spe: 1 });
+        let pokemon_ref = (pokemon.side_index, pokemon.position);
+        battle.boost(&[("spe", 1)], pokemon_ref, Some(pokemon_ref), None);
+    }
     AbilityHandlerResult::Undefined
 }
-
-/// onResidualSubOrder(...)
-pub fn on_residual_sub_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onResidual(...)
-pub fn on_residual(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
