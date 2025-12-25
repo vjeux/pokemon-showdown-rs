130a313cea610e8d7553adeb08af86ee125a9c5f
Developer <dev@example.com>
2025-12-25 17:10:20 +0000

    Document sandveil - sandstorm immunity and evasion boost

--- src/data/ability_callbacks/sandveil.rs
+++ src/data/ability_callbacks/sandveil.rs
@@ -31,19 +31,29 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onImmunity(...)
+/// onImmunity(type, pokemon)
+/// Immune to sandstorm damage
+///
+/// TODO: onImmunity handler not yet implemented
+/// TODO: Needs immunity type checking
+/// When implemented, should:
+/// 1. If type is 'sandstorm', return false (immune)
 pub fn on_immunity(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onModifyAccuracyPriority(...)
-pub fn on_modify_accuracy_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_MODIFY_ACCURACY_PRIORITY: i32 = -1;
 
-/// onModifyAccuracy(...)
+/// onModifyAccuracy(accuracy)
+/// Lowers opponent's accuracy by ~20% in sandstorm
+///
+/// TODO: onModifyAccuracy handler not yet implemented
+/// TODO: Needs accuracy type checking, field.isWeather()
+/// When implemented, should:
+/// 1. Check if accuracy is a number
+/// 2. If weather is sandstorm
+/// 3. Multiply accuracy by 3277/4096 (~0.8x, so 20% reduction)
 pub fn on_modify_accuracy(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
