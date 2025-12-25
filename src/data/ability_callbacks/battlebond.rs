fa536a3408661c0000aa469275caedaa82bc85e6
Developer <dev@example.com>
2025-12-25 15:25:42 +0000

    Implement Battle Bond ability
    Implements onSourceAfterFaint handler that boosts Attack, Sp. Atk, and Speed
    by 1 stage when Greninja-Bond faints a foe with a move. Also includes
    onModifyMovePriority returning -1, and onModifyMove stub for Water Shuriken.
    
    Note: onModifyMove cannot fully modify moves yet due to Rust implementation
    limitations (requires mutable move). bondTriggered state setting also requires
    infrastructure changes.
    
    Tests passing.

--- src/data/ability_callbacks/battlebond.rs
+++ src/data/ability_callbacks/battlebond.rs
@@ -36,21 +36,73 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSourceAfterFaint(...)
-pub fn on_source_after_faint(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onSourceAfterFaint(length, target, source, effect)
+pub fn on_source_after_faint(battle: &mut Battle, _length: i32, _target: &Pokemon, source: &Pokemon, effect: &Effect) -> AbilityHandlerResult {
+    // if (source.bondTriggered) return;
+    if source.ability_state.data.get("bondTriggered").and_then(|v| v.as_bool()).unwrap_or(false) {
+        return AbilityHandlerResult::Undefined;
+    }
+
+    // if (effect?.effectType !== 'Move') return;
+    if effect.effect_type != "Move" {
+        return AbilityHandlerResult::Undefined;
+    }
+
+    // if (source.species.id === 'greninjabond' && source.hp && !source.transformed && source.side.foePokemonLeft())
+    if source.species_id.as_str() == "greninjabond" &&
+       source.hp > 0 &&
+       !source.transformed {
+        // Check if foes are left
+        let foe_side_index = 1 - source.side_index;
+        let foe_pokemon_left = battle.sides[foe_side_index].foe_pokemon_left();
+
+        if foe_pokemon_left > 0 {
+            // this.boost({ atk: 1, spa: 1, spe: 1 }, source, source, this.effect);
+            battle.boost(
+                &[("atk", 1), ("spa", 1), ("spe", 1)],
+                (source.side_index, source.position),
+                Some((source.side_index, source.position)),
+                None
+            );
+
+            // this.add('-activate', source, 'ability: Battle Bond');
+            battle.add("-activate", &[
+                Arg::Pokemon(source),
+                Arg::Str("ability: Battle Bond")
+            ]);
+
+            // source.bondTriggered = true;
+            // Note: We need to set bondTriggered in the source pokemon's ability_state
+            // This requires mutable access to source, which we don't have here.
+            // In practice, this would need to be handled by the battle system.
+            // For now, we'll leave a comment about this limitation.
+            // TODO: Set source.ability_state.data["bondTriggered"] = true
+        }
+    }
+
     AbilityHandlerResult::Undefined
 }
 
 /// onModifyMovePriority(...)
-pub fn on_modify_move_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
+pub fn on_modify_move_priority(_battle: &mut Battle) -> AbilityHandlerResult {
+    // onModifyMovePriority: -1,
+    AbilityHandlerResult::Number(-1)
 }
 
-/// onModifyMove(...)
-pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onModifyMove(move, attacker)
+pub fn on_modify_move(_battle: &mut Battle, move_: &MoveDef, attacker: &Pokemon) -> AbilityHandlerResult {
+    // if (move.id === 'watershuriken' && attacker.species.name === 'Greninja-Ash' && !attacker.transformed)
+    // Note: This requires mutable move to modify multihit, which is not currently
+    // supported in the Rust implementation. The JS version sets move.multihit = 3.
+    // This functionality would need infrastructure changes to support move modification.
+
+    // Check the conditions even though we can't modify the move yet
+    if move_.id.as_str() == "watershuriken" &&
+       attacker.species_id.as_str() == "greninjaash" &&
+       !attacker.transformed {
+        // TODO: Set move.multihit = 3 once move modification is supported
+    }
+
     AbilityHandlerResult::Undefined
 }
 
