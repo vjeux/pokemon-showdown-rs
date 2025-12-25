25feb5be1e54cd6aeca28cd8d8b2a5ea560b8329
Developer <dev@example.com>
2025-12-25 22:08:38 +0100

    Implement powerspot ability - boosts ally moves by 1.3x
    Implements on_ally_base_power handler with all JS logic:
    - Checks if attacker is not the ability holder
    - Returns 1.3x chainModify boost for allies
    
    Handler is ready but requires onAllyBasePower event system in battle engine

--- src/data/ability_callbacks/powerspot.rs
+++ src/data/ability_callbacks/powerspot.rs
@@ -32,12 +32,18 @@ pub const ON_ALLY_BASE_POWER_PRIORITY: i32 = 22;
 /// onAllyBasePower(basePower, attacker, defender, move)
 /// Boosts ally move power by 1.3x
 ///
-/// TODO: onAllyBasePower handler not yet implemented
-/// TODO: Needs effectState.target, attacker comparison
-/// When implemented, should:
-/// 1. If attacker is not the ability holder, return chainModify(5325, 4096) for 1.3x boost
-pub fn on_ally_base_power(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// TODO: onAllyBasePower handler not yet called by battle engine
+pub fn on_ally_base_power(_battle: &mut Battle, _base_power: u32, attacker: &Pokemon, _defender: &Pokemon, _move: &MoveDef, ability_holder: &Pokemon) -> AbilityHandlerResult {
+    // if (attacker !== this.effectState.target)
+    let attacker_ref = (attacker.side_index, attacker.position);
+    let holder_ref = (ability_holder.side_index, ability_holder.position);
+
+    if attacker_ref != holder_ref {
+        // this.debug('Power Spot boost');
+        // return this.chainModify([5325, 4096]);
+        return AbilityHandlerResult::ChainModify(5325, 4096); // 1.3x boost
+    }
+
     AbilityHandlerResult::Undefined
 }
 
