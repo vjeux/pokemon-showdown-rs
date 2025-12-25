6450f7b6e91956a4e45d5abc64aa09fff87ae6bf
Developer <dev@example.com>
2025-12-25 17:11:51 +0000

    Document scrappy - hit Ghosts and block Intimidate

--- src/data/ability_callbacks/scrappy.rs
+++ src/data/ability_callbacks/scrappy.rs
@@ -34,19 +34,30 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyMovePriority(...)
-pub fn on_modify_move_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_MODIFY_MOVE_PRIORITY: i32 = -5;
 
-/// onModifyMove(...)
+/// onModifyMove(move)
+/// Allows Fighting and Normal moves to hit Ghost types
+///
+/// TODO: onModifyMove handler not yet implemented
+/// TODO: Needs move.ignoreImmunity field manipulation
+/// When implemented, should:
+/// 1. Initialize move.ignoreImmunity as empty object if not set
+/// 2. If not set to true (all immunities), set Fighting and Normal to true
 pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onTryBoost(...)
+/// onTryBoost(boost, target, source, effect)
+/// Blocks Intimidate's Attack drop
+///
+/// TODO: onTryBoost handler not yet implemented
+/// TODO: Needs effect.name, boost.atk, delete boost.atk, battle.add()
+/// When implemented, should:
+/// 1. If effect is Intimidate and boost.atk exists
+/// 2. Delete boost.atk to prevent the drop
+/// 3. Show fail message for unboost Attack
 pub fn on_try_boost(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
