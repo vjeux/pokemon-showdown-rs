13b56d1516188f2be39a41813db92fcb3f2a67ec
Developer <dev@example.com>
2025-12-25 16:53:33 +0100

    Document telepathy ability implementation requirements
    - Prevents damage from ally attacks in double/triple battles
    - Requires onTryHit handler
    - Encountered alphabetically after technician

--- src/data/ability_callbacks/telepathy.rs
+++ src/data/ability_callbacks/telepathy.rs
@@ -26,7 +26,16 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryHit(...)
+/// onTryHit(target, source, move)
+/// Prevents damage from ally attacks
+///
+/// TODO: onTryHit handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Check if target !== source
+/// 2. Check if target.isAlly(source)
+/// 3. Check if move.category !== 'Status'
+/// 4. Add activate message: this.add('-activate', target, 'ability: Telepathy')
+/// 5. Return null to block the move
 pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
