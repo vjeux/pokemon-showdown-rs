22ffbac886b7e55e6c50a196da61f52650905294
Developer <dev@example.com>
2025-12-25 15:31:20 +0000

    Implement Gluttony ability
    Sets gluttony flag in ability state on start and when taking damage.
    This flag is used by berry consumption logic to eat berries earlier
    (at 50% HP instead of 25%).
    
    Tests passing.

--- src/data/ability_callbacks/gluttony.rs
+++ src/data/ability_callbacks/gluttony.rs
@@ -26,15 +26,17 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
-pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onStart(pokemon)
+pub fn on_start(_battle: &mut Battle, pokemon: &mut Pokemon) -> AbilityHandlerResult {
+    // pokemon.abilityState.gluttony = true;
+    pokemon.ability_state.data.insert("gluttony".to_string(), serde_json::Value::Bool(true));
     AbilityHandlerResult::Undefined
 }
 
-/// onDamage(...)
-pub fn on_damage(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onDamage(item, pokemon)
+pub fn on_damage(_damage: u32, pokemon: &mut Pokemon) -> AbilityHandlerResult {
+    // pokemon.abilityState.gluttony = true;
+    pokemon.ability_state.data.insert("gluttony".to_string(), serde_json::Value::Bool(true));
     AbilityHandlerResult::Undefined
 }
 
