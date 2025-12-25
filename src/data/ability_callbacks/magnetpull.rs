0c1ead41d9cf124491dce110ad8e8a56e6ccf081
Developer <dev@example.com>
2025-12-25 16:34:13 +0000

    Document magnetpull - traps Steel-type foes

--- src/data/ability_callbacks/magnetpull.rs
+++ src/data/ability_callbacks/magnetpull.rs
@@ -32,13 +32,28 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onFoeTrapPokemon(...)
+/// onFoeTrapPokemon(pokemon)
+/// Traps adjacent Steel-type foes
+///
+/// TODO: onFoeTrapPokemon handler not yet implemented
+/// TODO: Needs pokemon.hasType(), isAdjacent(), tryTrap()
+/// When implemented, should:
+/// 1. Check if pokemon.hasType('Steel') && pokemon.isAdjacent(effectState.target)
+/// 2. Call pokemon.tryTrap(true)
 pub fn on_foe_trap_pokemon(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onFoeMaybeTrapPokemon(...)
+/// onFoeMaybeTrapPokemon(pokemon, source)
+/// Marks Steel-type foes as potentially trapped
+///
+/// TODO: onFoeMaybeTrapPokemon handler not yet implemented
+/// TODO: Needs pokemon.knownType, pokemon.maybeTrapped field
+/// When implemented, should:
+/// 1. Default source to effectState.target if not provided
+/// 2. Check if pokemon.isAdjacent(source)
+/// 3. Set pokemon.maybeTrapped = true for Steel types or unknown types
 pub fn on_foe_maybe_trap_pokemon(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
