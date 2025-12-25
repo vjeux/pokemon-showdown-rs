19c3bfb8429957c9f51ceb3faec1d8f6f5d1e304
Developer <dev@example.com>
2025-12-25 17:19:30 +0000

    Document stakeout - doubles damage vs switched-in Pokemon

--- src/data/ability_callbacks/stakeout.rs
+++ src/data/ability_callbacks/stakeout.rs
@@ -34,25 +34,31 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyAtkPriority(...)
-pub fn on_modify_atk_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_MODIFY_ATK_PRIORITY: i32 = 5;
 
-/// onModifyAtk(...)
+/// onModifyAtk(atk, attacker, defender)
+/// Doubles Attack against Pokemon that just switched in
+///
+/// TODO: onModifyAtk handler not yet implemented
+/// TODO: Needs defender.activeTurns
+/// When implemented, should:
+/// 1. Check if defender.activeTurns is 0 (just switched in)
+/// 2. Multiply Attack by 2x (ChainModify(2, 1))
 pub fn on_modify_atk(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onModifySpAPriority(...)
-pub fn on_modify_sp_a_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_MODIFY_SP_A_PRIORITY: i32 = 5;
 
-/// onModifySpA(...)
+/// onModifySpA(atk, attacker, defender)
+/// Doubles Special Attack against Pokemon that just switched in
+///
+/// TODO: onModifySpA handler not yet implemented
+/// TODO: Needs defender.activeTurns
+/// When implemented, should:
+/// 1. Check if defender.activeTurns is 0 (just switched in)
+/// 2. Multiply Special Attack by 2x (ChainModify(2, 1))
 pub fn on_modify_sp_a(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
