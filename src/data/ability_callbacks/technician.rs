69c8be803f907027ffa29e87a6751d003a429da3
Developer <dev@example.com>
2025-12-25 15:53:01 +0000

    Implement Technician ability
    - Boosts moves with base power 60 or less by 1.5x
    - Uses onBasePower handler with priority 30
    - Simplified implementation checking base_power directly
    - Note: May need event.modifier support for exact JS parity
    - Encountered alphabetically after tanglinghair
    - All tests passing (46 tests)

--- src/data/ability_callbacks/technician.rs
+++ src/data/ability_callbacks/technician.rs
@@ -29,15 +29,24 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onBasePowerPriority(...)
-pub fn on_base_power_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+/// onBasePowerPriority: 30
+pub const ON_BASE_POWER_PRIORITY: i32 = 30;
 
-/// onBasePower(...)
-pub fn on_base_power(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onBasePower(basePower, attacker, defender, move)
+/// Boosts base power of moves with 60 or less base power by 1.5x
+///
+/// Note: JS checks basePowerAfterMultiplier using this.modify(basePower, this.event.modifier)
+/// For now, implementing simple check against base_power parameter
+/// TODO: May need to account for prior modifiers in the damage calculation chain
+pub fn on_base_power(_battle: &Battle, base_power: u32, _attacker: &Pokemon, _defender: &Pokemon, _move: &MoveDef) -> AbilityHandlerResult {
+    // const basePowerAfterMultiplier = this.modify(basePower, this.event.modifier);
+    // if (basePowerAfterMultiplier <= 60)
+    // Note: Simplified - checking base_power directly. May need event.modifier support later.
+    if base_power <= 60 {
+        // this.debug('Technician boost');
+        // return this.chainModify(1.5);
+        return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
+    }
     AbilityHandlerResult::Undefined
 }
 
