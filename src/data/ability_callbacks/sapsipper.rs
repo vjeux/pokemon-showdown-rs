7009ff62e8f1b1919e234f5ec00e3cd361587756
Developer <dev@example.com>
2025-12-25 17:10:49 +0000

    Document sapsipper - blocks Grass moves for Attack boost

--- src/data/ability_callbacks/sapsipper.rs
+++ src/data/ability_callbacks/sapsipper.rs
@@ -35,19 +35,32 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryHitPriority(...)
-pub fn on_try_hit_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_TRY_HIT_PRIORITY: i32 = 1;
 
-/// onTryHit(...)
+/// onTryHit(target, source, move)
+/// Blocks Grass moves and boosts Attack instead
+///
+/// TODO: onTryHit handler not yet implemented
+/// TODO: Needs target, source, move.type, boost(), battle.add()
+/// When implemented, should:
+/// 1. Check if target is not source and move is Grass-type
+/// 2. Try to boost Attack by 1 stage
+/// 3. If boost fails (already maxed), show immune message
+/// 4. Return null to block the move
 pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAllyTryHitSide(...)
+/// onAllyTryHitSide(target, source, move)
+/// Boosts Attack when ally is targeted by Grass move
+///
+/// TODO: onAllyTryHitSide handler not yet implemented
+/// TODO: Needs effectState.target, target.isAlly(), move.type, boost()
+/// When implemented, should:
+/// 1. Skip if source is the ability holder or target is not ally of source
+/// 2. If move is Grass-type
+/// 3. Boost the ability holder's Attack by 1 stage
 pub fn on_ally_try_hit_side(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
