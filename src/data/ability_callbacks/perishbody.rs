ad132a19dcd8061b9e665585361d8d8968ea328f
Developer <dev@example.com>
2025-12-25 16:51:40 +0000

    Document perishbody - inflicts Perish Song on contact

--- src/data/ability_callbacks/perishbody.rs
+++ src/data/ability_callbacks/perishbody.rs
@@ -26,7 +26,15 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onDamagingHit(...)
+/// onDamagingHit(damage, target, source, move)
+/// Inflicts Perish Song on both self and attacker when hit by contact move
+///
+/// TODO: onDamagingHit handler not yet implemented
+/// TODO: Needs checkMoveMakesContact(), source.volatiles, addVolatile()
+/// When implemented, should:
+/// 1. Skip if move doesn't make contact or source already has perishsong
+/// 2. Add ability message
+/// 3. Add perishsong volatile to both source and target
 pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
