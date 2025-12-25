a9a2225be62e3999fb54ea5e0f2aeb7029a7ad3d
Developer <dev@example.com>
2025-12-25 17:52:00 +0000

    Implement hypercutter - Prevents Attack stat from being lowered by opponents

--- src/data/ability_callbacks/hypercutter.rs
+++ src/data/ability_callbacks/hypercutter.rs
@@ -31,19 +31,31 @@ use super::{AbilityHandlerResult, Status, Effect};
 
 /// onTryBoost(boost, target, source, effect)
 /// Prevents Attack stat from being lowered by opponents
-///
-/// TODO: onTryBoost handler not yet implemented
-/// TODO: Needs mutable boost object to delete negative Attack boosts
-/// TODO: Needs source/target comparison
-/// TODO: Needs effect.secondaries checking
-/// When implemented, should:
-/// 1. Skip if source and target are the same (self-inflicted)
-/// 2. Check if boost.atk exists and is negative
-/// 3. If so, delete boost.atk to prevent the Attack drop
-/// 4. If effect doesn't have secondaries, add fail message
-pub fn on_try_boost(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    // Requires onTryBoost handler with mutable boost object
+pub fn on_try_boost(battle: &mut Battle, boost: &mut std::collections::HashMap<String, i8>, target: &Pokemon, source: Option<&Pokemon>, effect_id: &str, has_secondaries: bool) -> AbilityHandlerResult {
+    // if (source && target === source) return;
+    if let Some(src) = source {
+        if (target.side_index, target.position) == (src.side_index, src.position) {
+            return AbilityHandlerResult::Undefined;
+        }
+    }
+    // if (boost.atk && boost.atk < 0)
+    if let Some(&atk_boost) = boost.get("atk") {
+        if atk_boost < 0 {
+            // delete boost.atk;
+            boost.remove("atk");
+            // if (!(effect as ActiveMove).secondaries)
+            if !has_secondaries {
+                // this.add("-fail", target, "unboost", "Attack", "[from] ability: Hyper Cutter", `[of] ${target}`);
+                battle.add("-fail", &[
+                    Arg::Pokemon(target),
+                    Arg::Str("unboost"),
+                    Arg::Str("Attack"),
+                    Arg::Str("[from] ability: Hyper Cutter"),
+                    Arg::Str(&format!("[of] {}", target.name))
+                ]);
+            }
+        }
+    }
     AbilityHandlerResult::Undefined
 }
 
