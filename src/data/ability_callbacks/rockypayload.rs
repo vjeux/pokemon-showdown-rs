c8f9ff8e9c05e1d5fd7ac2878be0b9c8a2e831e0
Developer <dev@example.com>
2025-12-25 13:54:51 +0000

    Implement rockypayload ability
    Boosts Rock-type moves by 1.5x.
    Affects both physical and special moves.

--- src/data/ability_callbacks/rockypayload.rs
+++ src/data/ability_callbacks/rockypayload.rs
@@ -34,27 +34,29 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyAtkPriority(...)
-pub fn on_modify_atk_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub const ON_MODIFY_ATK_PRIORITY: i32 = 5;
+
+/// onModifyAtk(atk, attacker, defender, move)
+/// Boosts Rock-type moves by 1.5x
+pub fn on_modify_atk(_atk: u32, _attacker: &Pokemon, _defender: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (move.type === 'Rock')
+    if move_.move_type == "Rock" {
+        // return this.chainModify(1.5);
+        return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
+    }
     AbilityHandlerResult::Undefined
 }
 
-/// onModifyAtk(...)
-pub fn on_modify_atk(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_MODIFY_SP_A_PRIORITY: i32 = 5;
 
-/// onModifySpAPriority(...)
-pub fn on_modify_sp_a_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onModifySpA(...)
-pub fn on_modify_sp_a(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onModifySpA(atk, attacker, defender, move)
+/// Boosts Rock-type moves by 1.5x
+pub fn on_modify_sp_a(_atk: u32, _attacker: &Pokemon, _defender: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (move.type === 'Rock')
+    if move_.move_type == "Rock" {
+        // return this.chainModify(1.5);
+        return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
+    }
     AbilityHandlerResult::Undefined
 }
 
