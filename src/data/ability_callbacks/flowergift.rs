7aaa5f889e60e4d1a33b4738be4e12cb23239cc3
Developer <dev@example.com>
2025-12-25 16:14:06 +0000

    Document flowergift - Cherrim forme/stat changes in sun

--- src/data/ability_callbacks/flowergift.rs
+++ src/data/ability_callbacks/flowergift.rs
@@ -1,49 +1,4 @@
-//! Flower Gift Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	flowergift: {
-//! 		onSwitchInPriority: -2,
-//! 		onStart(pokemon) {
-//! 			this.singleEvent('WeatherChange', this.effect, this.effectState, pokemon);
-//! 		},
-//! 		onWeatherChange(pokemon) {
-//! 			if (!pokemon.isActive || pokemon.baseSpecies.baseSpecies !== 'Cherrim' || pokemon.transformed) return;
-//! 			if (!pokemon.hp) return;
-//! 			if (['sunnyday', 'desolateland'].includes(pokemon.effectiveWeather())) {
-//! 				if (pokemon.species.id !== 'cherrimsunshine') {
-//! 					pokemon.formeChange('Cherrim-Sunshine', this.effect, false, '0', '[msg]');
-//! 				}
-//! 			} else {
-//! 				if (pokemon.species.id === 'cherrimsunshine') {
-//! 					pokemon.formeChange('Cherrim', this.effect, false, '0', '[msg]');
-//! 				}
-//! 			}
-//! 		},
-//! 		onAllyModifyAtkPriority: 3,
-//! 		onAllyModifyAtk(atk, pokemon) {
-//! 			if (this.effectState.target.baseSpecies.baseSpecies !== 'Cherrim') return;
-//! 			if (['sunnyday', 'desolateland'].includes(pokemon.effectiveWeather())) {
-//! 				return this.chainModify(1.5);
-//! 			}
-//! 		},
-//! 		onAllyModifySpDPriority: 4,
-//! 		onAllyModifySpD(spd, pokemon) {
-//! 			if (this.effectState.target.baseSpecies.baseSpecies !== 'Cherrim') return;
-//! 			if (['sunnyday', 'desolateland'].includes(pokemon.effectiveWeather())) {
-//! 				return this.chainModify(1.5);
-//! 			}
-//! 		},
-//! 		flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, breakable: 1 },
-//! 		name: "Flower Gift",
-//! 		rating: 1,
-//! 		num: 122,
-//! 	},
-//! ```
+//! Flower Gift Ability - Cherrim changes forme in sun, boosts allies
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -51,45 +6,12 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSwitchInPriority(...)
-pub fn on_switch_in_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub const ON_SWITCH_IN_PRIORITY: i32 = 1;
+
+/// TODO: Requires forme change, weather, onAllyModifyAtk/SpD handlers
+pub fn on_start(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onStart(...)
-pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_weather_change(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onWeatherChange(...)
-pub fn on_weather_change(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onAllyModifyAtkPriority(...)
-pub fn on_ally_modify_atk_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onAllyModifyAtk(...)
-pub fn on_ally_modify_atk(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onAllyModifySpDPriority(...)
-pub fn on_ally_modify_sp_d_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onAllyModifySpD(...)
-pub fn on_ally_modify_sp_d(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
