3b087b81b261ae9ac2fc9ddec034c2a31581dd69
Developer <dev@example.com>
2025-12-25 16:59:31 +0000

    Document purifyingsalt - prevents status and halves Ghost damage

--- src/data/ability_callbacks/purifyingsalt.rs
+++ src/data/ability_callbacks/purifyingsalt.rs
@@ -46,37 +46,54 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSetStatus(...)
+/// onSetStatus(status, target, source, effect)
+/// Prevents all status conditions
+///
+/// TODO: onSetStatus handler not yet implemented
+/// TODO: Needs effect checking
+/// When implemented, should:
+/// 1. If effect has status property, add immune message
+/// 2. Return false to prevent any status
 pub fn on_set_status(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onTryAddVolatile(...)
+/// onTryAddVolatile(status, target)
+/// Prevents Yawn
+///
+/// TODO: onTryAddVolatile handler not yet implemented
+/// TODO: Needs status.id
+/// When implemented, should:
+/// 1. If status is yawn, add immune message and return null to prevent it
 pub fn on_try_add_volatile(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onSourceModifyAtkPriority(...)
-pub fn on_source_modify_atk_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_SOURCE_MODIFY_ATK_PRIORITY: i32 = 6;
 
-/// onSourceModifyAtk(...)
+/// onSourceModifyAtk(atk, attacker, defender, move)
+/// Halves damage from Ghost-type moves (Atk)
+///
+/// TODO: onSourceModifyAtk handler not yet implemented
+/// TODO: Needs move.type
+/// When implemented, should:
+/// 1. If move is Ghost-type, return chainModify(0.5)
 pub fn on_source_modify_atk(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onSourceModifySpAPriority(...)
-pub fn on_source_modify_sp_a_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_SOURCE_MODIFY_SP_A_PRIORITY: i32 = 5;
 
-/// onSourceModifySpA(...)
+/// onSourceModifySpA(spa, attacker, defender, move)
+/// Halves damage from Ghost-type moves (SpA)
+///
+/// TODO: onSourceModifySpA handler not yet implemented
+/// TODO: Needs move.type
+/// When implemented, should:
+/// 1. If move is Ghost-type, return chainModify(0.5)
 pub fn on_source_modify_sp_a(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
