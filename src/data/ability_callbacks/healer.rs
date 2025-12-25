9d6bfbcb41641b185642dc2f962c93d8489241b6
Developer <dev@example.com>
2025-12-25 16:15:39 +0000

    Document healer - heals ally status

--- src/data/ability_callbacks/healer.rs
+++ src/data/ability_callbacks/healer.rs
@@ -1,28 +1,4 @@
-//! Healer Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	healer: {
-//! 		onResidualOrder: 5,
-//! 		onResidualSubOrder: 3,
-//! 		onResidual(pokemon) {
-//! 			for (const allyActive of pokemon.adjacentAllies()) {
-//! 				if (allyActive.status && this.randomChance(3, 10)) {
-//! 					this.add('-activate', pokemon, 'ability: Healer');
-//! 					allyActive.cureStatus();
-//! 				}
-//! 			}
-//! 		},
-//! 		flags: {},
-//! 		name: "Healer",
-//! 		rating: 0,
-//! 		num: 131,
-//! 	},
-//! ```
+//! Healer Ability - Heals ally status in doubles
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -30,21 +6,8 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onResidualOrder(...)
-pub fn on_residual_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// TODO: Requires onResidualOrder, allies(), random chance
+pub const ON_RESIDUAL_ORDER: i32 = 5;
+pub fn on_residual(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onResidualSubOrder(...)
-pub fn on_residual_sub_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onResidual(...)
-pub fn on_residual(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
