31b222cd22cb6d52cb2e19b169a65c0553deaa08
Developer <dev@example.com>
2025-12-25 17:28:30 +0000

    Document sweetveil - Prevents allies from falling asleep

--- src/data/ability_callbacks/sweetveil.rs
+++ src/data/ability_callbacks/sweetveil.rs
@@ -36,13 +36,31 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onAllySetStatus(...)
+/// onAllySetStatus(status, target, source, effect)
+/// Prevents allies (and self) from falling asleep
+///
+/// TODO: onAllySetStatus handler not yet implemented
+/// TODO: Needs status.id checking, battle.effectState.target, battle.add()
+/// When implemented, should:
+/// 1. Check if status.id is 'slp' (sleep)
+/// 2. Get effectHolder from effectState.target (the Pokemon with Sweet Veil)
+/// 3. Add block message: battle.add('-block', target, 'ability: Sweet Veil', '[of] effectHolder')
+/// 4. Return Null to prevent sleep
 pub fn on_ally_set_status(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAllyTryAddVolatile(...)
+/// onAllyTryAddVolatile(status, target)
+/// Prevents Yawn from being applied to allies (and self)
+///
+/// TODO: onAllyTryAddVolatile handler not yet implemented
+/// TODO: Needs status.id checking, battle.effectState.target, battle.add()
+/// When implemented, should:
+/// 1. Check if status.id is 'yawn'
+/// 2. Get effectHolder from effectState.target
+/// 3. Add block message: battle.add('-block', target, 'ability: Sweet Veil', '[of] effectHolder')
+/// 4. Return Null to prevent Yawn
 pub fn on_ally_try_add_volatile(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
