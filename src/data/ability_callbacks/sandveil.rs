c2e42e4af06ce2276e17856020fa4c96d0f935a9
Developer <dev@example.com>
2025-12-25 19:25:45 +0000

    Implement sandveil ability - sandstorm immunity and evasion boost
    - Added onImmunity handler for sandstorm immunity
    - Added onModifyAccuracy handler to reduce opponent accuracy by 20% in sandstorm
    - Uses field.is_weather() to check for sandstorm

--- src/data/ability_callbacks/sandveil.rs
+++ src/data/ability_callbacks/sandveil.rs
@@ -31,31 +31,30 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
+pub const ON_MODIFY_ACCURACY_PRIORITY: i32 = -1;
+
 /// onImmunity(type, pokemon)
 /// Immune to sandstorm damage
-///
-/// TODO: onImmunity handler not yet implemented
-/// TODO: Needs immunity type checking
-/// When implemented, should:
-/// 1. If type is 'sandstorm', return false (immune)
-pub fn on_immunity(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_immunity(_battle: &mut Battle, immunity_type: &str, _pokemon: &Pokemon) -> AbilityHandlerResult {
+    // if (type === 'sandstorm') return false;
+    if immunity_type == "sandstorm" {
+        return AbilityHandlerResult::False;
+    }
     AbilityHandlerResult::Undefined
 }
 
-pub const ON_MODIFY_ACCURACY_PRIORITY: i32 = -1;
-
 /// onModifyAccuracy(accuracy)
 /// Lowers opponent's accuracy by ~20% in sandstorm
-///
-/// TODO: onModifyAccuracy handler not yet implemented
-/// TODO: Needs accuracy type checking, field.isWeather()
-/// When implemented, should:
-/// 1. Check if accuracy is a number
-/// 2. If weather is sandstorm
-/// 3. Multiply accuracy by 3277/4096 (~0.8x, so 20% reduction)
-pub fn on_modify_accuracy(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_modify_accuracy(battle: &mut Battle, _accuracy: u32) -> AbilityHandlerResult {
+    // if (typeof accuracy !== 'number') return;
+    // Note: In Rust, accuracy is always u32, so this check is implicit
+
+    // if (this.field.isWeather('sandstorm'))
+    if battle.field.is_weather("sandstorm") {
+        // this.debug('Sand Veil - decreasing accuracy');
+        // return this.chainModify([3277, 4096]);
+        return AbilityHandlerResult::ChainModify(3277, 4096); // ~0.8x
+    }
     AbilityHandlerResult::Undefined
 }
 
