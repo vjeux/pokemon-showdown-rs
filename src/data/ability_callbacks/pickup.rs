a7242b2e29bbe4a143b9f828a8cab809e763c10e
Developer <dev@example.com>
2025-12-25 16:52:50 +0000

    Document pickup - picks up adjacent Pokemon used items

--- src/data/ability_callbacks/pickup.rs
+++ src/data/ability_callbacks/pickup.rs
@@ -34,19 +34,21 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onResidualOrder(...)
-pub fn on_residual_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_RESIDUAL_ORDER: i32 = 28;
+pub const ON_RESIDUAL_SUB_ORDER: i32 = 2;
 
-/// onResidualSubOrder(...)
-pub fn on_residual_sub_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onResidual(...)
+/// onResidual(pokemon)
+/// Picks up items used by adjacent Pokemon this turn
+///
+/// TODO: onResidual handler not yet implemented
+/// TODO: Needs pokemon.item, getAllActive(), target.lastItem, target.usedItemThisTurn, pokemon.isAdjacent(), sample(), setItem()
+/// When implemented, should:
+/// 1. Skip if pokemon already has an item
+/// 2. Filter all active Pokemon for those with lastItem, usedItemThisTurn, and adjacent to this Pokemon
+/// 3. If no targets, return
+/// 4. Randomly sample one target
+/// 5. Take the target's lastItem and clear it
+/// 6. Add item message and set the item on this Pokemon
 pub fn on_residual(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
