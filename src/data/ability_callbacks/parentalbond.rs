39aacb7388c4b4b58a85e3d0b3d2b05a7ac8522f
Developer <dev@example.com>
2025-12-25 16:50:40 +0000

    Document parentalbond - makes damaging moves hit twice

--- src/data/ability_callbacks/parentalbond.rs
+++ src/data/ability_callbacks/parentalbond.rs
@@ -33,13 +33,27 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onPrepareHit(...)
+/// onPrepareHit(source, target, move)
+/// Makes damaging moves hit twice (second hit is 25% power)
+///
+/// TODO: onPrepareHit handler not yet implemented
+/// TODO: Needs move.category, move.multihit, move.flags, move.spreadHit, move.isZ, move.isMax
+/// When implemented, should:
+/// 1. Skip if move is Status, already multihit, has noparentalbond flag, charge flag, futuremove flag, spreadHit, isZ, or isMax
+/// 2. Set move.multihit = 2 and move.multihitType = 'parentalbond'
 pub fn on_prepare_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onSourceModifySecondaries(...)
+/// onSourceModifySecondaries(secondaries, target, source, move)
+/// Filters secondaries for Secret Power to prevent suppressing King's Rock/Razor Fang
+///
+/// TODO: onSourceModifySecondaries handler not yet implemented
+/// TODO: Needs move.multihitType, move.id, move.hit, secondaries array manipulation
+/// When implemented, should:
+/// 1. If move.multihitType === 'parentalbond' && move.id === 'secretpower' && move.hit < 2
+/// 2. Return secondaries filtered to only flinch effects
 pub fn on_source_modify_secondaries(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
