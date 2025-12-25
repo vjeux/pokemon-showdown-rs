6ea0bd8612d7ad433a3cd101d3c4ccb1218fbe3f
Developer <dev@example.com>
2025-12-25 14:00:47 +0000

    Implement prismarmor and solidrock abilities
    Both reduce super-effective damage by 25%.

--- src/data/ability_callbacks/prismarmor.rs
+++ src/data/ability_callbacks/prismarmor.rs
@@ -26,9 +26,15 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSourceModifyDamage(...)
-pub fn on_source_modify_damage(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onSourceModifyDamage(damage, source, target, move)
+/// Reduces super-effective damage by 25%
+pub fn on_source_modify_damage(_damage: u32, _source: &Pokemon, target: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (target.getMoveHitData(move).typeMod > 0)
+    let hit_data = target.get_move_hit_data(&move_.id);
+    if hit_data.type_mod > 0 {
+        // return this.chainModify(0.75);
+        return AbilityHandlerResult::ChainModify(3072, 4096); // 0.75x
+    }
     AbilityHandlerResult::Undefined
 }
 
