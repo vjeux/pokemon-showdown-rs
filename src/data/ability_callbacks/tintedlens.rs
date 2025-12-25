d65aa5e0b0265bffb977d3b9f66f0b08c034e1cf
Developer <dev@example.com>
2025-12-25 16:02:04 +0000

    Document tintedlens ability implementation requirements
    - Doubles damage of not very effective moves
    - Needs onModifyDamage handler
    - Needs target.getMoveHitData(move).typeMod system
    - Encountered alphabetically after thickfat

--- src/data/ability_callbacks/tintedlens.rs
+++ src/data/ability_callbacks/tintedlens.rs
@@ -26,9 +26,17 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyDamage(...)
-pub fn on_modify_damage(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
+/// onModifyDamage(damage, source, target, move)
+/// Doubles damage of not very effective moves
+///
+/// TODO: onModifyDamage handler not yet implemented in battle system
+/// TODO: target.getMoveHitData(move).typeMod not yet available
+/// When implemented, should:
+/// 1. Check if target.getMoveHitData(move).typeMod < 0 (not very effective)
+/// 2. Return this.chainModify(2) to double damage
+pub fn on_modify_damage(_battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
+    // Requires type effectiveness calculation system
     AbilityHandlerResult::Undefined
 }
 
