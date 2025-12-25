b6a0744d6149a4c8406a380d75b990e551243b98
Developer <dev@example.com>
2025-12-25 15:45:04 +0000

    Document snowcloak ability implementation requirements
    - Added detailed TODOs for onImmunity and onModifyAccuracy handlers
    - Provides hail immunity and accuracy reduction in hail/snow weather
    - Added ON_MODIFY_ACCURACY_PRIORITY constant
    - Encountered alphabetically after sniper

--- src/data/ability_callbacks/snowcloak.rs
+++ src/data/ability_callbacks/snowcloak.rs
@@ -31,19 +31,27 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onImmunity(...)
+/// onImmunity(type, pokemon)
+/// Provides immunity to hail damage
+///
+/// TODO: onImmunity handler not yet implemented in battle system
+/// When implemented, should check if type === 'hail' and return false
 pub fn on_immunity(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onModifyAccuracyPriority(...)
-pub fn on_modify_accuracy_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+/// onModifyAccuracyPriority: -1
+pub const ON_MODIFY_ACCURACY_PRIORITY: i32 = -1;
 
-/// onModifyAccuracy(...)
+/// onModifyAccuracy(accuracy)
+/// Decreases opponent accuracy by ~20% in hail/snow
+///
+/// TODO: onModifyAccuracy handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Check if accuracy is a number (typeof accuracy !== 'number')
+/// 2. Check if weather is hail or snowscape
+/// 3. Return chainModify([3277, 4096]) which is ~0.8x accuracy for opponents
 pub fn on_modify_accuracy(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
