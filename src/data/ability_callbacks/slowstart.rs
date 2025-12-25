d389adf75908107d684f496499fdfcc1c995b201
Developer <dev@example.com>
2025-12-25 15:36:28 +0000

    Implement Slow Start ability (partial)
    Implements onStart to announce ability and set 5-turn counter, and onResidual
    to count down turns and announce when effect ends. Also includes stubs for
    onModifyAtk and onModifySpe that check the counter (will halve stats when
    stat modification infrastructure is complete).
    
    Tests passing.

--- src/data/ability_callbacks/slowstart.rs
+++ src/data/ability_callbacks/slowstart.rs
@@ -46,45 +46,72 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
-pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onStart(pokemon)
+pub fn on_start(battle: &mut Battle, pokemon: &mut Pokemon) -> AbilityHandlerResult {
+    // this.add('-start', pokemon, 'ability: Slow Start');
+    battle.add("-start", &[Arg::Pokemon(pokemon), Arg::Str("ability: Slow Start")]);
+    // this.effectState.counter = 5;
+    pokemon.ability_state.data.insert("counter".to_string(), serde_json::Value::Number(5.into()));
     AbilityHandlerResult::Undefined
 }
 
-/// onResidualOrder(...)
-pub fn on_residual_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onResidualOrder: 28
+pub const ON_RESIDUAL_ORDER: i32 = 28;
+/// onResidualSubOrder: 2
+pub const ON_RESIDUAL_SUB_ORDER: i32 = 2;
+
+/// onResidual(pokemon)
+pub fn on_residual(battle: &mut Battle, pokemon: &mut Pokemon) -> AbilityHandlerResult {
+    // if (pokemon.activeTurns && this.effectState.counter)
+    if pokemon.active_turns > 0 {
+        if let Some(counter_val) = pokemon.ability_state.data.get_mut("counter") {
+            if let Some(counter) = counter_val.as_i64() {
+                if counter > 0 {
+                    // this.effectState.counter--;
+                    *counter_val = serde_json::Value::Number((counter - 1).into());
+
+                    // if (!this.effectState.counter)
+                    if counter - 1 == 0 {
+                        // this.add('-end', pokemon, 'Slow Start');
+                        battle.add("-end", &[Arg::Pokemon(pokemon), Arg::Str("Slow Start")]);
+                        // delete this.effectState.counter;
+                        pokemon.ability_state.data.remove("counter");
+                    }
+                }
+            }
+        }
+    }
     AbilityHandlerResult::Undefined
 }
 
-/// onResidualSubOrder(...)
-pub fn on_residual_sub_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onModifyAtkPriority: 5
+pub const ON_MODIFY_ATK_PRIORITY: i32 = 5;
+
+/// onModifyAtk(atk, pokemon)
+pub fn on_modify_atk(_battle: &mut Battle, pokemon: &Pokemon) -> AbilityHandlerResult {
+    // if (this.effectState.counter)
+    if let Some(counter) = pokemon.ability_state.data.get("counter").and_then(|v| v.as_i64()) {
+        if counter > 0 {
+            // return this.chainModify(0.5);
+            // Note: This should return chainModify(0.5) to halve attack
+            // For now returning Undefined as stat modification infrastructure may not be complete
+            // TODO: Return chainModify when infrastructure supports it
+        }
+    }
     AbilityHandlerResult::Undefined
 }
 
-/// onResidual(...)
-pub fn on_residual(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onModifyAtkPriority(...)
-pub fn on_modify_atk_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onModifyAtk(...)
-pub fn on_modify_atk(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onModifySpe(...)
-pub fn on_modify_spe(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onModifySpe(spe, pokemon)
+pub fn on_modify_spe(_battle: &mut Battle, pokemon: &Pokemon) -> AbilityHandlerResult {
+    // if (this.effectState.counter)
+    if let Some(counter) = pokemon.ability_state.data.get("counter").and_then(|v| v.as_i64()) {
+        if counter > 0 {
+            // return this.chainModify(0.5);
+            // Note: This should return chainModify(0.5) to halve speed
+            // For now returning Undefined as stat modification infrastructure may not be complete
+            // TODO: Return chainModify when infrastructure supports it
+        }
+    }
     AbilityHandlerResult::Undefined
 }
 
