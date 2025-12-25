1a682550c5998fbdc2058e496fa316e71abc117c
Developer <dev@example.com>
2025-12-25 17:13:55 +0000

    Document shadowtag - traps adjacent foes

--- src/data/ability_callbacks/shadowtag.rs
+++ src/data/ability_callbacks/shadowtag.rs
@@ -32,13 +32,28 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onFoeTrapPokemon(...)
+/// onFoeTrapPokemon(pokemon)
+/// Prevents adjacent foes from switching out
+///
+/// TODO: onFoeTrapPokemon handler not yet implemented
+/// TODO: Needs pokemon.hasAbility(), pokemon.isAdjacent(), effectState.target, pokemon.tryTrap()
+/// When implemented, should:
+/// 1. Check if pokemon doesn't have Shadow Tag and is adjacent to ability holder
+/// 2. Call pokemon.tryTrap(true) to prevent switching
 pub fn on_foe_trap_pokemon(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onFoeMaybeTrapPokemon(...)
+/// onFoeMaybeTrapPokemon(pokemon, source)
+/// Marks adjacent foes as potentially trapped
+///
+/// TODO: onFoeMaybeTrapPokemon handler not yet implemented
+/// TODO: Needs source parameter, pokemon.isAdjacent(), pokemon.hasAbility(), pokemon.maybeTrapped
+/// When implemented, should:
+/// 1. Default source to effectState.target if not provided
+/// 2. Check if pokemon is adjacent to source
+/// 3. If pokemon doesn't have Shadow Tag, set maybeTrapped = true
 pub fn on_foe_maybe_trap_pokemon(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
