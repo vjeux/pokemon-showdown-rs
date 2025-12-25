6325209a5fb32fad79f230dda7dee563592da725
Developer <dev@example.com>
2025-12-25 16:10:18 +0000

    Document wellbakedbody ability implementation requirements
    - Immune to Fire moves, raises Defense by 2 stages
    - Needs onTryHit handler
    - Returns null to block Fire moves
    - Encountered alphabetically after waterv eil

--- src/data/ability_callbacks/wellbakedbody.rs
+++ src/data/ability_callbacks/wellbakedbody.rs
@@ -28,9 +28,18 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryHit(...)
-pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
+/// onTryHit(target, source, move)
+/// Immune to Fire moves, raises Defense by 2 stages when hit
+///
+/// TODO: onTryHit handler not yet implemented
+/// When implemented, should:
+/// 1. Check if target !== source && move.type === 'Fire'
+/// 2. Call this.boost({ def: 2 }) to raise Defense
+/// 3. If boost fails (already maxed), show immune message
+/// 4. Return null to nullify the Fire move
+pub fn on_try_hit(_battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
+    // Requires onTryHit handler
     AbilityHandlerResult::Undefined
 }
 
