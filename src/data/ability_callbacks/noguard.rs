1484e003360d7d9b84e0178f518f209238f2204b
Developer <dev@example.com>
2025-12-25 16:43:10 +0000

    Document noguard - ensures all moves hit, bypasses invulnerability

--- src/data/ability_callbacks/noguard.rs
+++ src/data/ability_callbacks/noguard.rs
@@ -30,19 +30,30 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onAnyInvulnerabilityPriority(...)
-pub fn on_any_invulnerability_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_ANY_INVULNERABILITY_PRIORITY: i32 = 1;
 
-/// onAnyInvulnerability(...)
+/// onAnyInvulnerability(target, source, move)
+/// Ensures moves bypass invulnerability (Fly, Dig, etc.)
+///
+/// TODO: onAnyInvulnerability handler not yet implemented
+/// TODO: Needs effectState.target access
+/// When implemented, should:
+/// 1. Check if move exists and (source or target is this pokemon)
+/// 2. Return 0 to bypass invulnerability
 pub fn on_any_invulnerability(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAnyAccuracy(...)
+/// onAnyAccuracy(accuracy, target, source, move)
+/// Ensures all moves involving this pokemon always hit
+///
+/// TODO: onAnyAccuracy handler not yet implemented
+/// TODO: Needs effectState.target access
+/// When implemented, should:
+/// 1. Check if move exists and (source or target is this pokemon)
+/// 2. Return true to guarantee hit
+/// 3. Otherwise return accuracy unchanged
 pub fn on_any_accuracy(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
