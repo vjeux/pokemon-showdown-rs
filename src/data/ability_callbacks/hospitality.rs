4918ea2b3ed63d8ad19d7da4e6d7f123c378789b
Developer <dev@example.com>
2025-12-25 16:15:39 +0000

    Document hospitality - heals ally HP

--- src/data/ability_callbacks/hospitality.rs
+++ src/data/ability_callbacks/hospitality.rs
@@ -1,24 +1,4 @@
-//! Hospitality Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	hospitality: {
-//! 		onSwitchInPriority: -2,
-//! 		onStart(pokemon) {
-//! 			for (const ally of pokemon.adjacentAllies()) {
-//! 				this.heal(ally.baseMaxhp / 4, ally, pokemon);
-//! 			}
-//! 		},
-//! 		flags: {},
-//! 		name: "Hospitality",
-//! 		rating: 0,
-//! 		num: 299,
-//! 	},
-//! ```
+//! Hospitality Ability - Heals ally HP on switch-in
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -26,15 +6,7 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSwitchInPriority(...)
-pub fn on_switch_in_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// TODO: Requires onStart with allies()
+pub fn on_start(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onStart(...)
-pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
