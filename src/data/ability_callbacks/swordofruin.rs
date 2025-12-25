58ac502f013fc2efcf5b47e6d0100cdda5c3c824
Developer <dev@example.com>
2025-12-25 16:48:46 +0100

    Document swordofruin ability implementation requirements
    - Added detailed TODOs for onStart and onAnyModifyDef handlers
    - Reduces all foes Defense by 25%
    - Requires onAnyModifyDef handler and move.ruinedDef tracking
    - Encountered alphabetically after synchronize

--- src/data/ability_callbacks/swordofruin.rs
+++ src/data/ability_callbacks/swordofruin.rs
@@ -32,13 +32,27 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
+/// onStart(pokemon)
+/// Announces Sword of Ruin when sent out
+///
+/// TODO: Requires suppressingAbility check
+/// When implemented, should:
+/// 1. Check if this.suppressingAbility(pokemon) and return if true
+/// 2. Add ability announcement: this.add('-ability', pokemon, 'Sword of Ruin')
 pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAnyModifyDef(...)
+/// onAnyModifyDef(def, target, source, move)
+/// Reduces all Pokemon's Defense by 25% except those with Sword of Ruin
+///
+/// TODO: onAnyModifyDef handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Get abilityHolder from effectState.target
+/// 2. Check if target.hasAbility('Sword of Ruin'), return if true
+/// 3. Track move.ruinedDef to ensure only one Sword of Ruin applies per move
+/// 4. Return chainModify(0.75) to reduce Defense by 25%
 pub fn on_any_modify_def(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
