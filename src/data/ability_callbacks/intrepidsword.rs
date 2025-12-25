d95a2c105ac97dcba8bb834cdd6d1e89a4267a7d
Developer <dev@example.com>
2025-12-25 16:23:34 +0000

    Document intrepidsword - one-time Attack boost

--- src/data/ability_callbacks/intrepidsword.rs
+++ src/data/ability_callbacks/intrepidsword.rs
@@ -25,7 +25,14 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
+/// onStart(pokemon)
+/// Boosts Attack by 1 stage on first switch-in only
+///
+/// TODO: onStart handler exists but needs pokemon.swordBoost field
+/// When implemented, should:
+/// 1. Check if pokemon.swordBoost is set (return if true)
+/// 2. Set pokemon.swordBoost = true
+/// 3. Call this.boost({ atk: 1 }, pokemon)
 pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
