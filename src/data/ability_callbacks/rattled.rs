83f4897f92b38bfeeafec0efe465dd2ab777ca63
Developer <dev@example.com>
2025-12-25 17:01:53 +0000

    Document rattled - Speed boost from Dark/Bug/Ghost or Intimidate

--- src/data/ability_callbacks/rattled.rs
+++ src/data/ability_callbacks/rattled.rs
@@ -30,13 +30,27 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onDamagingHit(...)
+/// onDamagingHit(damage, target, source, move)
+/// Boosts Speed when hit by Dark, Bug, or Ghost-type moves
+///
+/// TODO: onDamagingHit handler not yet implemented
+/// TODO: Needs move.type, boost()
+/// When implemented, should:
+/// 1. If move is Dark, Bug, or Ghost-type
+/// 2. Boost Speed by 1 stage
 pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAfterBoost(...)
+/// onAfterBoost(boost, target, source, effect)
+/// Boosts Speed when affected by Intimidate
+///
+/// TODO: onAfterBoost handler not yet implemented
+/// TODO: Needs effect.name, boost.atk, boost()
+/// When implemented, should:
+/// 1. If effect is Intimidate and boost.atk exists
+/// 2. Boost Speed by 1 stage
 pub fn on_after_boost(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
