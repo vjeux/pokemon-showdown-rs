a86d9070c94eab4f510422184264964e0e97609b
Developer <dev@example.com>
2025-12-25 15:45:40 +0000

    Document solarpower ability implementation requirements
    - Added detailed TODOs for onModifySpA and onWeather handlers
    - Boosts Sp. Atk by 1.5x in sun, takes 1/8 damage per turn
    - Added ON_MODIFY_SP_A_PRIORITY constant
    - Encountered alphabetically after snowcloak

--- src/data/ability_callbacks/solarpower.rs
+++ src/data/ability_callbacks/solarpower.rs
@@ -32,19 +32,29 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifySpAPriority(...)
-pub fn on_modify_sp_a_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+/// onModifySpAPriority: 5
+pub const ON_MODIFY_SP_A_PRIORITY: i32 = 5;
 
-/// onModifySpA(...)
+/// onModifySpA(spa, pokemon)
+/// Boosts Special Attack by 1.5x in sunny weather
+///
+/// TODO: onModifySpA handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Check if pokemon.effectiveWeather() is 'sunnyday' or 'desolateland'
+/// 2. Return chainModify(1.5) which is ChainModify(6144, 4096)
 pub fn on_modify_sp_a(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onWeather(...)
+/// onWeather(target, source, effect)
+/// Damages the Pokemon by 1/8 max HP each turn in sun
+///
+/// TODO: onWeather handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Check if target has Utility Umbrella item (return early if so)
+/// 2. Check if effect.id is 'sunnyday' or 'desolateland'
+/// 3. Call battle.damage(target.baseMaxhp / 8, target, target)
 pub fn on_weather(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
