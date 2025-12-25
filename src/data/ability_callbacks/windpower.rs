25c27d26287be4c805a4fa3b48a834612c66f4c2
Developer <dev@example.com>
2025-12-25 16:11:08 +0000

    Document windpower ability - gets charged by wind moves

--- src/data/ability_callbacks/windpower.rs
+++ src/data/ability_callbacks/windpower.rs
@@ -1,30 +1,4 @@
-//! Wind Power Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	windpower: {
-//! 		onDamagingHitOrder: 1,
-//! 		onDamagingHit(damage, target, source, move) {
-//! 			if (move.flags['wind']) {
-//! 				target.addVolatile('charge');
-//! 			}
-//! 		},
-//! 		onSideConditionStart(side, source, sideCondition) {
-//! 			const pokemon = this.effectState.target;
-//! 			if (sideCondition.id === 'tailwind') {
-//! 				pokemon.addVolatile('charge');
-//! 			}
-//! 		},
-//! 		flags: {},
-//! 		name: "Wind Power",
-//! 		rating: 1,
-//! 		num: 277,
-//! 	},
-//! ```
+//! Wind Power Ability - Gets charged when hit by wind moves
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -32,21 +6,17 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onDamagingHitOrder(...)
-pub fn on_damaging_hit_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub const ON_DAMAGING_HIT_ORDER: i32 = 1;
+
+/// onDamagingHit, onSideConditionStart
+/// Gets Charge status when hit by wind moves or Tailwind is set
+///
+/// TODO: Needs volatile status system (addVolatile charge)
+/// TODO: Needs side condition system for Tailwind detection
+pub fn on_damaging_hit(_battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
 
-/// onDamagingHit(...)
-pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_side_condition_start(_battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onSideConditionStart(...)
-pub fn on_side_condition_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
