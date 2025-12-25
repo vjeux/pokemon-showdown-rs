37947440ee7d781ebf7a1577c527c249a1cc3d60
Developer <dev@example.com>
2025-12-25 16:42:27 +0000

    Document neutralizinggas - suppresses all abilities field-wide

--- src/data/ability_callbacks/neutralizinggas.rs
+++ src/data/ability_callbacks/neutralizinggas.rs
@@ -79,19 +79,44 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSwitchInPriority(...)
-pub fn on_switch_in_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_SWITCH_IN_PRIORITY: i32 = 2;
 
-/// onSwitchIn(...)
+/// onSwitchIn(pokemon)
+/// Suppresses all abilities on the field (implemented in Pokemon#ignoringAbility)
+///
+/// TODO: onSwitchIn handler not yet implemented
+/// TODO: Very complex - suppresses abilities field-wide
+/// TODO: Needs pokemon.abilityState.ending, getAllActive(), hasItem('Ability Shield')
+/// TODO: Needs volatiles['commanding'], illusion field, volatiles['slowstart']
+/// TODO: Needs singleEvent system, getAbility()
+/// When implemented, should:
+/// 1. Add ability message
+/// 2. Set abilityState.ending = false
+/// 3. Loop through all active pokemon:
+///    - Skip if has Ability Shield item
+///    - Skip if has commanding volatile
+///    - End Illusion if present
+///    - Remove slowstart volatile
+///    - End strong weather abilities (desolateland/primordialsea/deltastream)
 pub fn on_switch_in(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onEnd(...)
+/// onEnd(source)
+/// Re-activates suppressed abilities when Neutralizing Gas ends
+///
+/// TODO: onEnd handler not yet implemented
+/// TODO: Needs source.transformed check, getAllActive(), speedSort()
+/// TODO: Needs ability.flags['cantsuppress'], singleEvent('Start')
+/// When implemented, should:
+/// 1. Return if source.transformed
+/// 2. Check if another Neutralizing Gas is still active
+/// 3. Add end message
+/// 4. Mark abilityState.ending = true
+/// 5. Speed-sort all active pokemon
+/// 6. Fire Start event for each pokemon's ability (except source and cantsuppress)
+/// 7. Special handling for gluttony ability
 pub fn on_end(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
