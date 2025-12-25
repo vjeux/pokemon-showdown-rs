908979fadaff398001517005ed14913c96ed09f5
Developer <dev@example.com>
2025-12-25 13:56:17 +0000

    Implement reckless ability - boosts recoil moves by 1.2x

--- src/data/ability_callbacks/reckless.rs
+++ src/data/ability_callbacks/reckless.rs
@@ -27,15 +27,16 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onBasePowerPriority(...)
-pub fn on_base_power_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_BASE_POWER_PRIORITY: i32 = 23;
 
-/// onBasePower(...)
-pub fn on_base_power(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onBasePower(basePower, attacker, defender, move)
+/// Boosts recoil moves by 1.2x
+pub fn on_base_power(_base_power: u32, _attacker: &Pokemon, _defender: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (move.recoil || move.hasCrashDamage)
+    if move_.recoil.is_some() || move_.has_crash_damage {
+        // return this.chainModify([4915, 4096]);
+        return AbilityHandlerResult::ChainModify(4915, 4096); // ~1.2x
+    }
     AbilityHandlerResult::Undefined
 }
 
