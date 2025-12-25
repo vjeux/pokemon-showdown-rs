2263516e6346c9e61302b36aed21e2a59a46e9f1
Developer <dev@example.com>
2025-12-25 16:26:07 +0000

    Document leafguard - status immunity in sun

--- src/data/ability_callbacks/leafguard.rs
+++ src/data/ability_callbacks/leafguard.rs
@@ -34,13 +34,29 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSetStatus(...)
+/// onSetStatus(status, target, source, effect)
+/// Prevents status conditions in sunny weather
+///
+/// TODO: onSetStatus exists but needs weather system
+/// TODO: Needs target.effectiveWeather() method
+/// When implemented, should:
+/// 1. Check if effectiveWeather is 'sunnyday' or 'desolateland'
+/// 2. Add immune message if effect has status
+/// 3. Return false to prevent status
 pub fn on_set_status(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onTryAddVolatile(...)
+/// onTryAddVolatile(status, target)
+/// Prevents yawn in sunny weather
+///
+/// TODO: onTryAddVolatile not yet implemented
+/// TODO: Needs target.effectiveWeather() method
+/// When implemented, should:
+/// 1. Check if status.id === 'yawn'
+/// 2. Check if effectiveWeather is 'sunnyday' or 'desolateland'
+/// 3. Add immune message and return null
 pub fn on_try_add_volatile(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
