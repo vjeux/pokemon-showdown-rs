3fae557c14cc7c62d981f476d02389d03adeedc6
Developer <dev@example.com>
2025-12-25 11:50:37 +0100

    Implement effectspore ability - randomly inflicts sleep/paralysis/poison on contact

--- src/data/ability_callbacks/batch_1.rs
+++ src/data/ability_callbacks/batch_1.rs
@@ -2513,9 +2513,19 @@ pub mod eartheater {
 pub mod effectspore {
     use super::*;
 
-    /// onDamagingHit(...)
-    pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
+    /// onDamagingHit(damage, target, source, move)
+    pub fn on_damaging_hit(battle: &mut Battle, _damage: u32, _target: &Pokemon, source: &mut Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+        let source_ref = (source.side_index, source.position);
+        if battle.check_move_makes_contact(&move_.id, source_ref) && source.status.is_empty() && source.run_status_immunity("powder") {
+            let r = battle.random(100);
+            if r < 11 {
+                source.set_status(ID::new("slp"));
+            } else if r < 21 {
+                source.set_status(ID::new("par"));
+            } else if r < 30 {
+                source.set_status(ID::new("psn"));
+            }
+        }
         AbilityHandlerResult::Undefined
     }
 }
