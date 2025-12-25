a4c1fefc5e553f371c94d018a595f76ee06484f5
Developer <dev@example.com>
2025-12-25 15:52:19 +0000

    Implement Tangling Hair ability
    - Lowers Speed of attackers that make contact by 1 stage
    - Uses onDamagingHit handler with contact check
    - Matches JS implementation 1-to-1
    - Encountered alphabetically after tangledfeet
    - All tests passing (46 tests)

--- src/data/ability_callbacks/tanglinghair.rs
+++ src/data/ability_callbacks/tanglinghair.rs
@@ -26,9 +26,18 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onDamagingHit(...)
-pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onDamagingHit(damage, target, source, move)
+/// Lowers Speed of attackers that make contact
+pub fn on_damaging_hit(battle: &mut Battle, _damage: u32, target: &Pokemon, source: &mut Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (this.checkMoveMakesContact(move, source, target, true))
+    let source_ref = (source.side_index, source.position);
+    if battle.check_move_makes_contact(&move_.id, source_ref) {
+        // this.add('-ability', target, 'Tangling Hair');
+        battle.add("-ability", &[Arg::Pokemon(target), Arg::Str("Tangling Hair")]);
+        // this.boost({ spe: -1 }, source, target, null, true);
+        let target_ref = (target.side_index, target.position);
+        battle.boost(&[("spe", -1)], source_ref, Some(target_ref), None);
+    }
     AbilityHandlerResult::Undefined
 }
 
