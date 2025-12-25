2f4e95e732634aa8b2c7f73be3b9ad7f6b4f5c21
Developer <dev@example.com>
2025-12-25 17:17:18 +0000

    Document skilllink - max hits for multi-hit moves

--- src/data/ability_callbacks/skilllink.rs
+++ src/data/ability_callbacks/skilllink.rs
@@ -28,7 +28,14 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyMove(...)
+/// onModifyMove(move)
+/// Makes multi-hit moves always hit the maximum number of times
+///
+/// TODO: onModifyMove handler not yet implemented
+/// TODO: Needs move.multihit array manipulation, move.multiaccuracy deletion
+/// When implemented, should:
+/// 1. If move.multihit is an array with length, set it to the maximum value (move.multihit[1])
+/// 2. Delete move.multiaccuracy to ensure all hits land
 pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
