d50f29d457d2a3ed1782cb922f8d754077f89083
Developer <dev@example.com>
2025-12-25 11:58:14 +0100

    Implement flashfire ability - absorbs Fire moves and powers up Fire attacks

--- src/data/ability_callbacks/batch_1.rs
+++ src/data/ability_callbacks/batch_1.rs
@@ -2965,45 +2965,42 @@ pub mod flareboost {
 pub mod flashfire {
     use super::*;
 
-    /// onTryHit(...)
-    pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
+    /// onTryHit(target, source, move)
+    pub fn on_try_hit(battle: &mut Battle, target: &mut Pokemon, source: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+        let target_ref = (target.side_index, target.position);
+        let source_ref = (source.side_index, source.position);
+        if target_ref != source_ref && move_.move_type == "Fire" {
+            // Fire move hitting this pokemon - gain the boost
+            if !target.add_volatile(ID::new("flashfire")) {
+                battle.add("-immune", &[Arg::Pokemon(target), Arg::Str("[from] ability: Flash Fire")]);
+            }
+            return AbilityHandlerResult::Null;
+        }
         AbilityHandlerResult::Undefined
     }
 
-    /// onEnd(...)
-    pub fn on_end(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
+    /// onEnd(pokemon)
+    pub fn on_end(_battle: &mut Battle, pokemon: &mut Pokemon) -> AbilityHandlerResult {
+        pokemon.remove_volatile(&ID::new("flashfire"));
         AbilityHandlerResult::Undefined
     }
 
-    /// onStart(...)
-    pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
+    pub const ON_MODIFY_ATK_PRIORITY: i32 = 5;
+    pub const ON_MODIFY_SPA_PRIORITY: i32 = 5;
+
+    /// onModifyAtk(atk, attacker, defender, move)
+    pub fn on_modify_atk(_atk: u32, attacker: &Pokemon, _defender: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+        if move_.move_type == "Fire" && attacker.has_ability(&["flashfire"]) && attacker.has_volatile(&ID::new("flashfire")) {
+            return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
+        }
         AbilityHandlerResult::Undefined
     }
 
-    /// onModifyAtkPriority(...)
-    pub fn on_modify_atk_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
-        AbilityHandlerResult::Undefined
-    }
-
-    /// onModifyAtk(...)
-    pub fn on_modify_atk(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
-        AbilityHandlerResult::Undefined
-    }
-
-    /// onModifySpAPriority(...)
-    pub fn on_modify_sp_a_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
-        AbilityHandlerResult::Undefined
-    }
-
-    /// onModifySpA(...)
-    pub fn on_modify_sp_a(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
+    /// onModifySpA(spa, attacker, defender, move)
+    pub fn on_modify_spa(_spa: u32, attacker: &Pokemon, _defender: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+        if move_.move_type == "Fire" && attacker.has_ability(&["flashfire"]) && attacker.has_volatile(&ID::new("flashfire")) {
+            return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
+        }
         AbilityHandlerResult::Undefined
     }
 
@@ -3011,7 +3008,17 @@ pub mod flashfire {
     pub mod condition {
         use super::*;
 
-        // TODO: Implement condition handlers
+        /// onStart(target)
+        pub fn on_start(battle: &mut Battle, target: &Pokemon) -> AbilityHandlerResult {
+            battle.add("-start", &[Arg::Pokemon(target), Arg::Str("ability: Flash Fire")]);
+            AbilityHandlerResult::Undefined
+        }
+
+        /// onEnd(target)
+        pub fn on_end(battle: &mut Battle, target: &Pokemon) -> AbilityHandlerResult {
+            battle.add("-end", &[Arg::Pokemon(target), Arg::Str("ability: Flash Fire"), Arg::Str("[silent]")]);
+            AbilityHandlerResult::Undefined
+        }
     }
 }
 
