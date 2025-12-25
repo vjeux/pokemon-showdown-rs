e308465843b51c201b7ee7a87577cbcf051d276e
Developer <dev@example.com>
2025-12-25 17:20:14 +0000

    Document stalwart - ignores target redirection

--- src/data/ability_callbacks/stalwart.rs
+++ src/data/ability_callbacks/stalwart.rs
@@ -25,13 +25,16 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyMovePriority(...)
-pub fn on_modify_move_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_MODIFY_MOVE_PRIORITY: i32 = 1;
 
-/// onModifyMove(...)
+/// onModifyMove(move)
+/// Ignores target redirection (same as Propeller Tail)
+///
+/// TODO: onModifyMove handler not yet implemented
+/// TODO: Needs move.tracksTarget, move.target
+/// When implemented, should:
+/// 1. Set move.tracksTarget = true if move.target is not 'scripted'
+/// 2. Main implementation is in Battle#getTarget
 pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
