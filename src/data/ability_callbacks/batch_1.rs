964a5e4ecfa9d333dbbd2f12bdce8195157daa33
Developer <dev@example.com>
2025-12-25 11:59:31 +0100

    Implement friendguard ability - reduces damage to allies by 25%

--- src/data/ability_callbacks/batch_1.rs
+++ src/data/ability_callbacks/batch_1.rs
@@ -3328,9 +3328,16 @@ pub mod forewarn {
 pub mod friendguard {
     use super::*;
 
-    /// onAnyModifyDamage(...)
-    pub fn on_any_modify_damage(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
+    /// onAnyModifyDamage(damage, source, target, move)
+    /// Reduces damage to allies by 25%
+    pub fn on_any_modify_damage(_battle: &Battle, _damage: u32, _source: &Pokemon, target: &Pokemon, _move_: &MoveDef, effect_holder: &Pokemon) -> AbilityHandlerResult {
+        // If target is an ally of the effect holder (but not the holder itself)
+        let holder_ref = (effect_holder.side_index, effect_holder.position);
+        let target_ref = (target.side_index, target.position);
+        if target_ref != holder_ref && target.side_index == effect_holder.side_index {
+            // 0.75x = 3072/4096
+            return AbilityHandlerResult::ChainModify(3072, 4096);
+        }
         AbilityHandlerResult::Undefined
     }
 }
