3b792456c0653cc4b97471a4beb3eec1831a7268
Developer <dev@example.com>
2025-12-25 16:46:59 +0000

    Document overcoat - immune to sandstorm/hail/powder

--- src/data/ability_callbacks/overcoat.rs
+++ src/data/ability_callbacks/overcoat.rs
@@ -30,19 +30,28 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onImmunity(...)
+/// onImmunity(type, pokemon)
+/// Grants immunity to sandstorm, hail, and powder
+///
+/// TODO: onImmunity handler not yet implemented
+/// When implemented, should:
+/// 1. Check if type is sandstorm, hail, or powder
+/// 2. Return false to grant immunity
 pub fn on_immunity(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onTryHitPriority(...)
-pub fn on_try_hit_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_TRY_HIT_PRIORITY: i32 = 1;
 
-/// onTryHit(...)
+/// onTryHit(target, source, move)
+/// Blocks powder moves
+///
+/// TODO: onTryHit handler not yet implemented
+/// TODO: Needs move.flags['powder'], dex.getImmunity()
+/// When implemented, should:
+/// 1. Check if move has powder flag, target !== source, and getImmunity('powder', target)
+/// 2. Add immune message and return null
 pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
