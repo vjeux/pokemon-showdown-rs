bb91bf1671e0c6fe897245b269ec914b476bdf03
Developer <dev@example.com>
2025-12-25 16:13:49 +0100

    Implement baddreams ability
    Damages sleeping opponents at the end of each turn.
    Deals 1/8 of target's max HP to any sleeping foe or foe with Comatose ability.

--- src/data/ability_callbacks/baddreams.rs
+++ src/data/ability_callbacks/baddreams.rs
@@ -30,21 +30,34 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onResidualOrder(...)
-pub fn on_residual_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+/// onResidualOrder: 28
+pub const ON_RESIDUAL_ORDER: i32 = 28;
 
-/// onResidualSubOrder(...)
-pub fn on_residual_sub_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+/// onResidualSubOrder: 2
+pub const ON_RESIDUAL_SUB_ORDER: i32 = 2;
 
-/// onResidual(...)
-pub fn on_residual(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onResidual(pokemon)
+pub fn on_residual(battle: &mut Battle, pokemon: &Pokemon) -> AbilityHandlerResult {
+    // if (!pokemon.hp) return;
+    if pokemon.hp == 0 {
+        return AbilityHandlerResult::Undefined;
+    }
+
+    // for (const target of pokemon.foes())
+    let foe_side_index = if pokemon.side_index == 0 { 1 } else { 0 };
+    let foes = pokemon.foes_stub(foe_side_index, false);
+
+    for (target_side_idx, target_pos) in foes {
+        let target = &battle.sides[target_side_idx].pokemon[target_pos];
+
+        // if (target.status === 'slp' || target.hasAbility('comatose'))
+        if target.status.as_str() == "slp" || target.ability.as_str() == "comatose" {
+            // this.damage(target.baseMaxhp / 8, target, pokemon);
+            let damage = target.base_maxhp / 8;
+            battle.damage(damage, (target_side_idx, target_pos), Some((pokemon.side_index, pokemon.position)), None);
+        }
+    }
+
     AbilityHandlerResult::Undefined
 }
 
