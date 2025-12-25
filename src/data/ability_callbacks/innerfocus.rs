265188f517e19c6bd242f7f1994e6205797d7239
Developer <dev@example.com>
2025-12-25 17:54:49 +0000

    Implement innerfocus - Partially implemented, blocks Intimidate (flinch prevention needs volatile status system)

--- src/data/ability_callbacks/innerfocus.rs
+++ src/data/ability_callbacks/innerfocus.rs
@@ -38,19 +38,30 @@ use super::{AbilityHandlerResult, Status, Effect};
 /// 2. Return null to prevent the volatile status
 pub fn on_try_add_volatile(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
+    // Requires volatile status system
     AbilityHandlerResult::Undefined
 }
 
 /// onTryBoost(boost, target, source, effect)
 /// Prevents Attack reduction from Intimidate
-///
-/// TODO: onTryBoost handler not yet implemented in battle system
-/// When implemented, should:
-/// 1. Check if effect.name === 'Intimidate' && boost.atk exists
-/// 2. Delete boost.atk to prevent the reduction
-/// 3. Add fail message
-pub fn on_try_boost(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_try_boost(battle: &mut Battle, boost: &mut std::collections::HashMap<String, i8>, target: &Pokemon, source: Option<&Pokemon>, effect_id: &str, has_secondaries: bool) -> AbilityHandlerResult {
+    // if (effect.name === 'Intimidate' && boost.atk)
+    if effect_id == "intimidate" {
+        if let Some(&atk_boost) = boost.get("atk") {
+            if atk_boost < 0 {
+                // delete boost.atk;
+                boost.remove("atk");
+                // this.add('-fail', target, 'unboost', 'Attack', '[from] ability: Inner Focus', `[of] ${target}`);
+                battle.add("-fail", &[
+                    Arg::Pokemon(target),
+                    Arg::Str("unboost"),
+                    Arg::Str("Attack"),
+                    Arg::Str("[from] ability: Inner Focus"),
+                    Arg::Str(&format!("[of] {}", target.name))
+                ]);
+            }
+        }
+    }
     AbilityHandlerResult::Undefined
 }
 
