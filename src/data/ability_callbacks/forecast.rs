7eb56dfbfd96f4caf723318858b6f98247eea79c
Developer <dev@example.com>
2025-12-25 16:14:06 +0000

    Document forecast - Castform weather forme changes

--- src/data/ability_callbacks/forecast.rs
+++ src/data/ability_callbacks/forecast.rs
@@ -1,46 +1,4 @@
-//! Forecast Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	forecast: {
-//! 		onSwitchInPriority: -2,
-//! 		onStart(pokemon) {
-//! 			this.singleEvent('WeatherChange', this.effect, this.effectState, pokemon);
-//! 		},
-//! 		onWeatherChange(pokemon) {
-//! 			if (pokemon.baseSpecies.baseSpecies !== 'Castform' || pokemon.transformed) return;
-//! 			let forme = null;
-//! 			switch (pokemon.effectiveWeather()) {
-//! 			case 'sunnyday':
-//! 			case 'desolateland':
-//! 				if (pokemon.species.id !== 'castformsunny') forme = 'Castform-Sunny';
-//! 				break;
-//! 			case 'raindance':
-//! 			case 'primordialsea':
-//! 				if (pokemon.species.id !== 'castformrainy') forme = 'Castform-Rainy';
-//! 				break;
-//! 			case 'hail':
-//! 			case 'snowscape':
-//! 				if (pokemon.species.id !== 'castformsnowy') forme = 'Castform-Snowy';
-//! 				break;
-//! 			default:
-//! 				if (pokemon.species.id !== 'castform') forme = 'Castform';
-//! 				break;
-//! 			}
-//! 			if (pokemon.isActive && forme) {
-//! 				pokemon.formeChange(forme, this.effect, false, '0', '[msg]');
-//! 			}
-//! 		},
-//! 		flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1 },
-//! 		name: "Forecast",
-//! 		rating: 2,
-//! 		num: 59,
-//! 	},
-//! ```
+//! Forecast Ability - Castform changes forme with weather
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -48,21 +6,12 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSwitchInPriority(...)
-pub fn on_switch_in_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub const ON_SWITCH_IN_PRIORITY: i32 = 1;
+
+/// TODO: Requires forme change, weather system, effectiveWeather()
+pub fn on_start(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onStart(...)
-pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_weather_change(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onWeatherChange(...)
-pub fn on_weather_change(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
