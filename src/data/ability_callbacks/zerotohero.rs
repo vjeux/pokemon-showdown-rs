996a51b822f515f40dcb41d998f7129bc858972f
Developer <dev@example.com>
2025-12-25 16:11:08 +0000

    Document zerotohero ability - Palafin Hero transformation

--- src/data/ability_callbacks/zerotohero.rs
+++ src/data/ability_callbacks/zerotohero.rs
@@ -1,32 +1,4 @@
-//! Zero to Hero Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	zerotohero: {
-//! 		onSwitchOut(pokemon) {
-//! 			if (pokemon.baseSpecies.baseSpecies !== 'Palafin') return;
-//! 			if (pokemon.species.forme !== 'Hero') {
-//! 				pokemon.formeChange('Palafin-Hero', this.effect, true);
-//! 				pokemon.heroMessageDisplayed = false;
-//! 			}
-//! 		},
-//! 		onSwitchIn(pokemon) {
-//! 			if (pokemon.baseSpecies.baseSpecies !== 'Palafin') return;
-//! 			if (!pokemon.heroMessageDisplayed && pokemon.species.forme === 'Hero') {
-//! 				this.add('-activate', pokemon, 'ability: Zero to Hero');
-//! 				pokemon.heroMessageDisplayed = true;
-//! 			}
-//! 		},
-//! 		flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, cantsuppress: 1, notransform: 1 },
-//! 		name: "Zero to Hero",
-//! 		rating: 5,
-//! 		num: 278,
-//! 	},
-//! ```
+//! Zero to Hero Ability - Palafin changes to Hero forme when switching out
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -34,15 +6,17 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSwitchOut(...)
-pub fn on_switch_out(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onSwitchOut, onSwitchIn
+/// Transforms Palafin to Hero forme on switch
+///
+/// TODO: Needs onSwitchOut handler
+/// TODO: Needs onSwitchIn handler  
+/// TODO: Needs forme change system (pokemon.formeChange)
+/// TODO: Needs pokemon.heroMessageDisplayed tracking
+pub fn on_switch_out(_battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
 
-/// onSwitchIn(...)
-pub fn on_switch_in(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_switch_in(_battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
