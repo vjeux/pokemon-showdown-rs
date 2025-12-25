09389ec1c85a36db5a94a18027a31497a9a03e15
Developer <dev@example.com>
2025-12-25 16:15:39 +0000

    Document harvest - restores Berry

--- src/data/ability_callbacks/harvest.rs
+++ src/data/ability_callbacks/harvest.rs
@@ -1,29 +1,4 @@
-//! Harvest Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	harvest: {
-//! 		onResidualOrder: 28,
-//! 		onResidualSubOrder: 2,
-//! 		onResidual(pokemon) {
-//! 			if (this.field.isWeather(['sunnyday', 'desolateland']) || this.randomChance(1, 2)) {
-//! 				if (pokemon.hp && !pokemon.item && this.dex.items.get(pokemon.lastItem).isBerry) {
-//! 					pokemon.setItem(pokemon.lastItem);
-//! 					pokemon.lastItem = '';
-//! 					this.add('-item', pokemon, pokemon.getItem(), '[from] ability: Harvest');
-//! 				}
-//! 			}
-//! 		},
-//! 		flags: {},
-//! 		name: "Harvest",
-//! 		rating: 2.5,
-//! 		num: 139,
-//! 	},
-//! ```
+//! Harvest Ability - Restores Berry at end of turn
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -31,21 +6,8 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onResidualOrder(...)
-pub fn on_residual_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// TODO: Requires onResidualOrder, item restoration, weather checking
+pub const ON_RESIDUAL_ORDER: i32 = 28;
+pub fn on_residual(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onResidualSubOrder(...)
-pub fn on_residual_sub_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onResidual(...)
-pub fn on_residual(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
