ef4c7ecfc02f587a7dc0a8a992d95a3a6afa0a9f
Developer <dev@example.com>
2025-12-25 16:41:45 +0000

    Document neuroforce - boosts super-effective damage 1.25x

--- src/data/ability_callbacks/neuroforce.rs
+++ src/data/ability_callbacks/neuroforce.rs
@@ -25,7 +25,14 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyDamage(...)
+/// onModifyDamage(damage, source, target, move)
+/// Boosts super-effective damage by 1.25x
+///
+/// TODO: onModifyDamage handler not yet implemented
+/// TODO: Needs target.getMoveHitData(move).typeMod
+/// When implemented, should:
+/// 1. Check if move && target.getMoveHitData(move).typeMod > 0 (super-effective)
+/// 2. Return chainModify(5120, 4096) for 1.25x multiplier
 pub fn on_modify_damage(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
