1c57f4dd851d8e810f0e90386b50d99147d39b94
Developer <dev@example.com>
2025-12-25 17:12:16 +0000

    Document screencleaner - removes screens on switch-in

--- src/data/ability_callbacks/screencleaner.rs
+++ src/data/ability_callbacks/screencleaner.rs
@@ -34,7 +34,16 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
+/// onStart(pokemon)
+/// Removes screens (Reflect, Light Screen, Aurora Veil) from both sides
+///
+/// TODO: onStart handler not yet implemented
+/// TODO: Needs pokemon.side, pokemon.side.foeSidesWithConditions(), side.getSideCondition(), side.removeSideCondition(), battle.add()
+/// When implemented, should:
+/// 1. Loop through ['reflect', 'lightscreen', 'auroraveil'] side conditions
+/// 2. Check both own side and foe sides
+/// 3. Remove any active screen conditions
+/// 4. Show activate message once if any screens removed
 pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
