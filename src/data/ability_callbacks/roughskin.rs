8e0a1d4e488f9d6736d2bc61716a4bb270f0f0af
Developer <dev@example.com>
2025-12-25 19:47:25 +0100

    Implement roughskin - Damages attacker by 1/8 max HP on contact

--- src/data/ability_callbacks/roughskin.rs
+++ src/data/ability_callbacks/roughskin.rs
@@ -30,14 +30,14 @@ pub const ON_DAMAGING_HIT_ORDER: i32 = 1;
 
 /// onDamagingHit(damage, target, source, move)
 /// Damages attacker by 1/8 of their max HP if they make contact
-///
-/// TODO: onDamagingHit handler not yet implemented
-/// TODO: Needs checkMoveMakesContact(), source.baseMaxhp, battle.damage()
-/// When implemented, should:
-/// 1. Check if the move makes contact with target
-/// 2. If contact made, damage the source for 1/8 of their baseMaxhp
-pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_damaging_hit(battle: &mut Battle, _damage: u32, _target: &Pokemon, source: &mut Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (this.checkMoveMakesContact(move, source, target, true))
+    let source_ref = (source.side_index, source.position);
+    if battle.check_move_makes_contact(&move_.id, source_ref) {
+        // this.damage(source.baseMaxhp / 8, source, target);
+        let damage_amount = source.base_maxhp / 8;
+        battle.damage(damage_amount, source_ref, Some(source_ref), None);
+    }
     AbilityHandlerResult::Undefined
 }
 
