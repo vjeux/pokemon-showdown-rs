6c8ec416f1e3af83bc2ec9b2b5777297d85bbb76
Developer <dev@example.com>
2025-12-25 16:15:39 +0000

    Document guarddog - immune to Intimidate

--- src/data/ability_callbacks/guarddog.rs
+++ src/data/ability_callbacks/guarddog.rs
@@ -1,30 +1,4 @@
-//! Guard Dog Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	guarddog: {
-//! 		onDragOutPriority: 1,
-//! 		onDragOut(pokemon) {
-//! 			this.add('-activate', pokemon, 'ability: Guard Dog');
-//! 			return null;
-//! 		},
-//! 		onTryBoostPriority: 2,
-//! 		onTryBoost(boost, target, source, effect) {
-//! 			if (effect.name === 'Intimidate' && boost.atk) {
-//! 				delete boost.atk;
-//! 				this.boost({ atk: 1 }, target, target, null, false, true);
-//! 			}
-//! 		},
-//! 		flags: { breakable: 1 },
-//! 		name: "Guard Dog",
-//! 		rating: 2,
-//! 		num: 275,
-//! 	},
-//! ```
+//! Guard Dog Ability - Immune to Intimidate, boosts Attack when targeted
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -32,27 +6,10 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onDragOutPriority(...)
-pub fn on_drag_out_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// TODO: Requires onTryBoost, onTryHit, and cannotRunAway flag
+pub fn on_try_boost(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onDragOut(...)
-pub fn on_drag_out(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_try_hit(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onTryBoostPriority(...)
-pub fn on_try_boost_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onTryBoost(...)
-pub fn on_try_boost(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
