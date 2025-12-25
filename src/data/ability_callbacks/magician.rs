5889d7a18e0a8aaba86a6957aadcea3fe8f3e055
Developer <dev@example.com>
2025-12-25 16:30:21 +0000

    Document magician - steals item after attack

--- src/data/ability_callbacks/magician.rs
+++ src/data/ability_callbacks/magician.rs
@@ -38,7 +38,18 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onAfterMoveSecondarySelf(...)
+/// onAfterMoveSecondarySelf(source, target, move)
+/// Steals target's item after attacking
+///
+/// TODO: onAfterMoveSecondarySelf handler not yet implemented
+/// TODO: Needs item system (pokemon.item, takeItem(), setItem())
+/// TODO: Needs source.switchFlag, move.hitTargets, volatiles['gem']
+/// When implemented, should:
+/// 1. Skip if no move, switching, no hitTargets, has item, gem used, fling, or status move
+/// 2. Sort hitTargets by speed
+/// 3. Loop through targets, skip source
+/// 4. Call pokemon.takeItem(source) and source.setItem(yourItem)
+/// 5. Add item message and return
 pub fn on_after_move_secondary_self(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
