10f0c8a89b1059b5f8bf23f80ff11004be4da021
Developer <dev@example.com>
2025-12-25 17:02:23 +0000

    Document rebound - CAP ability bounces moves first turn

--- src/data/ability_callbacks/rebound.rs
+++ src/data/ability_callbacks/rebound.rs
@@ -47,19 +47,35 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryHitPriority(...)
-pub fn on_try_hit_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_TRY_HIT_PRIORITY: i32 = 1;
 
-/// onTryHit(...)
+/// onTryHit(target, source, move)
+/// Bounces reflectable moves back (CAP ability, only works first turn)
+///
+/// TODO: onTryHit handler not yet implemented
+/// TODO: Needs effectState.target.activeTurns, move.hasBounced, move.flags['reflectable'], target.isSemiInvulnerable(), dex.getActiveMove(), actions.useMove()
+/// When implemented, should:
+/// 1. Skip if not the Pokemon's first turn active
+/// 2. Skip if target is source, move bounced, no reflectable flag, or target semi-invulnerable
+/// 3. Create new move copy with hasBounced = true, pranksterBoosted = false
+/// 4. Use the move from target back at source
+/// 5. Return null to block the original move
 pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAllyTryHitSide(...)
+/// onAllyTryHitSide(target, source, move)
+/// Bounces reflectable moves back for allies (CAP ability, only works first turn)
+///
+/// TODO: onAllyTryHitSide handler not yet implemented
+/// TODO: Needs effectState.target.activeTurns, target.isAlly(), move.hasBounced, move.flags['reflectable'], target.isSemiInvulnerable()
+/// When implemented, should:
+/// 1. Skip if not the Pokemon's first turn active
+/// 2. Skip if target is ally of source, move bounced, no reflectable flag, or target semi-invulnerable
+/// 3. Create new move copy, use from ability holder back at source
+/// 4. Set move.hasBounced = true to prevent multiple bounces in FFA
+/// 5. Return null to block the original move
 pub fn on_ally_try_hit_side(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
