5a3e44a8ed6b356a054d26813edd42a310c3795e
Developer <dev@example.com>
2025-12-25 18:16:33 +0000

    Implement oblivious (partial) - Blocks attract/captivate/taunt moves and Intimidate ability

--- src/data/ability_callbacks/oblivious.rs
+++ src/data/ability_callbacks/oblivious.rs
@@ -73,26 +73,36 @@ pub fn on_immunity(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHa
 
 /// onTryHit(pokemon, target, move)
 /// Blocks attract, captivate, and taunt moves
-///
-/// TODO: onTryHit handler not yet implemented
-/// When implemented, should:
-/// 1. Check if move is attract, captivate, or taunt
-/// 2. Add immune message and return null
-pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_try_hit(battle: &mut Battle, pokemon: &mut Pokemon, _target: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if (move.id === 'attract' || move.id === 'captivate' || move.id === 'taunt')
+    if move_.id.as_str() == "attract" || move_.id.as_str() == "captivate" || move_.id.as_str() == "taunt" {
+        // this.add('-immune', pokemon, '[from] ability: Oblivious');
+        battle.add("-immune", &[
+            Arg::Pokemon(pokemon),
+            Arg::Str("[from] ability: Oblivious")
+        ]);
+        // return null;
+        return AbilityHandlerResult::Null;
+    }
     AbilityHandlerResult::Undefined
 }
 
 /// onTryBoost(boost, target, source, effect)
 /// Blocks Intimidate
-///
-/// TODO: onTryBoost handler not yet implemented
-/// TODO: Needs boost object manipulation
-/// When implemented, should:
-/// 1. Check if effect.name === 'Intimidate' && boost.atk exists
-/// 2. Delete boost.atk and add fail message
-pub fn on_try_boost(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_try_boost(battle: &mut Battle, boost: &mut std::collections::HashMap<String, i8>, target: &Pokemon, _source: Option<&Pokemon>, effect_id: &str, _has_secondaries: bool) -> AbilityHandlerResult {
+    // if (effect.name === 'Intimidate' && boost.atk)
+    if effect_id == "intimidate" && boost.contains_key("atk") {
+        // delete boost.atk;
+        boost.remove("atk");
+        // this.add('-fail', target, 'unboost', 'Attack', '[from] ability: Oblivious', `[of] ${target}`);
+        battle.add("-fail", &[
+            Arg::Pokemon(target),
+            Arg::Str("unboost"),
+            Arg::Str("Attack"),
+            Arg::Str("[from] ability: Oblivious"),
+            Arg::Str(&format!("[of] {}", target.position))
+        ]);
+    }
     AbilityHandlerResult::Undefined
 }
 
