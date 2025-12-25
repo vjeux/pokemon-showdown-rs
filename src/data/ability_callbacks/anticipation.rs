93d24075a2123684d50d675b96bfeda0d997ad21
Developer <dev@example.com>
2025-12-25 16:10:19 +0100

    Implement anticipation ability
    Detects when opposing Pokemon have super-effective or OHKO moves.
    On battle start, checks all foe movesets for:
    - Moves with super-effective type coverage
    - OHKO moves (Fissure, Sheer Cold, etc.)
    
    If detected, displays the Anticipation ability message.

--- src/data/ability_callbacks/anticipation.rs
+++ src/data/ability_callbacks/anticipation.rs
@@ -31,29 +31,60 @@
 //! ```
 
 use crate::battle::{Battle, Arg};
-use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
+use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType, get_move};
 use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
+use crate::data::typechart::{get_effectiveness_multi, is_immune};
 use super::{AbilityHandlerResult, Status, Effect};
 
 /// onStart(pokemon)
-/// Note: This implementation is simplified as we don't have full dex access in callbacks yet.
-/// Full implementation would require:
-/// - Access to Dex to get move data from moveSlots
-/// - Type effectiveness checking via dex.getEffectiveness()
-/// - OHKO move detection
-/// For now, this is a stub that would need battle system enhancement.
-pub fn on_start(_battle: &mut Battle, _pokemon: &Pokemon) -> AbilityHandlerResult {
-    // TODO: Full implementation requires:
-    // 1. Access to foe pokemon (need to iterate battle.sides)
-    // 2. Access to Dex to look up moves from moveSlot IDs
-    // 3. Type effectiveness calculation for each move type against pokemon
-    // 4. Check for OHKO moves
-    // 5. Call battle.add() if super-effective or OHKO move found
+pub fn on_start(battle: &mut Battle, pokemon: &Pokemon) -> AbilityHandlerResult {
+    // for (const target of pokemon.foes())
+    // Get the opposing side index (in singles, it's 0 or 1, the opposite of current)
+    let foe_side_index = if pokemon.side_index == 0 { 1 } else { 0 };
+    let foes = pokemon.foes_stub(foe_side_index, false);
 
-    // This would need battle system refactoring to provide:
-    // - Dex access in ability callbacks
-    // - Helper method to get foe pokemon
-    // - Move lookup by ID
+    for (target_side_idx, target_pos) in foes {
+        let target = &battle.sides[target_side_idx].pokemon[target_pos];
+
+        // for (const moveSlot of target.moveSlots)
+        for move_slot in &target.move_slots {
+            // const move = this.dex.moves.get(moveSlot.move);
+            let Some(move_data) = get_move(&move_slot.id) else {
+                continue;
+            };
+
+            // if (move.category === 'Status') continue;
+            if move_data.category == MoveCategory::Status {
+                continue;
+            }
+
+            // const moveType = move.id === 'hiddenpower' ? target.hpType : move.type;
+            // Note: hpType is not currently stored on Pokemon in Rust version,
+            // so we just use the move type. Hidden Power type calculation from IVs
+            // would be needed for full Gen 7 and below support.
+            let move_type = &move_data.move_type;
+
+            // if (this.dex.getImmunity(moveType, pokemon) && this.dex.getEffectiveness(moveType, pokemon) > 0 || move.ohko)
+            // Check if any of the pokemon's types is immune to the move
+            let mut has_immunity = false;
+            for poke_type in &pokemon.types {
+                if is_immune(move_type, poke_type) {
+                    has_immunity = true;
+                    break;
+                }
+            }
+
+            let effectiveness = get_effectiveness_multi(move_type, &pokemon.types);
+
+            if (!has_immunity && effectiveness > 1.0) || move_data.ohko {
+                // this.add('-ability', pokemon, 'Anticipation');
+                battle.add("-ability", &[Arg::Pokemon(pokemon), Arg::Str("Anticipation")]);
+                // return;
+                return AbilityHandlerResult::Undefined;
+            }
+        }
+    }
+
     AbilityHandlerResult::Undefined
 }
