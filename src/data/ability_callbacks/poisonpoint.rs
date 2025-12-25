4319f91953a8c84122175ea3f5b6ba96fcc1420f
Developer <dev@example.com>
2025-12-25 16:54:16 +0000

    Document poisonpoint - 30% poison on contact

--- src/data/ability_callbacks/poisonpoint.rs
+++ src/data/ability_callbacks/poisonpoint.rs
@@ -27,7 +27,14 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onDamagingHit(...)
+/// onDamagingHit(damage, target, source, move)
+/// 30% chance to poison the attacker when hit by a contact move
+///
+/// TODO: onDamagingHit handler not yet implemented
+/// TODO: Needs checkMoveMakesContact(), randomChance(), source.trySetStatus()
+/// When implemented, should:
+/// 1. Check if move makes contact
+/// 2. If so, 30% chance (3/10) to try to poison the source
 pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
