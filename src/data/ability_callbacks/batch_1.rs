77796becf6b757cef646d0b966c4a606cf3b77a2
Developer <dev@example.com>
2025-12-25 11:42:19 +0100

    Implement chlorophyll ability - doubles Speed in sun

--- src/data/ability_callbacks/batch_1.rs
+++ src/data/ability_callbacks/batch_1.rs
@@ -1197,9 +1197,14 @@ pub mod chillingneigh {
 pub mod chlorophyll {
     use super::*;
 
-    /// onModifySpe(...)
-    pub fn on_modify_spe(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-        // TODO: Implement 1-to-1 from JS
+    /// onModifySpe(spe, pokemon)
+    pub fn on_modify_spe(battle: &Battle, _spe: u32, _pokemon: &Pokemon) -> AbilityHandlerResult {
+        // if (['sunnyday', 'desolateland'].includes(pokemon.effectiveWeather()))
+        let weather = battle.field.effective_weather();
+        if *weather == ID::new("sunnyday") || *weather == ID::new("desolateland") {
+            // return this.chainModify(2);
+            return AbilityHandlerResult::ChainModify(8192, 4096); // 2x
+        }
         AbilityHandlerResult::Undefined
     }
 }
