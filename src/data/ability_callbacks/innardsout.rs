a898a350a8edb078133ca5bc853c0ae839cb9a3c
Developer <dev@example.com>
2025-12-25 16:22:01 +0000

    Document innardsout - deals damage on KO, needs param fix

--- src/data/ability_callbacks/innardsout.rs
+++ src/data/ability_callbacks/innardsout.rs
@@ -26,15 +26,24 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onDamagingHitOrder(...)
-pub fn on_damaging_hit_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+/// onDamagingHitOrder: 1
+pub const ON_DAMAGING_HIT_ORDER: i32 = 1;
 
-/// onDamagingHit(...)
+/// onDamagingHit(damage, target, source, move)
+/// Deals damage equal to damage taken when KO'd
+///
+/// TODO: Partially implementable - onDamagingHit exists but needs verification
+/// TODO: JS calls target.getUndynamaxedHP(damage) which takes damage as parameter
+/// TODO: Rust has pokemon.get_undynamaxed_hp() with no parameters
+/// TODO: Need to verify if getUndynamaxedHP should take damage param or use damage directly
+/// When implemented, should:
+/// 1. Check if !target.hp (target fainted)
+/// 2. Call this.damage(amount, source, target) where amount is undynamaxed damage
 pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
+    // if (!target.hp) {
+    //     this.damage(target.getUndynamaxedHP(damage), source, target);
+    // }
     AbilityHandlerResult::Undefined
 }
 
