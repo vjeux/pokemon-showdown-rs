cee0a52560828ecfceb44d308471e04c613b148a
Developer <dev@example.com>
2025-12-25 16:06:42 +0000

    Document unseenfist ability implementation requirements
    - Contact moves ignore protection (Protect, Detect)
    - Needs mutable MoveDef in onModifyMove
    - Should delete move.flags protect flag for contact moves
    - Encountered alphabetically after unnerve

--- src/data/ability_callbacks/unseenfist.rs
+++ src/data/ability_callbacks/unseenfist.rs
@@ -23,9 +23,17 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyMove(...)
-pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
+/// onModifyMove(move)
+/// Contact moves ignore protection (Protect, Detect, etc.)
+///
+/// TODO: onModifyMove needs mutable MoveDef to delete move.flags['protect']
+/// Currently takes &MoveDef (immutable)
+/// When implemented, should:
+/// 1. Check if move.flags['contact']
+/// 2. If true, delete move.flags['protect'] to bypass protection
+pub fn on_modify_move(_battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
+    // Requires mutable MoveDef
     AbilityHandlerResult::Undefined
 }
 
