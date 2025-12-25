99e2fb0d161ff3c96148f44d9400b4d047422c98
Developer <dev@example.com>
2025-12-25 16:22:28 +0000

    Document innerfocus - prevents flinch and Intimidate

--- src/data/ability_callbacks/innerfocus.rs
+++ src/data/ability_callbacks/innerfocus.rs
@@ -29,13 +29,26 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryAddVolatile(...)
+/// onTryAddVolatile(status, pokemon)
+/// Prevents flinching
+///
+/// TODO: onTryAddVolatile handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Check if status.id === 'flinch'
+/// 2. Return null to prevent the volatile status
 pub fn on_try_add_volatile(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onTryBoost(...)
+/// onTryBoost(boost, target, source, effect)
+/// Prevents Attack reduction from Intimidate
+///
+/// TODO: onTryBoost handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Check if effect.name === 'Intimidate' && boost.atk exists
+/// 2. Delete boost.atk to prevent the reduction
+/// 3. Add fail message
 pub fn on_try_boost(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
