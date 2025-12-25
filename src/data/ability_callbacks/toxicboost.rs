67422935047935063593ebc114ca37bf3b20234f
Developer <dev@example.com>
2025-12-25 14:24:05 +0000

    Implement toxicboost ability
    - onBasePower: Boosts Physical move power by 1.5x when poisoned (psn) or badly poisoned (tox)
      - Returns ChainModify(6144, 4096) for 1.5x boost
    
    Matches JS implementation 1-to-1 from data/abilities.ts

--- src/data/ability_callbacks/toxicboost.rs
+++ src/data/ability_callbacks/toxicboost.rs
@@ -26,15 +26,15 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onBasePowerPriority(...)
-pub fn on_base_power_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onBasePowerPriority: 19
+pub const ON_BASE_POWER_PRIORITY: i32 = 19;
+
+/// onBasePower(basePower, attacker, defender, move)
+pub fn on_base_power(_base_power: u32, attacker: &Pokemon, _defender: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    // if ((attacker.status === 'psn' || attacker.status === 'tox') && move.category === 'Physical')
+    if (attacker.status.as_str() == "psn" || attacker.status.as_str() == "tox") && move_.category == MoveCategory::Physical {
+        // return this.chainModify(1.5);
+        return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x = 6144/4096
+    }
     AbilityHandlerResult::Undefined
 }
-
-/// onBasePower(...)
-pub fn on_base_power(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
