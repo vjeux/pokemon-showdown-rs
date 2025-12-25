948a8246e3b318b621da22658fd0b39d1a8986d4
Developer <dev@example.com>
2025-12-25 16:57:04 +0000

    Document primordialsea - permanent heavy rain weather

--- src/data/ability_callbacks/primordialsea.rs
+++ src/data/ability_callbacks/primordialsea.rs
@@ -38,19 +38,41 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
+/// onStart(source)
+/// Sets Primordial Sea weather
+///
+/// TODO: onStart handler not yet implemented
+/// TODO: Needs field.setWeather()
+/// When implemented, should:
+/// 1. Set weather to 'primordialsea'
 pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAnySetWeather(...)
+/// onAnySetWeather(target, source, weather)
+/// Prevents other weather from replacing strong weathers
+///
+/// TODO: onAnySetWeather handler not yet implemented
+/// TODO: Needs field.getWeather(), weather.id
+/// When implemented, should:
+/// 1. Define strong weathers: desolateland, primordialsea, deltastream
+/// 2. If current weather is primordialsea and new weather is not strong, return false to prevent it
 pub fn on_any_set_weather(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onEnd(...)
+/// onEnd(pokemon)
+/// Clears weather when ability holder faints (unless another has the ability)
+///
+/// TODO: onEnd handler not yet implemented
+/// TODO: Needs field.weatherState.source, getAllActive(), target.hasAbility(), field.clearWeather()
+/// When implemented, should:
+/// 1. Skip if this Pokemon is not the weather source
+/// 2. Check all active Pokemon for another with Primordial Sea
+/// 3. If found, transfer weather source to them
+/// 4. Otherwise, clear the weather
 pub fn on_end(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
