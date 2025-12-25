9799302fbfcb8c46854e95e92c394808558cfd8d
Developer <dev@example.com>
2025-12-25 19:42:26 +0000

    Implement sniper ability
    - Implemented on_modify_damage handler
    - Boosts damage by 1.5x on critical hits
    - Uses was_crit parameter to check for crits
    
    Progress: 161/314 abilities (51.6%)

--- src/data/ability_callbacks/sniper.rs
+++ src/data/ability_callbacks/sniper.rs
@@ -28,22 +28,13 @@ use super::{AbilityHandlerResult, Status, Effect};
 
 /// onModifyDamage(damage, source, target, move)
 /// Boosts damage by 1.5x on critical hits
-///
-/// TODO: This handler is not yet registered in the ability system.
-/// The onModifyDamage handler needs to be added to the battle engine alongside
-/// onSourceModifyDamage. When implemented, this should:
-/// 1. Check if target.getMoveHitData(move).crit is true
-/// 2. Return chainModify(1.5) which is ChainModify(6144, 4096)
-///
-/// Expected signature once infrastructure is ready:
-/// pub fn on_modify_damage(_battle: &mut Battle, _damage: u32, _source: &Pokemon, target: &Pokemon, move_: &MoveDef, was_crit: bool) -> AbilityHandlerResult {
-///     if was_crit {
-///         return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
-///     }
-///     AbilityHandlerResult::Undefined
-/// }
-pub fn on_modify_damage(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement when onModifyDamage handler infrastructure is added
+pub fn on_modify_damage(_battle: &mut Battle, _damage: u32, _source: &Pokemon, _target: &Pokemon, _move: &MoveDef, was_crit: bool) -> AbilityHandlerResult {
+    // if (target.getMoveHitData(move).crit)
+    if was_crit {
+        // this.debug('Sniper boost');
+        // return this.chainModify(1.5);
+        return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
+    }
     AbilityHandlerResult::Undefined
 }
 
