415de85b3645539320b490d7191e1ddbc4592705
Developer <dev@example.com>
2025-12-25 16:46:18 +0100

    Document soundproof ability implementation requirements
    - Added detailed TODOs for onTryHit and onAllyTryHitSide handlers
    - Blocks sound-based moves from hitting the Pokemon
    - Encountered alphabetically after solarpower

--- src/data/ability_callbacks/soundproof.rs
+++ src/data/ability_callbacks/soundproof.rs
@@ -31,13 +31,27 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryHit(...)
+/// onTryHit(target, source, move)
+/// Blocks sound-based moves
+///
+/// TODO: onTryHit handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Check if target !== source
+/// 2. Check if move.flags['sound'] is true
+/// 3. Add message: this.add('-immune', target, '[from] ability: Soundproof')
+/// 4. Return null to block the move
 pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAllyTryHitSide(...)
+/// onAllyTryHitSide(target, source, move)
+/// Shows immunity message when ally is targeted by sound move
+///
+/// TODO: onAllyTryHitSide handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Check if move.flags['sound'] is true
+/// 2. Add message: this.add('-immune', this.effectState.target, '[from] ability: Soundproof')
 pub fn on_ally_try_hit_side(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
