a8441a2a9daa54d30ce6a6f36c8db850da04395c
Developer <dev@example.com>
2025-12-25 16:51:14 +0000

    Document pastelveil - cures/prevents poison on self and allies

--- src/data/ability_callbacks/pastelveil.rs
+++ src/data/ability_callbacks/pastelveil.rs
@@ -52,31 +52,66 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
+/// onStart(pokemon)
+/// Cures poison/toxic on self and allies on switch-in
+///
+/// TODO: onStart handler not yet implemented
+/// TODO: Needs pokemon.alliesAndSelf(), ally.status, ally.cureStatus()
+/// When implemented, should:
+/// 1. Loop through all allies and self
+/// 2. If any have 'psn' or 'tox' status, add activate message and cure them
 pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onUpdate(...)
+/// onUpdate(pokemon)
+/// Cures poison/toxic on self
+///
+/// TODO: onUpdate handler not yet implemented
+/// TODO: Needs pokemon.status, pokemon.cureStatus()
+/// When implemented, should:
+/// 1. If status is 'psn' or 'tox', add activate message and cure it
 pub fn on_update(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAnySwitchIn(...)
+/// onAnySwitchIn()
+/// Calls onStart when any Pokemon switches in
+///
+/// TODO: onAnySwitchIn handler not yet implemented
+/// TODO: Needs to call onStart with effectState.target
+/// When implemented, should:
+/// 1. Call onStart handler with the ability holder
 pub fn on_any_switch_in(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onSetStatus(...)
+/// onSetStatus(status, target, source, effect)
+/// Prevents poison/toxic on self
+///
+/// TODO: onSetStatus handler not yet implemented
+/// TODO: Needs status.id, effect checking
+/// When implemented, should:
+/// 1. If status is not 'psn' or 'tox', allow it
+/// 2. If effect has status property, add immune message
+/// 3. Return false to prevent the status
 pub fn on_set_status(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAllySetStatus(...)
+/// onAllySetStatus(status, target, source, effect)
+/// Prevents poison/toxic on allies
+///
+/// TODO: onAllySetStatus handler not yet implemented
+/// TODO: Needs status.id, effect checking, effectState.target
+/// When implemented, should:
+/// 1. If status is not 'psn' or 'tox', allow it
+/// 2. If effect has status property, add block message with ability holder
+/// 3. Return false to prevent the status
 pub fn on_ally_set_status(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
