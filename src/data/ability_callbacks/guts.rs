bfe6be7c34c25be078d68dc659c867c2011d0b5d
Developer <dev@example.com>
2025-12-25 13:30:32 +0000

    Implement guts ability
    Guts boosts Attack by 1.5x when the Pokemon has a status condition.
    Implemented 1-to-1 from JS source.

--- src/data/ability_callbacks/guts.rs
+++ src/data/ability_callbacks/guts.rs
@@ -26,15 +26,17 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyAtkPriority(...)
-pub fn on_modify_atk_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+/// onModifyAtkPriority: 5
+pub const ON_MODIFY_ATK_PRIORITY: i32 = 5;
 
-/// onModifyAtk(...)
-pub fn on_modify_atk(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onModifyAtk(atk, pokemon)
+/// Boosts Attack by 1.5x when statused
+pub fn on_modify_atk(_atk: u32, pokemon: &Pokemon) -> AbilityHandlerResult {
+    // if (pokemon.status)
+    if !pokemon.status.is_empty() {
+        // return this.chainModify(1.5);
+        return AbilityHandlerResult::ChainModify(6144, 4096); // 1.5x
+    }
     AbilityHandlerResult::Undefined
 }
 
