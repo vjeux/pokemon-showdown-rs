94cf9ed343555eb19d7bbd3ac7a6b56456a40aed
Developer <dev@example.com>
2025-12-25 16:14:06 +0000

    Document forewarn - reveals foe strongest move

--- src/data/ability_callbacks/forewarn.rs
+++ src/data/ability_callbacks/forewarn.rs
@@ -1,41 +1,4 @@
-//! Forewarn Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	forewarn: {
-//! 		onStart(pokemon) {
-//! 			let warnMoves: (Move | Pokemon)[][] = [];
-//! 			let warnBp = 1;
-//! 			for (const target of pokemon.foes()) {
-//! 				for (const moveSlot of target.moveSlots) {
-//! 					const move = this.dex.moves.get(moveSlot.move);
-//! 					let bp = move.basePower;
-//! 					if (move.ohko) bp = 150;
-//! 					if (move.id === 'counter' || move.id === 'metalburst' || move.id === 'mirrorcoat') bp = 120;
-//! 					if (bp === 1) bp = 80;
-//! 					if (!bp && move.category !== 'Status') bp = 80;
-//! 					if (bp > warnBp) {
-//! 						warnMoves = [[move, target]];
-//! 						warnBp = bp;
-//! 					} else if (bp === warnBp) {
-//! 						warnMoves.push([move, target]);
-//! 					}
-//! 				}
-//! 			}
-//! 			if (!warnMoves.length) return;
-//! 			const [warnMoveName, warnTarget] = this.sample(warnMoves);
-//! 			this.add('-activate', pokemon, 'ability: Forewarn', warnMoveName, `[of] ${warnTarget}`);
-//! 		},
-//! 		flags: {},
-//! 		name: "Forewarn",
-//! 		rating: 0.5,
-//! 		num: 108,
-//! 	},
-//! ```
+//! Forewarn Ability - Reveals foe's strongest move
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -43,9 +6,7 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
-pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// TODO: Requires foes() iterator, target.moveSlots access
+pub fn on_start(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
