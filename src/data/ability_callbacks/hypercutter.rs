df1ac88c0e3069f7b11c76775df4fa2108ab0045
Developer <dev@example.com>
2025-12-25 17:43:06 +0000

    Document hypercutter - Prevents Attack from being lowered

--- src/data/ability_callbacks/hypercutter.rs
+++ src/data/ability_callbacks/hypercutter.rs
@@ -29,9 +29,21 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryBoost(...)
+/// onTryBoost(boost, target, source, effect)
+/// Prevents Attack stat from being lowered by opponents
+///
+/// TODO: onTryBoost handler not yet implemented
+/// TODO: Needs mutable boost object to delete negative Attack boosts
+/// TODO: Needs source/target comparison
+/// TODO: Needs effect.secondaries checking
+/// When implemented, should:
+/// 1. Skip if source and target are the same (self-inflicted)
+/// 2. Check if boost.atk exists and is negative
+/// 3. If so, delete boost.atk to prevent the Attack drop
+/// 4. If effect doesn't have secondaries, add fail message
 pub fn on_try_boost(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
+    // Requires onTryBoost handler with mutable boost object
     AbilityHandlerResult::Undefined
 }
 
