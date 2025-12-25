ebb80311f45bc92298d99e73dc9c880791cef724
Developer <dev@example.com>
2025-12-25 17:20:42 +0000

    Document stancechange - Aegislash forme changes

--- src/data/ability_callbacks/stancechange.rs
+++ src/data/ability_callbacks/stancechange.rs
@@ -27,13 +27,18 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyMovePriority(...)
-pub fn on_modify_move_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_MODIFY_MOVE_PRIORITY: i32 = 1;
 
-/// onModifyMove(...)
+/// onModifyMove(move, attacker, defender)
+/// Aegislash forme changes based on move used
+///
+/// TODO: onModifyMove handler not yet implemented
+/// TODO: Needs attacker.species.baseSpecies, attacker.transformed, move.category, move.id, attacker.species.name, attacker.formeChange()
+/// When implemented, should:
+/// 1. Skip if not Aegislash or if transformed
+/// 2. Skip if move is Status category and not King's Shield
+/// 3. Change to Shield Forme (Aegislash) if using King's Shield
+/// 4. Change to Blade Forme (Aegislash-Blade) if using attacking move
 pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
