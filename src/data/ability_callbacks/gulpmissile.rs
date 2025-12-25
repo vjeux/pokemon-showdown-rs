a3d1313d640901c8fd816e6a68d6157801c9503c
Developer <dev@example.com>
2025-12-25 16:15:39 +0000

    Document gulpmissile - Cramorant forme mechanic

--- src/data/ability_callbacks/gulpmissile.rs
+++ src/data/ability_callbacks/gulpmissile.rs
@@ -1,37 +1,4 @@
-//! Gulp Missile Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	gulpmissile: {
-//! 		onDamagingHit(damage, target, source, move) {
-//! 			if (!source.hp || !source.isActive || target.isSemiInvulnerable()) return;
-//! 			if (['cramorantgulping', 'cramorantgorging'].includes(target.species.id)) {
-//! 				this.damage(source.baseMaxhp / 4, source, target);
-//! 				if (target.species.id === 'cramorantgulping') {
-//! 					this.boost({ def: -1 }, source, target, null, true);
-//! 				} else {
-//! 					source.trySetStatus('par', target, move);
-//! 				}
-//! 				target.formeChange('cramorant', move);
-//! 			}
-//! 		},
-//! 		// The Dive part of this mechanic is implemented in Dive's `onTryMove` in moves.ts
-//! 		onSourceTryPrimaryHit(target, source, effect) {
-//! 			if (effect?.id === 'surf' && source.hasAbility('gulpmissile') && source.species.name === 'Cramorant') {
-//! 				const forme = source.hp <= source.maxhp / 2 ? 'cramorantgorging' : 'cramorantgulping';
-//! 				source.formeChange(forme, effect);
-//! 			}
-//! 		},
-//! 		flags: { cantsuppress: 1, notransform: 1 },
-//! 		name: "Gulp Missile",
-//! 		rating: 2.5,
-//! 		num: 241,
-//! 	},
-//! ```
+//! Gulp Missile Ability - Cramorant catches prey with Surf/Dive
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -39,15 +6,10 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onDamagingHit(...)
-pub fn on_damaging_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// TODO: Complex - requires forme change, onSourceTryPrimaryHit, onDamagingHit
+pub fn on_source_try_primary_hit(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
-/// onSourceTryPrimaryHit(...)
-pub fn on_source_try_primary_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_damaging_hit(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
