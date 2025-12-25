f59bb7c3c7802b26c7f1ff0e6b509c32c8af12b0
Developer <dev@example.com>
2025-12-25 16:15:38 +0000

    Document fullmetalbody - prevents stat reduction

--- src/data/ability_callbacks/fullmetalbody.rs
+++ src/data/ability_callbacks/fullmetalbody.rs
@@ -1,32 +1,4 @@
-//! Full Metal Body Ability
-//!
-//! Pokemon Showdown - http://pokemonshowdown.com/
-//!
-//! Generated from data/abilities.ts
-//!
-//! ```text
-//! JS Source (data/abilities.ts):
-//! 	fullmetalbody: {
-//! 		onTryBoost(boost, target, source, effect) {
-//! 			if (source && target === source) return;
-//! 			let showMsg = false;
-//! 			let i: BoostID;
-//! 			for (i in boost) {
-//! 				if (boost[i]! < 0) {
-//! 					delete boost[i];
-//! 					showMsg = true;
-//! 				}
-//! 			}
-//! 			if (showMsg && !(effect as ActiveMove).secondaries && effect.id !== 'octolock') {
-//! 				this.add("-fail", target, "unboost", "[from] ability: Full Metal Body", `[of] ${target}`);
-//! 			}
-//! 		},
-//! 		flags: {},
-//! 		name: "Full Metal Body",
-//! 		rating: 2,
-//! 		num: 230,
-//! 	},
-//! ```
+//! Full Metal Body Ability - Prevents stat reduction
 
 use crate::battle::{Battle, Arg};
 use crate::data::moves::{MoveDef, MoveCategory, MoveTargetType};
@@ -34,9 +6,7 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryBoost(...)
-pub fn on_try_boost(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// TODO: Requires onTryBoost handler (same as White Smoke/Clear Body)
+pub fn on_try_boost(_battle: &mut Battle, /* TODO */) -> AbilityHandlerResult {
     AbilityHandlerResult::Undefined
 }
-
