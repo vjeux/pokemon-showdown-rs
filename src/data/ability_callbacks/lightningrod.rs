39b9a0e30395f1cafd604c2d0be4613206e0d630
Developer <dev@example.com>
2025-12-25 16:27:20 +0000

    Document lightningrod - Electric absorption and redirect

--- src/data/ability_callbacks/lightningrod.rs
+++ src/data/ability_callbacks/lightningrod.rs
@@ -39,13 +39,28 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryHit(...)
+/// onTryHit(target, source, move)
+/// Absorbs Electric moves and boosts SpA
+///
+/// TODO: onTryHit handler not yet implemented
+/// When implemented, should:
+/// 1. Check if target !== source && move.type === 'Electric'
+/// 2. Boost SpA by 1 (if boost fails, add immune message)
+/// 3. Return null to negate the move
 pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAnyRedirectTarget(...)
+/// onAnyRedirectTarget(target, source, source2, move)
+/// Redirects Electric moves to this Pokemon in doubles
+///
+/// TODO: onAnyRedirectTarget handler not yet implemented
+/// When implemented, should:
+/// 1. Check if move.type === 'Electric' and not pledgecombo
+/// 2. Determine redirect target based on move.target
+/// 3. Validate target and redirect if valid
+/// 4. Add activate message and return effectState.target
 pub fn on_any_redirect_target(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
