d153b6064994d281f76fed1ec1921df138f473d1
Developer <dev@example.com>
2025-12-25 15:59:24 +0000

    Implement teravolt onStart, document onModifyMove needs
    - onStart announces ability when entering battle (IMPLEMENTED)
    - onModifyMove needs mutable MoveDef to set ignoreAbility flag
    - Makes moves ignore target abilities
    - Encountered alphabetically after terashift

--- src/data/ability_callbacks/teravolt.rs
+++ src/data/ability_callbacks/teravolt.rs
@@ -26,15 +26,24 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
-pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onStart(pokemon)
+/// Announces the ability when Pokemon enters battle
+pub fn on_start(battle: &mut Battle, pokemon: &Pokemon) -> AbilityHandlerResult {
+    // this.add('-ability', pokemon, 'Teravolt');
+    battle.add("-ability", &[Arg::Pokemon(pokemon), Arg::Str("Teravolt")]);
     AbilityHandlerResult::Undefined
 }
 
-/// onModifyMove(...)
-pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onModifyMove(move)
+/// Makes moves ignore target's abilities
+///
+/// TODO: onModifyMove handler needs mutable MoveDef to set move.ignoreAbility
+/// Currently onModifyMove takes &MoveDef (immutable reference)
+/// When implemented, should:
+/// 1. Set move.ignoreAbility = true
+pub fn on_modify_move(_battle: &mut Battle, _move: &MoveDef, _attacker: &Pokemon) -> AbilityHandlerResult {
+    // TODO: Requires mutable MoveDef to implement move.ignoreAbility = true
+    // See battlebond.rs for similar issue with move modification
     AbilityHandlerResult::Undefined
 }
 
