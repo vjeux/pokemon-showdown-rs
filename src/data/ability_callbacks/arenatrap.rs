77ba551f2a516c13c4176c3cf04bdc79ec5eadb2
Developer <dev@example.com>
2025-12-25 14:32:38 +0000

    Implement arenatrap ability - Traps grounded foes

--- src/data/ability_callbacks/arenatrap.rs
+++ src/data/ability_callbacks/arenatrap.rs
@@ -33,15 +33,34 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onFoeTrapPokemon(...)
-pub fn on_foe_trap_pokemon(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onFoeTrapPokemon(pokemon)
+/// Note: Simplified implementation - isAdjacent and isGrounded checks not available
+pub fn on_foe_trap_pokemon(_battle: &mut Battle, pokemon: &mut Pokemon, _arena_trap_holder: &Pokemon) -> AbilityHandlerResult {
+    // if (!pokemon.isAdjacent(this.effectState.target)) return;
+    // TODO: Need isAdjacent check
+
+    // if (pokemon.isGrounded())
+    // TODO: Need isGrounded check - for now assume grounded if not Flying type
+    let is_grounded = !pokemon.types.contains(&"Flying".to_string());
+    if is_grounded {
+        // pokemon.tryTrap(true);
+        pokemon.trapped = true;
+    }
     AbilityHandlerResult::Undefined
 }
 
-/// onFoeMaybeTrapPokemon(...)
-pub fn on_foe_maybe_trap_pokemon(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onFoeMaybeTrapPokemon(pokemon, source)
+pub fn on_foe_maybe_trap_pokemon(_battle: &mut Battle, pokemon: &mut Pokemon, _source: Option<&Pokemon>, _arena_trap_holder: &Pokemon) -> AbilityHandlerResult {
+    // if (!source) source = this.effectState.target;
+    // if (!source || !pokemon.isAdjacent(source)) return;
+    // TODO: Need isAdjacent check
+
+    // if (pokemon.isGrounded(!pokemon.knownType))
+    // TODO: Need isGrounded check - for now assume grounded if not Flying type
+    let is_grounded = !pokemon.types.contains(&"Flying".to_string());
+    if is_grounded {
+        // pokemon.maybeTrapped = true;
+        pokemon.maybe_trapped = true;
+    }
     AbilityHandlerResult::Undefined
 }
-
