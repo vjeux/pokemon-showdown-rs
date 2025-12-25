02df7802121e7f27d62698c0a6b5a3cc5bc1ee55
Developer <dev@example.com>
2025-12-25 16:50:34 +0100

    Document tabletsofruin ability implementation requirements
    - Added detailed TODOs for onStart and onAnyModifyAtk handlers
    - Reduces all foes Attack by 25%
    - Requires onAnyModifyAtk handler and move.ruinedAtk tracking
    - Encountered alphabetically after symbiosis

--- src/data/ability_callbacks/tabletsofruin.rs
+++ src/data/ability_callbacks/tabletsofruin.rs
@@ -32,13 +32,27 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onStart(...)
+/// onStart(pokemon)
+/// Announces Tablets of Ruin when sent out
+///
+/// TODO: Requires suppressingAbility check
+/// When implemented, should:
+/// 1. Check if this.suppressingAbility(pokemon) and return if true
+/// 2. Add ability announcement: this.add('-ability', pokemon, 'Tablets of Ruin')
 pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAnyModifyAtk(...)
+/// onAnyModifyAtk(atk, source, target, move)
+/// Reduces all Pokemon's Attack by 25% except those with Tablets of Ruin
+///
+/// TODO: onAnyModifyAtk handler not yet implemented in battle system
+/// When implemented, should:
+/// 1. Get abilityHolder from effectState.target
+/// 2. Check if source.hasAbility('Tablets of Ruin'), return if true
+/// 3. Track move.ruinedAtk to ensure only one Tablets of Ruin applies per move
+/// 4. Return chainModify(0.75) to reduce Attack by 25%
 pub fn on_any_modify_atk(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
