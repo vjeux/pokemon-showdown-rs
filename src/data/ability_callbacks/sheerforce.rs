c736c0bb9547922d6287dd312b452e8cd4b45672
Developer <dev@example.com>
2025-12-25 17:14:39 +0000

    Document sheerforce - removes secondaries for power boost

--- src/data/ability_callbacks/sheerforce.rs
+++ src/data/ability_callbacks/sheerforce.rs
@@ -34,19 +34,31 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyMove(...)
+/// onModifyMove(move, pokemon)
+/// Removes secondary effects from moves to boost power
+///
+/// TODO: onModifyMove handler not yet implemented
+/// TODO: Needs move.secondaries, delete move.secondaries, delete move.self, move.selfBoost, move.hasSheerForce
+/// When implemented, should:
+/// 1. If move has secondaries
+/// 2. Delete move.secondaries and move.self
+/// 3. For Clangor Soul Blaze, also delete move.selfBoost
+/// 4. Set move.hasSheerForce = true
 pub fn on_modify_move(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onBasePowerPriority(...)
-pub fn on_base_power_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_BASE_POWER_PRIORITY: i32 = 21;
 
-/// onBasePower(...)
+/// onBasePower(basePower, pokemon, target, move)
+/// Boosts moves with removed secondaries by 1.3x
+///
+/// TODO: onBasePower handler not yet implemented
+/// TODO: Needs move.hasSheerForce
+/// When implemented, should:
+/// 1. If move.hasSheerForce is true
+/// 2. Multiply base power by 5325/4096 (~1.3x)
 pub fn on_base_power(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
