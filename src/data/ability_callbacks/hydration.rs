2f67c928aa0d64693170f40a13d2919a34cd10cb
Developer <dev@example.com>
2025-12-25 17:42:40 +0000

    Document hydration - Cures status in rain weather

--- src/data/ability_callbacks/hydration.rs
+++ src/data/ability_callbacks/hydration.rs
@@ -29,21 +29,22 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onResidualOrder(...)
-pub fn on_residual_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_RESIDUAL_ORDER: i32 = 5;
+pub const ON_RESIDUAL_SUB_ORDER: i32 = 3;
 
-/// onResidualSubOrder(...)
-pub fn on_residual_sub_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onResidual(...)
+/// onResidual(pokemon)
+/// Cures status conditions in rain (raindance or primordialsea weather)
+///
+/// TODO: onResidual handler not yet implemented
+/// TODO: Needs weather system (pokemon.effectiveWeather())
+/// TODO: Needs pokemon.status checking
+/// When implemented, should:
+/// 1. Check if pokemon.status exists (has a status condition)
+/// 2. Check if pokemon.effectiveWeather() is 'raindance' or 'primordialsea'
+/// 3. If both true: Add debug message, add activate message, cure status
 pub fn on_residual(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
+    // Requires weather system
     AbilityHandlerResult::Undefined
 }
 
