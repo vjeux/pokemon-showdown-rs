0ec6478b8ea36e97f48f7107df92ca7d99e90c7c
Developer <dev@example.com>
2025-12-25 16:29:37 +0000

    Document magicbounce - reflects status moves

--- src/data/ability_callbacks/magicbounce.rs
+++ src/data/ability_callbacks/magicbounce.rs
@@ -42,19 +42,36 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onTryHitPriority(...)
-pub fn on_try_hit_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+/// onTryHitPriority: 1
+pub const ON_TRY_HIT_PRIORITY: i32 = 1;
 
-/// onTryHit(...)
+/// onTryHit(target, source, move)
+/// Reflects status moves back at user
+///
+/// TODO: onTryHit handler not yet implemented
+/// TODO: Needs move.hasBounced, move.flags.reflectable, isSemiInvulnerable()
+/// TODO: Needs dex.getActiveMove() and actions.useMove()
+/// When implemented, should:
+/// 1. Skip if target === source or move bounced or not reflectable
+/// 2. Get new move instance and set hasBounced = true
+/// 3. Call this.actions.useMove(newMove, target, { target: source })
+/// 4. Return null to prevent original move
 pub fn on_try_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onAllyTryHitSide(...)
+/// onAllyTryHitSide(target, source, move)
+/// Reflects status moves targeting ally's side
+///
+/// TODO: onAllyTryHitSide handler not yet implemented
+/// TODO: Needs isAlly(), isSemiInvulnerable(), move bouncing system
+/// When implemented, should:
+/// 1. Skip if target is ally or move bounced or not reflectable
+/// 2. Create new move instance with hasBounced = true
+/// 3. Call actions.useMove from effectState.target
+/// 4. Set original move.hasBounced = true for free-for-all
+/// 5. Return null to prevent original move
 pub fn on_ally_try_hit_side(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
