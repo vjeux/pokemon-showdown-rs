1814d83375a2d86b8c25b5ac55b5c00deb2cc57b
Developer <dev@example.com>
2025-12-25 17:11:23 +0000

    Document schooling - Wishiwashi forme changes

--- src/data/ability_callbacks/schooling.rs
+++ src/data/ability_callbacks/schooling.rs
@@ -49,25 +49,33 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onSwitchInPriority(...)
-pub fn on_switch_in_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_SWITCH_IN_PRIORITY: i32 = -1;
 
-/// onStart(...)
+/// onStart(pokemon)
+/// Wishiwashi forme changes based on HP and level
+///
+/// TODO: onStart handler not yet implemented
+/// TODO: Needs pokemon.baseSpecies, pokemon.level, pokemon.transformed, pokemon.hp, pokemon.maxhp, pokemon.species.id, pokemon.formeChange()
+/// When implemented, should:
+/// 1. Check if Wishiwashi, level >= 20, and not transformed
+/// 2. If HP > 25%, change to School forme
+/// 3. If HP <= 25%, change to Solo forme
 pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
 }
 
-/// onResidualOrder(...)
-pub fn on_residual_order(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_RESIDUAL_ORDER: i32 = 29;
 
-/// onResidual(...)
+/// onResidual(pokemon)
+/// Checks HP and changes forme each turn
+///
+/// TODO: onResidual handler not yet implemented
+/// TODO: Needs pokemon.baseSpecies, pokemon.level, pokemon.transformed, pokemon.hp, pokemon.maxhp, pokemon.species.id, pokemon.formeChange()
+/// When implemented, should:
+/// 1. Check if Wishiwashi, level >= 20, not transformed, and alive
+/// 2. If HP > 25%, change to School forme
+/// 3. If HP <= 25%, change to Solo forme
 pub fn on_residual(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
