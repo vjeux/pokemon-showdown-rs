75e2aece0d83baa5dc832a7c575700d5da9d2bd2
Developer <dev@example.com>
2025-12-25 13:35:37 +0000

    Implement galvanize ability - converts Normal moves to Electric with 1.2x boost

--- src/data/ability_callbacks/galvanize.rs
+++ src/data/ability_callbacks/galvanize.rs
@@ -35,27 +35,34 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onModifyTypePriority(...)
-pub fn on_modify_type_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub const ON_MODIFY_TYPE_PRIORITY: i32 = -1;
+
+/// Moves that don't get type-changed
+const NO_MODIFY_TYPE: &[&str] = &[
+    "judgment", "multiattack", "naturalgift", "revelationdance", "technoblast", "terrainpulse", "weatherball",
+];
+
+/// onModifyType(move, pokemon)
+/// Changes Normal-type moves to Electric-type
+pub fn on_modify_type(move_: &mut MoveDef, pokemon: &Pokemon) -> AbilityHandlerResult {
+    if move_.move_type == "Normal"
+        && !NO_MODIFY_TYPE.contains(&move_.id.as_str())
+        && !(move_.is_z && move_.category != crate::data::moves::MoveCategory::Status)
+        && !(move_.name == "Tera Blast" && pokemon.terastallized.is_some())
+    {
+        move_.move_type = "Electric".to_string();
+        move_.type_changer_boosted = true;
+    }
     AbilityHandlerResult::Undefined
 }
 
-/// onModifyType(...)
-pub fn on_modify_type(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
+pub const ON_BASE_POWER_PRIORITY: i32 = 23;
 
-/// onBasePowerPriority(...)
-pub fn on_base_power_priority(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
-    AbilityHandlerResult::Undefined
-}
-
-/// onBasePower(...)
-pub fn on_base_power(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+/// onBasePower(basePower, pokemon, target, move)
+pub fn on_base_power(_base_power: u32, _pokemon: &Pokemon, _target: &Pokemon, move_: &MoveDef) -> AbilityHandlerResult {
+    if move_.type_changer_boosted {
+        return AbilityHandlerResult::ChainModify(4915, 4096); // ~1.2x
+    }
     AbilityHandlerResult::Undefined
 }
 
