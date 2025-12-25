e5ca39607b8f0b5a26641acbc618175a2865fb91
Developer <dev@example.com>
2025-12-25 16:26:30 +0000

    Document libero - type change on move use

--- src/data/ability_callbacks/libero.rs
+++ src/data/ability_callbacks/libero.rs
@@ -30,7 +30,19 @@ use crate::pokemon::Pokemon;
 use crate::dex_data::ID;
 use super::{AbilityHandlerResult, Status, Effect};
 
-/// onPrepareHit(...)
+/// onPrepareHit(source, target, move)
+/// Changes to move's type before using it (once per switch-in)
+///
+/// TODO: onPrepareHit handler not yet implemented
+/// TODO: Needs effectState.libero tracking field
+/// TODO: Needs source.getTypes() and source.setType() methods
+/// When implemented, should:
+/// 1. Check effectState.libero (return if already activated)
+/// 2. Skip if move is bounced, future move, snatch, or calls another move
+/// 3. Get move type and check if different from current types
+/// 4. Call source.setType(type)
+/// 5. Set effectState.libero = true
+/// 6. Add typechange message
 pub fn on_prepare_hit(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
     // TODO: Implement 1-to-1 from JS
     AbilityHandlerResult::Undefined
