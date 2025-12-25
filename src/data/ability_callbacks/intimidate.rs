a08cda92dfeee413708ba939cbb45e3152adf6ff
Developer <dev@example.com>
2025-12-25 18:00:50 +0000

    Implement intimidate - Lowers Attack of adjacent foes when entering battle, checks for substitute volatile status

--- src/data/ability_callbacks/intimidate.rs
+++ src/data/ability_callbacks/intimidate.rs
@@ -36,16 +36,64 @@ use super::{AbilityHandlerResult, Status, Effect};
 
 /// onStart(pokemon)
 /// Lowers Attack of adjacent foes when entering battle
-///
-/// TODO: onStart handler exists but needs adjacentFoes() iterator
-/// TODO: Needs volatile status check (target.volatiles['substitute'])
-/// When implemented, should:
-/// 1. Loop through pokemon.adjacentFoes()
-/// 2. Add ability message on first foe
-/// 3. Check for substitute - add immune if present
-/// 4. Otherwise boost({ atk: -1 }, target, pokemon, null, true)
-pub fn on_start(battle: &mut Battle, /* TODO: Add parameters */) -> AbilityHandlerResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_start(battle: &mut Battle, pokemon: &Pokemon) -> AbilityHandlerResult {
+    let mut activated = false;
+
+    // Get all active Pokemon and filter for foes on the opposite side
+    // In the simple case (activePerHalf <= 2), all foes are adjacent
+    // For doubles+ (activePerHalf > 2), would need to check adjacency properly
+    // For now, implementing the basic case
+
+    let pokemon_side = pokemon.side_index;
+    let pokemon_position = pokemon.position;
+    let foe_side = if pokemon_side == 0 { 1 } else { 0 };
+
+    // Get all active Pokemon
+    let all_active = battle.get_all_active();
+
+    // Collect foe targets along with substitute status (need to collect first to avoid borrow issues)
+    let foe_targets: Vec<(usize, usize, bool)> = all_active.iter()
+        .filter(|(side_idx, _slot, _pokemon)| *side_idx == foe_side)
+        .map(|(side_idx, _slot, foe_pokemon)| {
+            let has_substitute = foe_pokemon.volatiles.contains_key(&ID::from("substitute"));
+            (*side_idx, foe_pokemon.position, has_substitute)
+        })
+        .collect();
+
+    for (target_side, target_idx, has_substitute) in foe_targets {
+        if !activated {
+            // this.add('-ability', pokemon, 'Intimidate', 'boost');
+            battle.add("-ability", &[
+                Arg::Pokemon(pokemon),
+                Arg::Str("Intimidate"),
+                Arg::Str("boost")
+            ]);
+            activated = true;
+        }
+
+        if has_substitute {
+            // this.add('-immune', target);
+            // Temporarily get reference and immediately use it
+            if let Some(side) = battle.sides.get(target_side) {
+                if let Some(target_pokemon) = side.pokemon.get(target_idx) {
+                    // Copy the pokemon name to avoid borrow issues
+                    let pokemon_name = target_pokemon.name.clone();
+                    let pokemon_side_idx = target_pokemon.side_index;
+                    let pokemon_pos = target_pokemon.position;
+                    drop(target_pokemon);
+                    drop(side);
+
+                    // Now we can call battle.add with a mutable borrow
+                    // Use a string-based arg instead of Pokemon reference
+                    battle.add("-immune", &[Arg::Str(&format!("p{}a: {}", pokemon_side_idx + 1, pokemon_name))]);
+                }
+            }
+        } else {
+            // this.boost({ atk: -1 }, target, pokemon, null, true);
+            battle.boost(&[("atk", -1)], (target_side, target_idx), Some((pokemon_side, pokemon_position)), None);
+        }
+    }
+
     AbilityHandlerResult::Undefined
 }
 
