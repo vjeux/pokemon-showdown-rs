fd864eaaec28f6f0d56defb0b35cfb2e993e85bc
Developer <dev@example.com>
2025-12-27 21:38:41 +0100

    Implement High Jump Kick callback - crash damage on move failure

--- src/data/move_callbacks/highjumpkick.rs
+++ src/data/move_callbacks/highjumpkick.rs
@@ -11,7 +11,24 @@ use crate::event::EventResult;
 ///     this.damage(source.baseMaxhp / 2, source, source, this.dex.conditions.get('High Jump Kick'));
 /// }
 pub fn on_move_fail(battle: &mut Battle, target_pos: Option<(usize, usize)>, source_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let source = match source_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // this.damage(source.baseMaxhp / 2, source, source, this.dex.conditions.get('High Jump Kick'));
+    let damage_amount = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.base_maxhp / 2
+    };
+
+    battle.damage(damage_amount, source, Some(source), Some(&ID::from("highjumpkick")));
+
     EventResult::Continue
 }
 
