33babfdf76b62abfadc61c2f4ff63c095a2afb9c
Developer <dev@example.com>
2025-12-27 20:27:01 +0100

    Implement endure callbacks - survives with 1 HP when hit

--- src/data/move_callbacks/endure.rs
+++ src/data/move_callbacks/endure.rs
@@ -11,15 +11,36 @@ use crate::event::EventResult;
 ///     return !!this.queue.willAct() && this.runEvent('StallMove', pokemon);
 /// }
 pub fn on_prepare_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = pokemon_pos;
+
+    // return !!this.queue.willAct() && this.runEvent('StallMove', pokemon);
+    let will_act = battle.queue.will_act();
+
+    if !will_act {
+        return EventResult::Bool(false);
+    }
+
+    let stall_result = battle.run_event("StallMove", pokemon, None, None);
+
+    EventResult::Bool(stall_result)
 }
 
 /// onHit(pokemon) {
 ///     pokemon.addVolatile('stall');
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let pokemon = pokemon_pos;
+
+    // pokemon.addVolatile('stall');
+    let pokemon_pokemon = match battle.pokemon_at_mut(pokemon.0, pokemon.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    pokemon_pokemon.add_volatile(ID::from("stall"));
+
     EventResult::Continue
 }
 
@@ -30,7 +51,22 @@ pub mod condition {
     ///     this.add('-singleturn', target, 'move: Endure');
     /// }
     pub fn on_start(battle: &mut Battle, target_pos: Option<(usize, usize)>) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        let target = match target_pos {
+            Some(pos) => pos,
+            None => return EventResult::Continue,
+        };
+
+        // this.add('-singleturn', target, 'move: Endure');
+        let target_arg = {
+            let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(target_pokemon)
+        };
+
+        battle.add("-singleturn", &[target_arg, "move: Endure".into()]);
+
         EventResult::Continue
     }
 
@@ -41,7 +77,45 @@ pub mod condition {
     ///     }
     /// }
     pub fn on_damage(battle: &mut Battle, damage: i32, target_pos: (usize, usize), source_pos: Option<(usize, usize)>, effect_id: Option<&str>) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        use crate::dex_data::ID;
+
+        let target = target_pos;
+
+        // if (effect?.effectType === 'Move' && damage >= target.hp) {
+        //     this.add('-activate', target, 'move: Endure');
+        //     return target.hp - 1;
+        // }
+        if let Some(effect) = effect_id {
+            // Check if effect is a Move
+            let is_move = battle.dex.get_move_by_id(&ID::from(effect)).is_some();
+
+            if is_move {
+                let target_hp = {
+                    let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                        Some(p) => p,
+                        None => return EventResult::Continue,
+                    };
+                    target_pokemon.hp
+                };
+
+                if damage >= target_hp {
+                    // this.add('-activate', target, 'move: Endure');
+                    let target_arg = {
+                        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                            Some(p) => p,
+                            None => return EventResult::Continue,
+                        };
+                        crate::battle::Arg::from(target_pokemon)
+                    };
+
+                    battle.add("-activate", &[target_arg, "move: Endure".into()]);
+
+                    // return target.hp - 1;
+                    return EventResult::Int(target_hp - 1);
+                }
+            }
+        }
+
         EventResult::Continue
     }
 }
