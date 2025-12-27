d583bfcd5e366fcfd3192a4d81fba33ba8f28af6
Developer <dev@example.com>
2025-12-27 23:49:16 +0000

    Implement stealthrock move callbacks

--- src/data/move_callbacks/stealthrock.rs
+++ src/data/move_callbacks/stealthrock.rs
@@ -15,7 +15,14 @@ pub mod condition {
     ///     this.add('-sidestart', side, 'move: Stealth Rock');
     /// }
     pub fn on_side_start(battle: &mut Battle) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // onSideStart(side) {
+        //     this.add('-sidestart', side, 'move: Stealth Rock');
+        // }
+
+        // this.add('-sidestart', side, 'move: Stealth Rock');
+        let side = battle.get_effect_state_side();
+        battle.add_side("-sidestart", side, &["move: Stealth Rock".into()]);
+
         EventResult::Continue
     }
 
@@ -25,7 +32,39 @@ pub mod condition {
     ///     this.damage(pokemon.maxhp * (2 ** typeMod) / 8);
     /// }
     pub fn on_switch_in(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        use crate::dex_data::ID;
+
+        // onSwitchIn(pokemon) {
+        //     if (pokemon.hasItem('heavydutyboots')) return;
+        //     const typeMod = this.clampIntRange(pokemon.runEffectiveness(this.dex.getActiveMove('stealthrock')), -6, 6);
+        //     this.damage(pokemon.maxhp * (2 ** typeMod) / 8);
+        // }
+        let pokemon = pokemon_pos;
+
+        // if (pokemon.hasItem('heavydutyboots')) return;
+        let has_heavy_duty_boots = battle.has_item(pokemon, "heavydutyboots");
+
+        if has_heavy_duty_boots {
+            return EventResult::Continue;
+        }
+
+        // const typeMod = this.clampIntRange(pokemon.runEffectiveness(this.dex.getActiveMove('stealthrock')), -6, 6);
+        let effectiveness = battle.run_effectiveness(pokemon, &ID::from("stealthrock"));
+        let type_mod = battle.clamp_int_range(effectiveness, -6, 6);
+
+        // this.damage(pokemon.maxhp * (2 ** typeMod) / 8);
+        let max_hp = {
+            let pokemon_data = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_data.max_hp
+        };
+
+        let multiplier = 2_i32.pow(type_mod as u32);
+        let damage_amount = (max_hp * multiplier as i32) / 8;
+        battle.damage(damage_amount, pokemon);
+
         EventResult::Continue
     }
 }
