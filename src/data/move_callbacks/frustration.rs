0934cb8bcd7e2d9691c674aee8ba10bc9bbf5d27
Developer <dev@example.com>
2025-12-27 19:52:56 +0000

    Implement frustration callback - power based on low happiness

--- src/data/move_callbacks/frustration.rs
+++ src/data/move_callbacks/frustration.rs
@@ -11,7 +11,22 @@ use crate::event::EventResult;
 ///     return Math.floor(((255 - pokemon.happiness) * 10) / 25) || 1;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = pokemon_pos;
+
+    // return Math.floor(((255 - pokemon.happiness) * 10) / 25) || 1;
+    let happiness = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.happiness
+    };
+
+    let bp = ((255 - happiness) * 10) / 25;
+
+    // || 1 means if bp is 0, return 1
+    let bp = if bp == 0 { 1 } else { bp };
+
+    EventResult::Int(bp)
 }
 
