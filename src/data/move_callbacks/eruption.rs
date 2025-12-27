89cccfe75c2fb3f6cf2b8a43da7dfc601bac99cf
Developer <dev@example.com>
2025-12-27 20:29:01 +0100

    Implement eruption callback - HP-based base power

--- src/data/move_callbacks/eruption.rs
+++ src/data/move_callbacks/eruption.rs
@@ -13,7 +13,32 @@ use crate::event::EventResult;
 ///     return bp;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = pokemon_pos;
+
+    // Get current move's base power
+    let base_power = match &battle.current_move {
+        Some(move_id) => {
+            match battle.dex.get_move_by_id(move_id) {
+                Some(move_data) => move_data.base_power,
+                None => return EventResult::Continue,
+            }
+        }
+        None => return EventResult::Continue,
+    };
+
+    // const bp = move.basePower * pokemon.hp / pokemon.maxhp;
+    let bp = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        base_power * pokemon_pokemon.hp / pokemon_pokemon.max_hp
+    };
+
+    // this.debug(`BP: ${bp}`);
+    battle.debug(&format!("BP: {}", bp));
+
+    // return bp;
+    EventResult::Int(bp)
 }
 
