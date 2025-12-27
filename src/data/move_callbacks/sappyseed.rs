f44da61b8460e5d0c81fcbcc7355889fa8ed87d0
Developer <dev@example.com>
2025-12-27 23:06:30 +0000

    Implement sappyseed move callbacks

--- src/data/move_callbacks/sappyseed.rs
+++ src/data/move_callbacks/sappyseed.rs
@@ -12,7 +12,24 @@ use crate::event::EventResult;
 ///     target.addVolatile('leechseed', source);
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let source = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (target.hasType('Grass')) return null;
+    let has_grass = battle.has_type(target, "Grass");
+
+    if has_grass {
+        return EventResult::Null;
+    }
+
+    // target.addVolatile('leechseed', source);
+    battle.add_volatile(&ID::from("leechseed"), target, Some(source), None);
+
     EventResult::Continue
 }
 
