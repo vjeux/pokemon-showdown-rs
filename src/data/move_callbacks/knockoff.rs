2ab94a393877f66c3fbb2fd13e7189d4b744f673
Developer <dev@example.com>
2025-12-27 22:02:00 +0100

    Implement Knock Off move callbacks
    Implements both callbacks for Knock Off, a move that removes the
    target's held item and gets a power boost if the target has an item:
    
    - onBasePower: Checks if target has an item that can be taken via
      TakeItem single event. If the item exists and can be taken,
      boosts move power by 1.5x using chainModify
    
    - onAfterHit: If the source is still alive (has HP), removes the
      target's item using take_item() and displays an -enditem message
      with the item name and source information

--- src/data/move_callbacks/knockoff.rs
+++ src/data/move_callbacks/knockoff.rs
@@ -15,8 +15,37 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_base_power(battle: &mut Battle, base_power: i32, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // const item = target.getItem();
+    let item_id = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.item.clone()
+    };
+
+    let item_id = match item_id {
+        Some(id) => id,
+        None => return EventResult::Continue,
+    };
+
+    // if (!this.singleEvent('TakeItem', item, target.itemState, target, target, move, item)) return;
+    let can_take_item = battle.run_single_event("TakeItem", Some(&item_id), target, target, None);
+    if !can_take_item {
+        return EventResult::Continue;
+    }
+
+    // if (item.id) {
+    //     return this.chainModify(1.5);
+    // }
+    EventResult::ChainModify(1.5)
 }
 
 /// onAfterHit(target, source) {
@@ -28,7 +57,49 @@ pub fn on_base_power(battle: &mut Battle, base_power: i32, pokemon_pos: (usize, 
 ///     }
 /// }
 pub fn on_after_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let source = source_pos;
+    let target = target_pos;
+
+    // if (source.hp) {
+    let source_has_hp = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.hp > 0
+    };
+
+    if !source_has_hp {
+        return EventResult::Continue;
+    }
+
+    //     const item = target.takeItem();
+    let taken_item = {
+        let target_pokemon = match battle.pokemon_at_mut(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.take_item()
+    };
+
+    //     if (item) {
+    //         this.add('-enditem', target, item.name, '[from] move: Knock Off', `[of] ${source}`);
+    //     }
+    if let Some(item_id) = taken_item {
+        let item_name = battle.dex.get_item_by_id(&item_id)
+            .map(|i| i.name.clone())
+            .unwrap_or_else(|| item_id.to_string());
+
+        let target_arg = crate::battle::Arg::Pos(target.0, target.1);
+        let source_arg = crate::battle::Arg::Pos(source.0, source.1);
+        battle.add("-enditem", &[
+            target_arg,
+            item_name.into(),
+            "[from] move: Knock Off".into(),
+            format!("[of] {}", source_arg).into(),
+        ]);
+    }
+
     EventResult::Continue
 }
 
