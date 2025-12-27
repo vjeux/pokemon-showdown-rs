2f5350083a85c9ed6a4b7d00f55836e75fa716fa
Developer <dev@example.com>
2025-12-27 22:07:59 +0100

    Implement Light Screen move callbacks
    Implements all 4 callbacks for Light Screen, a move that reduces
    Special damage dealt to the user's side:
    
    - condition::durationCallback: Returns 8 turns if source has Light
      Clay item, otherwise 5 turns
    
    - condition::onAnyModifyDamage: Reduces Special damage when target is
      on the protected side, move is Special category, not a critical hit,
      and doesn't infiltrate. In doubles battles (activePerHalf > 1),
      reduces by 2732/4096 (~0.667x). In singles, reduces by 0.5x
    
    - condition::onSideStart: Displays -sidestart message when Light
      Screen is activated on a side
    
    - condition::onSideEnd: Displays -sideend message when Light Screen
      expires on a side

--- src/data/move_callbacks/lightscreen.rs
+++ src/data/move_callbacks/lightscreen.rs
@@ -18,8 +18,27 @@ pub mod condition {
     ///     return 5;
     /// }
     pub fn duration_callback(battle: &mut Battle, target_pos: Option<(usize, usize)>, source_pos: Option<(usize, usize)>, effect_id: Option<&str>) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
-        EventResult::Continue
+        use crate::dex_data::ID;
+
+        // if (source?.hasItem('lightclay')) {
+        //     return 8;
+        // }
+        let has_light_clay = if let Some(source) = source_pos {
+            let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                Some(p) => p,
+                None => return EventResult::Int(5),
+            };
+            source_pokemon.has_item(&ID::from("lightclay"))
+        } else {
+            false
+        };
+
+        if has_light_clay {
+            return EventResult::Int(8);
+        }
+
+        // return 5;
+        EventResult::Int(5)
     }
 
     /// onAnyModifyDamage(damage, source, target, move) {
@@ -32,15 +51,94 @@ pub mod condition {
     ///     }
     /// }
     pub fn on_any_modify_damage(battle: &mut Battle, damage: i32, source_pos: Option<(usize, usize)>, target_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
-        EventResult::Continue
+        let target = match target_pos {
+            Some(pos) => pos,
+            None => return EventResult::Continue,
+        };
+
+        let source = match source_pos {
+            Some(pos) => pos,
+            None => return EventResult::Continue,
+        };
+
+        // if (target !== source && this.effectState.target.hasAlly(target) && this.getCategory(move) === 'Special') {
+        // target !== source
+        if target == source {
+            return EventResult::Continue;
+        }
+
+        // this.effectState.target.hasAlly(target)
+        let effect_state_target = match &battle.effect_state {
+            Some(es) => es.target,
+            None => return EventResult::Continue,
+        };
+
+        let has_ally = battle.has_ally(effect_state_target, target);
+        if !has_ally {
+            return EventResult::Continue;
+        }
+
+        // this.getCategory(move) === 'Special'
+        let is_special = {
+            if let Some(ref active_move) = battle.active_move {
+                active_move.category == crate::data::MoveCategory::Special
+            } else {
+                false
+            }
+        };
+
+        if !is_special {
+            return EventResult::Continue;
+        }
+
+        //     if (!target.getMoveHitData(move).crit && !move.infiltrates) {
+        let (is_crit, infiltrates) = {
+            let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+
+            let move_id_id = crate::dex_data::ID::from(move_id);
+            let is_crit = target_pokemon.get_move_hit_data(&move_id_id)
+                .map(|hd| hd.crit)
+                .unwrap_or(false);
+
+            let infiltrates = battle.active_move.as_ref()
+                .map(|m| m.infiltrates)
+                .unwrap_or(false);
+
+            (is_crit, infiltrates)
+        };
+
+        if is_crit || infiltrates {
+            return EventResult::Continue;
+        }
+
+        //         this.debug('Light Screen weaken');
+        battle.debug("Light Screen weaken");
+
+        //         if (this.activePerHalf > 1) return this.chainModify([2732, 4096]);
+        //         return this.chainModify(0.5);
+        if battle.active_per_half > 1 {
+            EventResult::ChainModifyFraction(2732, 4096)
+        } else {
+            EventResult::ChainModify(0.5)
+        }
     }
 
     /// onSideStart(side) {
     ///     this.add('-sidestart', side, 'move: Light Screen');
     /// }
     pub fn on_side_start(battle: &mut Battle) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // this.add('-sidestart', side, 'move: Light Screen');
+        let side_index = match &battle.effect_state {
+            Some(es) => es.target.0,
+            None => return EventResult::Continue,
+        };
+
+        let side_arg = crate::battle::Arg::Side(side_index);
+        battle.add("-sidestart", &[side_arg, "move: Light Screen".into()]);
+
         EventResult::Continue
     }
 
@@ -48,7 +146,15 @@ pub mod condition {
     ///     this.add('-sideend', side, 'move: Light Screen');
     /// }
     pub fn on_side_end(battle: &mut Battle) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // this.add('-sideend', side, 'move: Light Screen');
+        let side_index = match &battle.effect_state {
+            Some(es) => es.target.0,
+            None => return EventResult::Continue,
+        };
+
+        let side_arg = crate::battle::Arg::Side(side_index);
+        battle.add("-sideend", &[side_arg, "move: Light Screen".into()]);
+
         EventResult::Continue
     }
 }
