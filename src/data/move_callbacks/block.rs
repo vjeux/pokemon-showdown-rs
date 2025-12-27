8f3766bb2a32ac55d86f5f9ddd31b2a25e2008fe
Developer <dev@example.com>
2025-12-27 18:16:23 +0000

    Implement block onHit - trap the target

--- src/data/move_callbacks/block.rs
+++ src/data/move_callbacks/block.rs
@@ -6,12 +6,26 @@
 
 use crate::battle::Battle;
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// onHit(target, source, move) {
 ///     return target.addVolatile('trapped', source, move, 'trapper');
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    // Get the target
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // return target.addVolatile('trapped', source, move, 'trapper');
+    let target_pokemon = match battle.pokemon_at_mut(target.0, target.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    let result = target_pokemon.add_volatile(ID::from("trapped"));
+
+    EventResult::Bool(result)
 }
 
