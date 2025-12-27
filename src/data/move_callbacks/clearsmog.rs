6eda8bc8c2282d988605cdc5f1a563305b10edc1
Developer <dev@example.com>
2025-12-27 19:41:58 +0100

    Implement clearsmog callback - clears target's stat boosts

--- src/data/move_callbacks/clearsmog.rs
+++ src/data/move_callbacks/clearsmog.rs
@@ -4,7 +4,7 @@
 //!
 //! Generated from data/moves.ts
 
-use crate::battle::Battle;
+use crate::battle::{Battle, Arg};
 use crate::event::EventResult;
 
 /// onHit(target) {
@@ -12,7 +12,31 @@ use crate::event::EventResult;
 ///     this.add('-clearboost', target);
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // Get the target
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // target.clearBoosts();
+    {
+        let target_pokemon = match battle.pokemon_at_mut(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.clear_boosts();
+    }
+
+    // this.add('-clearboost', target);
+    let target_arg = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        Arg::from(target_pokemon)
+    };
+
+    battle.add("-clearboost", &[target_arg]);
+
     EventResult::Continue
 }
-
