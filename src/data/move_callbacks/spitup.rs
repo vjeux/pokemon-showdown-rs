d416fdc124f64808f54143c0130283649f96a4b2
Developer <dev@example.com>
2025-12-27 23:45:47 +0000

    Implement spitup move callbacks

--- src/data/move_callbacks/spitup.rs
+++ src/data/move_callbacks/spitup.rs
@@ -12,23 +12,73 @@ use crate::event::EventResult;
 ///     return pokemon.volatiles['stockpile'].layers * 100;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    // basePowerCallback(pokemon) {
+    //     if (!pokemon.volatiles['stockpile']?.layers) return false;
+    //     return pokemon.volatiles['stockpile'].layers * 100;
+    // }
+    let pokemon = pokemon_pos;
+
+    // if (!pokemon.volatiles['stockpile']?.layers) return false;
+    let layers = {
+        let pokemon_data = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        if let Some(volatile) = pokemon_data.volatiles.get(&ID::from("stockpile")) {
+            volatile.layers.unwrap_or(0)
+        } else {
+            0
+        }
+    };
+
+    if layers == 0 {
+        return EventResult::Bool(false);
+    }
+
+    // return pokemon.volatiles['stockpile'].layers * 100;
+    EventResult::Int(layers * 100)
 }
 
 /// onTry(source) {
 ///     return !!source.volatiles['stockpile'];
 /// }
 pub fn on_try(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    // onTry(source) {
+    //     return !!source.volatiles['stockpile'];
+    // }
+    let source = source_pos;
+
+    // return !!source.volatiles['stockpile'];
+    let has_stockpile = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.volatiles.contains_key(&ID::from("stockpile"))
+    };
+
+    EventResult::Bool(has_stockpile)
 }
 
 /// onAfterMove(pokemon) {
 ///     pokemon.removeVolatile('stockpile');
 /// }
 pub fn on_after_move(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    // onAfterMove(pokemon) {
+    //     pokemon.removeVolatile('stockpile');
+    // }
+    let pokemon = source_pos;
+
+    // pokemon.removeVolatile('stockpile');
+    battle.remove_volatile(&ID::from("stockpile"), pokemon);
+
     EventResult::Continue
 }
 
