46ec7521506734cc2b66f47a4493b16f842fb099
Developer <dev@example.com>
2025-12-27 22:04:29 +0000

    Implement naturepower move callback

--- src/data/move_callbacks/naturepower.rs
+++ src/data/move_callbacks/naturepower.rs
@@ -22,7 +22,37 @@ use crate::event::EventResult;
 ///     return null;
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+    use crate::battle_actions;
+
+    let pokemon = source_pos;
+    let target = target_pos;
+
+    // let move = 'triattack';
+    let mut move_id = "triattack";
+
+    // if (this.field.isTerrain('electricterrain')) {
+    if battle.field.terrain == Some(ID::from("electricterrain")) {
+        // move = 'thunderbolt';
+        move_id = "thunderbolt";
+    // } else if (this.field.isTerrain('grassyterrain')) {
+    } else if battle.field.terrain == Some(ID::from("grassyterrain")) {
+        // move = 'energyball';
+        move_id = "energyball";
+    // } else if (this.field.isTerrain('mistyterrain')) {
+    } else if battle.field.terrain == Some(ID::from("mistyterrain")) {
+        // move = 'moonblast';
+        move_id = "moonblast";
+    // } else if (this.field.isTerrain('psychicterrain')) {
+    } else if battle.field.terrain == Some(ID::from("psychicterrain")) {
+        // move = 'psychic';
+        move_id = "psychic";
+    }
+
+    // this.actions.useMove(move, pokemon, { target });
+    battle_actions::use_move(battle, move_id, pokemon, Some(target));
+
+    // return null;
+    EventResult::Null
 }
 
