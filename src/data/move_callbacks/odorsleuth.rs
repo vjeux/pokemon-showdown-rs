7b51d26380e83965a6ce871ef7761eb8fb82cb68
Developer <dev@example.com>
2025-12-27 22:10:05 +0000

    Implement odorsleuth move callback

--- src/data/move_callbacks/odorsleuth.rs
+++ src/data/move_callbacks/odorsleuth.rs
@@ -11,7 +11,23 @@ use crate::event::EventResult;
 ///     if (target.volatiles['miracleeye']) return false;
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let target = target_pos;
+
+    // if (target.volatiles['miracleeye']) return false;
+    let has_miracleeye = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.volatiles.contains_key(&ID::from("miracleeye"))
+    };
+
+    if has_miracleeye {
+        return EventResult::Bool(false);
+    }
+
     EventResult::Continue
 }
 
