97f396dc197403ef047448ad99665d52e4af9bf5
Developer <dev@example.com>
2025-12-27 18:15:21 +0000

    Implement blizzard onModifyMove - perfect accuracy in hail/snow

--- src/data/move_callbacks/blizzard.rs
+++ src/data/move_callbacks/blizzard.rs
@@ -11,7 +11,18 @@ use crate::event::EventResult;
 ///     if (this.field.isWeather(['hail', 'snowscape'])) move.accuracy = true;
 /// }
 pub fn on_modify_move(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (this.field.isWeather(['hail', 'snowscape'])) move.accuracy = true;
+    if battle.field.is_weather_any(&["hail", "snowscape"]) {
+        // move.accuracy = true;
+        // Store accuracy override in current effect state
+        if let Some(ref mut effect_state) = battle.current_effect_state {
+            effect_state.data.insert(
+                "accuracy".to_string(),
+                serde_json::Value::Bool(true),
+            );
+        }
+    }
+
     EventResult::Continue
 }
 
