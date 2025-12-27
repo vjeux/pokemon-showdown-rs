89fd35987f8eaf2c305443a9134ad77540b1e6a0
Developer <dev@example.com>
2025-12-27 22:33:42 +0000

    Implement psychicfangs move callbacks

--- src/data/move_callbacks/psychicfangs.rs
+++ src/data/move_callbacks/psychicfangs.rs
@@ -14,7 +14,20 @@ use crate::event::EventResult;
 ///     pokemon.side.removeSideCondition('auroraveil');
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let pokemon = target_pos;
+
+    // will shatter screens through sub, before you hit
+    // pokemon.side.removeSideCondition('reflect');
+    battle.remove_side_condition(&ID::from("reflect"), pokemon.0);
+
+    // pokemon.side.removeSideCondition('lightscreen');
+    battle.remove_side_condition(&ID::from("lightscreen"), pokemon.0);
+
+    // pokemon.side.removeSideCondition('auroraveil');
+    battle.remove_side_condition(&ID::from("auroraveil"), pokemon.0);
+
     EventResult::Continue
 }
 
