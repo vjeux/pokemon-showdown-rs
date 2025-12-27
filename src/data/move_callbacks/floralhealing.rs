785db8060809915d03443419660ecf1c4542d26a
Developer <dev@example.com>
2025-12-27 19:44:42 +0000

    Implement floralhealing callback - heals 50% or 67% in Grassy Terrain

--- src/data/move_callbacks/floralhealing.rs
+++ src/data/move_callbacks/floralhealing.rs
@@ -24,7 +24,73 @@ use crate::event::EventResult;
 ///     return success;
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    let source = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // let success = false;
+    // if (this.field.isTerrain('grassyterrain')) {
+    //     success = !!this.heal(this.modify(target.baseMaxhp, 0.667));
+    // } else {
+    //     success = !!this.heal(Math.ceil(target.baseMaxhp * 0.5));
+    // }
+    let is_grassy_terrain = battle.field.is_terrain(&ID::from("grassyterrain"));
+
+    let heal_amount = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        let base_max_hp = target_pokemon.base_max_hp;
+
+        if is_grassy_terrain {
+            // this.modify(target.baseMaxhp, 0.667) - multiply by 2/3
+            battle.modify_value(base_max_hp, 0.667)
+        } else {
+            // Math.ceil(target.baseMaxhp * 0.5) - 50% rounded up
+            ((base_max_hp as f64 * 0.5).ceil()) as i32
+        }
+    };
+
+    let success = battle.heal(heal_amount, target, None, None);
+
+    // if (success && !target.isAlly(source)) {
+    //     target.staleness = 'external';
+    // }
+    if success {
+        let is_ally = battle.is_ally(target, source);
+        if !is_ally {
+            let target_pokemon = match battle.pokemon_at_mut(target.0, target.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            target_pokemon.staleness = Some("external".to_string());
+        }
+    }
+
+    // if (!success) {
+    //     this.add('-fail', target, 'heal');
+    //     return this.NOT_FAIL;
+    // }
+    if !success {
+        let target_arg = {
+            let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(target_pokemon)
+        };
+
+        battle.add("-fail", &[target_arg, "heal".into()]);
+
+        return EventResult::NotFail;
+    }
+
+    // return success;
+    EventResult::Bool(success)
 }
 
