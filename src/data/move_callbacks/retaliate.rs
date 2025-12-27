cad89da52b779f7dc0e8a88ce7753f90cb92cf43
Developer <dev@example.com>
2025-12-27 22:46:42 +0000

    Implement retaliate move callbacks

--- src/data/move_callbacks/retaliate.rs
+++ src/data/move_callbacks/retaliate.rs
@@ -14,7 +14,25 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_base_power(battle: &mut Battle, base_power: i32, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let pokemon = pokemon_pos;
+
+    // if (pokemon.side.faintedLastTurn) {
+    //     this.debug('Boosted for a faint last turn');
+    //     return this.chainModify(2);
+    // }
+    let fainted_last_turn = {
+        let side = match battle.sides.get(pokemon.0) {
+            Some(s) => s,
+            None => return EventResult::Continue,
+        };
+        side.fainted_last_turn
+    };
+
+    if fainted_last_turn {
+        battle.debug("Boosted for a faint last turn");
+        return EventResult::ChainModify(2);
+    }
+
     EventResult::Continue
 }
 
