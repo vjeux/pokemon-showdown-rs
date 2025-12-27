db145fb435f9458fcf67161c24c54691bcaf82fd
Developer <dev@example.com>
2025-12-27 12:41:02 +0000

    Implement psywave damageCallback

--- src/data/move_callbacks/psywave.rs
+++ src/data/move_callbacks/psywave.rs
@@ -11,7 +11,15 @@ use crate::event::EventResult;
 ///     return (this.random(50, 151) * pokemon.level) / 100;
 /// }
 pub fn damage_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // Generate random number between 50 and 150 (inclusive)
+    let random_multiplier = battle.random(50, 151);
+    let damage = (random_multiplier * pokemon.level) / 100;
+
+    EventResult::Number(damage)
 }
 
