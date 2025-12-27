d8a5cd26eb2466e0555a4a098b1781fb2dcd9ca5
Developer <dev@example.com>
2025-12-27 23:05:46 +0000

    Implement sandsearstorm move callbacks

--- src/data/move_callbacks/sandsearstorm.rs
+++ src/data/move_callbacks/sandsearstorm.rs
@@ -13,7 +13,23 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_modify_move(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    // if (target && ['raindance', 'primordialsea'].includes(target.effectiveWeather())) {
+    //     move.accuracy = true;
+    // }
+    if let Some(target) = target_pos {
+        let effective_weather = battle.effective_weather(target);
+
+        if effective_weather == Some(ID::from("raindance")) || effective_weather == Some(ID::from("primordialsea")) {
+            let active_move = match &mut battle.active_move {
+                Some(m) => m,
+                None => return EventResult::Continue,
+            };
+            active_move.accuracy = 0; // true in TS means always hit, represented as 0 in the accuracy field
+        }
+    }
+
     EventResult::Continue
 }
 
