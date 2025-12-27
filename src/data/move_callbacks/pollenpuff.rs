ca2098bcbed3d283b3fedf5a6104c68825491c83
Developer <dev@example.com>
2025-12-27 22:19:59 +0000

    Implement pollenpuff move callbacks

--- src/data/move_callbacks/pollenpuff.rs
+++ src/data/move_callbacks/pollenpuff.rs
@@ -14,7 +14,21 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let source = source_pos;
+    let target = target_pos;
+
+    // if (source.isAlly(target)) {
+    let is_ally = battle.is_ally(source, target);
+
+    if is_ally {
+        // move.basePower = 0;
+        // move.infiltrates = true;
+        if let Some(ref mut active_move) = battle.active_move {
+            active_move.base_power = 0;
+            active_move.infiltrates = true;
+        }
+    }
+
     EventResult::Continue
 }
 
@@ -26,7 +40,58 @@ pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (
 ///     }
 /// }
 pub fn on_try_move(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let source = source_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (source.isAlly(target) && source.volatiles['healblock']) {
+    let is_ally = battle.is_ally(source, target);
+
+    if !is_ally {
+        return EventResult::Continue;
+    }
+
+    let has_healblock = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.volatiles.contains_key(&ID::from("healblock"))
+    };
+
+    if has_healblock {
+        // this.attrLastMove('[still]');
+        battle.attr_last_move("[still]");
+
+        // this.add('cant', source, 'move: Heal Block', move);
+        let (source_arg, move_name) = {
+            let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            let source_arg = crate::battle::Arg::from(source_pokemon);
+
+            let move_id = match &battle.active_move {
+                Some(m) => m.id.clone(),
+                None => return EventResult::Continue,
+            };
+
+            let move_data = battle.dex.get_move_by_id(&move_id);
+            let move_name = move_data.map(|m| m.name.clone()).unwrap_or_else(|| move_id.to_string());
+
+            (source_arg, move_name)
+        };
+
+        battle.add("cant", &[source_arg, "move: Heal Block".into(), move_name.into()]);
+
+        // return false;
+        return EventResult::Bool(false);
+    }
+
     EventResult::Continue
 }
 
@@ -38,7 +103,33 @@ pub fn on_try_move(battle: &mut Battle, source_pos: (usize, usize), target_pos: 
 ///     }
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let source = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (source.isAlly(target)) {
+    let is_ally = battle.is_ally(source, target);
+
+    if is_ally {
+        // if (!this.heal(Math.floor(target.baseMaxhp * 0.5))) {
+        let heal_amount = {
+            let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            target_pokemon.base_max_hp / 2
+        };
+
+        let healed = battle.heal(heal_amount, target, None, None);
+
+        if !healed {
+            // return this.NOT_FAIL;
+            return EventResult::NotFail;
+        }
+    }
+
     EventResult::Continue
 }
 
