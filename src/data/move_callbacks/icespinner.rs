c1d8ee145d0dff6455ed5c06da8c5da0117a2b07
Developer <dev@example.com>
2025-12-27 21:46:04 +0100

    Implement Ice Spinner callbacks - terrain removal after hit

--- src/data/move_callbacks/icespinner.rs
+++ src/data/move_callbacks/icespinner.rs
@@ -13,7 +13,23 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_after_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let source = source_pos;
+
+    // if (source.hp) {
+    //     this.field.clearTerrain();
+    // }
+    let source_has_hp = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.hp > 0
+    };
+
+    if source_has_hp {
+        battle.field.clear_terrain();
+    }
+
     EventResult::Continue
 }
 
@@ -23,7 +39,26 @@ pub fn on_after_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos:
 ///     }
 /// }
 pub fn on_after_sub_damage(battle: &mut Battle, damage: i32, target_pos: Option<(usize, usize)>, source_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let source = match source_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (source.hp) {
+    //     this.field.clearTerrain();
+    // }
+    let source_has_hp = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.hp > 0
+    };
+
+    if source_has_hp {
+        battle.field.clear_terrain();
+    }
+
     EventResult::Continue
 }
 
