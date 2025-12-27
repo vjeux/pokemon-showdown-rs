5e95e45f20ab7a9d4f30c0fe5a53706bea451ff0
Developer <dev@example.com>
2025-12-27 22:13:29 +0000

    Implement payback move callback

--- src/data/move_callbacks/payback.rs
+++ src/data/move_callbacks/payback.rs
@@ -16,7 +16,49 @@ use crate::event::EventResult;
 ///     return move.basePower * 2;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (target.newlySwitched || this.queue.willMove(target)) {
+    let newly_switched = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.newly_switched
+    };
+
+    let will_move = battle.queue_will_move(target);
+
+    if newly_switched || will_move {
+        // this.debug('Payback NOT boosted');
+        // (debug is typically not needed in Rust implementation)
+
+        // return move.basePower;
+        let base_power = {
+            let active_move = match &battle.active_move {
+                Some(m) => m,
+                None => return EventResult::Continue,
+            };
+            active_move.base_power
+        };
+        return EventResult::Int(base_power);
+    }
+
+    // this.debug('Payback damage boost');
+    // (debug is typically not needed in Rust implementation)
+
+    // return move.basePower * 2;
+    let base_power = {
+        let active_move = match &battle.active_move {
+            Some(m) => m,
+            None => return EventResult::Continue,
+        };
+        active_move.base_power
+    };
+
+    EventResult::Int(base_power * 2)
 }
 
