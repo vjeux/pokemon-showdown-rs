14c0df7428e8599950fe12f62e72728839454612
Developer <dev@example.com>
2025-12-27 22:06:38 +0000

    Implement noretreat move callbacks

--- src/data/move_callbacks/noretreat.rs
+++ src/data/move_callbacks/noretreat.rs
@@ -14,7 +14,39 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_try(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let source = source_pos;
+
+    // if (source.volatiles['noretreat']) return false;
+    let has_noretreat = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.volatiles.contains_key(&ID::from("noretreat"))
+    };
+
+    if has_noretreat {
+        return EventResult::Bool(false);
+    }
+
+    // if (source.volatiles['trapped']) {
+    let has_trapped = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.volatiles.contains_key(&ID::from("trapped"))
+    };
+
+    if has_trapped {
+        // delete move.volatileStatus;
+        if let Some(ref mut active_move) = battle.active_move {
+            active_move.volatile_status = None;
+        }
+    }
+
     EventResult::Continue
 }
 
@@ -25,7 +57,19 @@ pub mod condition {
     ///     this.add('-start', pokemon, 'move: No Retreat');
     /// }
     pub fn on_start(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        let pokemon = pokemon_pos;
+
+        // this.add('-start', pokemon, 'move: No Retreat');
+        let pokemon_arg = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(pokemon_pokemon)
+        };
+
+        battle.add("-start", &[pokemon_arg, "move: No Retreat".into()]);
+
         EventResult::Continue
     }
 
@@ -33,7 +77,17 @@ pub mod condition {
     ///     pokemon.tryTrap();
     /// }
     pub fn on_trap_pokemon(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        let pokemon = pokemon_pos;
+
+        // pokemon.tryTrap();
+        {
+            let pokemon_pokemon = match battle.pokemon_at_mut(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.try_trap();
+        }
+
         EventResult::Continue
     }
 }
