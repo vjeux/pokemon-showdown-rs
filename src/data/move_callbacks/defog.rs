3e5af1c76f6f32a9e0f0fca9584fe0f2ac3babc3
Developer <dev@example.com>
2025-12-27 20:00:11 +0100

    Implement defog callback - removes hazards and terrain

--- src/data/move_callbacks/defog.rs
+++ src/data/move_callbacks/defog.rs
@@ -29,7 +29,150 @@ use crate::event::EventResult;
 ///     return success;
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    // Get source and target
+    let source = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // let success = false;
+    let mut success = false;
+
+    // if (!target.volatiles['substitute'] || move.infiltrates) success = !!this.boost({ evasion: -1 });
+    let has_substitute = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.volatiles.contains_key(&ID::from("substitute"))
+    };
+
+    // TODO: Check if move infiltrates - requires access to current move data
+    // For now, we'll check substitute status
+    if !has_substitute {
+        // success = !!this.boost({ evasion: -1 });
+        let mut boosts = std::collections::HashMap::new();
+        boosts.insert("evasion".to_string(), -1);
+        let boost_result = battle.boost(&boosts, target);
+        success = boost_result;
+    }
+
+    // const removeAll = ['spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge'];
+    let remove_all = vec![
+        "spikes", "toxicspikes", "stealthrock", "stickyweb", "gmaxsteelsurge"
+    ];
+
+    // const removeTarget = ['reflect', 'lightscreen', 'auroraveil', 'safeguard', 'mist', ...removeAll];
+    let mut remove_target = vec![
+        "reflect", "lightscreen", "auroraveil", "safeguard", "mist"
+    ];
+    remove_target.extend_from_slice(&remove_all);
+
+    // for (const targetCondition of removeTarget) {
+    //     if (target.side.removeSideCondition(targetCondition)) {
+    //         if (!removeAll.includes(targetCondition)) continue;
+    //         this.add('-sideend', target.side, this.dex.conditions.get(targetCondition).name, '[from] move: Defog', `[of] ${source}`);
+    //         success = true;
+    //     }
+    // }
+    let target_side_index = target.0;
+    for target_condition in &remove_target {
+        let condition_id = ID::from(*target_condition);
+        let removed = if let Some(target_side) = battle.sides.get_mut(target_side_index) {
+            target_side.remove_side_condition(&condition_id)
+        } else {
+            false
+        };
+
+        if removed {
+            // if (!removeAll.includes(targetCondition)) continue;
+            if !remove_all.contains(target_condition) {
+                continue;
+            }
+
+            // this.add('-sideend', target.side, this.dex.conditions.get(targetCondition).name, '[from] move: Defog', `[of] ${source}`);
+            let (target_side_arg, source_arg, condition_name) = {
+                let target_side = match battle.sides.get(target_side_index) {
+                    Some(s) => s,
+                    None => continue,
+                };
+                let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                    Some(p) => p,
+                    None => continue,
+                };
+
+                // Get condition name from dex
+                let condition_data = battle.dex.get_condition_by_id(&condition_id);
+                let condition_name = condition_data.map(|c| c.name.clone()).unwrap_or_else(|| condition_id.to_string());
+
+                (crate::battle::Arg::SideID(target_side_index), crate::battle::Arg::from(source_pokemon), condition_name)
+            };
+
+            battle.add("-sideend", &[
+                target_side_arg,
+                condition_name.into(),
+                "[from] move: Defog".into(),
+                format!("[of] {}", source_arg.to_protocol_string()).into(),
+            ]);
+
+            // success = true;
+            success = true;
+        }
+    }
+
+    // for (const sideCondition of removeAll) {
+    //     if (source.side.removeSideCondition(sideCondition)) {
+    //         this.add('-sideend', source.side, this.dex.conditions.get(sideCondition).name, '[from] move: Defog', `[of] ${source}`);
+    //         success = true;
+    //     }
+    // }
+    let source_side_index = source.0;
+    for side_condition in &remove_all {
+        let condition_id = ID::from(*side_condition);
+        let removed = if let Some(source_side) = battle.sides.get_mut(source_side_index) {
+            source_side.remove_side_condition(&condition_id)
+        } else {
+            false
+        };
+
+        if removed {
+            // this.add('-sideend', source.side, this.dex.conditions.get(sideCondition).name, '[from] move: Defog', `[of] ${source}`);
+            let (source_side_arg, source_arg, condition_name) = {
+                let source_side = match battle.sides.get(source_side_index) {
+                    Some(s) => s,
+                    None => continue,
+                };
+                let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                    Some(p) => p,
+                    None => continue,
+                };
+
+                // Get condition name from dex
+                let condition_data = battle.dex.get_condition_by_id(&condition_id);
+                let condition_name = condition_data.map(|c| c.name.clone()).unwrap_or_else(|| condition_id.to_string());
+
+                (crate::battle::Arg::SideID(source_side_index), crate::battle::Arg::from(source_pokemon), condition_name)
+            };
+
+            battle.add("-sideend", &[
+                source_side_arg,
+                condition_name.into(),
+                "[from] move: Defog".into(),
+                format!("[of] {}", source_arg.to_protocol_string()).into(),
+            ]);
+
+            // success = true;
+            success = true;
+        }
+    }
+
+    // this.field.clearTerrain();
+    battle.field.clear_terrain();
+
+    // return success;
+    EventResult::Bool(success)
 }
 
