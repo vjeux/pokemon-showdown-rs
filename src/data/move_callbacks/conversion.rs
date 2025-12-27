8b094fe6972e5224bbe4b0d91ec4ad31d23c9c42
Developer <dev@example.com>
2025-12-27 19:44:43 +0100

    Implement conversion callback - changes type to first move's type

--- src/data/move_callbacks/conversion.rs
+++ src/data/move_callbacks/conversion.rs
@@ -4,8 +4,9 @@
 //!
 //! Generated from data/moves.ts
 
-use crate::battle::Battle;
+use crate::battle::{Battle, Arg};
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// onHit(target) {
 ///     const type = this.dex.moves.get(target.moveSlots[0].id).type;
@@ -13,7 +14,67 @@ use crate::event::EventResult;
 ///     this.add('-start', target, 'typechange', type);
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // Get the target
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // const type = this.dex.moves.get(target.moveSlots[0].id).type;
+    let move_type = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        if target_pokemon.move_slots.is_empty() {
+            return EventResult::Bool(false);
+        }
+
+        let first_move_id = &target_pokemon.move_slots[0].id;
+        let move_data = match battle.dex.get_move_by_id(first_move_id) {
+            Some(m) => m,
+            None => return EventResult::Continue,
+        };
+
+        move_data.move_type.clone()
+    };
+
+    // if (target.hasType(type) || !target.setType(type)) return false;
+    let has_type = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.has_type(&move_type)
+    };
+
+    if has_type {
+        return EventResult::Bool(false);
+    }
+
+    let set_type_success = {
+        let target_pokemon = match battle.pokemon_at_mut(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.set_type(vec![move_type.clone()])
+    };
+
+    if !set_type_success {
+        return EventResult::Bool(false);
+    }
+
+    // this.add('-start', target, 'typechange', type);
+    let target_arg = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        Arg::from(target_pokemon)
+    };
+
+    battle.add("-start", &[target_arg, "typechange".into(), move_type.to_string().into()]);
+
     EventResult::Continue
 }
-
