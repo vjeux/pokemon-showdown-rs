cf0e473a6f09c5dd73f49254af6eaa2b096795c3
Developer <dev@example.com>
2025-12-27 19:50:16 +0000

    Implement forestscurse callback - adds Grass type to target

--- src/data/move_callbacks/forestscurse.rs
+++ src/data/move_callbacks/forestscurse.rs
@@ -13,7 +13,55 @@ use crate::event::EventResult;
 ///     this.add('-start', target, 'typeadd', 'Grass', '[from] move: Forest\'s Curse');
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (target.hasType('Grass')) return false;
+    let has_grass_type = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.has_type(&ID::from("grass"), battle)
+    };
+
+    if has_grass_type {
+        return EventResult::Bool(false);
+    }
+
+    // if (!target.addType('Grass')) return false;
+    let add_type_result = {
+        let target_pokemon = match battle.pokemon_at_mut(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.add_type(ID::from("grass"))
+    };
+
+    if !add_type_result {
+        return EventResult::Bool(false);
+    }
+
+    // this.add('-start', target, 'typeadd', 'Grass', '[from] move: Forest\'s Curse');
+    let target_arg = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        crate::battle::Arg::from(target_pokemon)
+    };
+
+    battle.add("-start", &[
+        target_arg,
+        "typeadd".into(),
+        "Grass".into(),
+        "[from] move: Forest's Curse".into(),
+    ]);
+
     EventResult::Continue
 }
 
