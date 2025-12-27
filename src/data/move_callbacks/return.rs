41bb95cc329c929b5a344f72c46d2e0ab25aee9f
Developer <dev@example.com>
2025-12-27 12:37:11 +0000

    Implement return basePowerCallback

--- src/data/move_callbacks/return.rs
+++ src/data/move_callbacks/return.rs
@@ -11,7 +11,14 @@ use crate::event::EventResult;
 ///     return Math.floor((pokemon.happiness * 10) / 25) || 1;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    let bp = (pokemon.happiness * 10) / 25;
+    let bp = if bp == 0 { 1 } else { bp };
+
+    EventResult::Number(bp)
 }
 
