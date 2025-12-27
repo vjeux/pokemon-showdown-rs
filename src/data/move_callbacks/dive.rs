62f7fe2d089da25bee5f59b010e3abe831a463a1
Developer <dev@example.com>
2025-12-27 20:06:50 +0100

    Implement dive callbacks - two-turn underwater move with Cramorant forme change

--- src/data/move_callbacks/dive.rs
+++ src/data/move_callbacks/dive.rs
@@ -23,8 +23,99 @@ use crate::event::EventResult;
 ///     return null;
 /// }
 pub fn on_try_move(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    // if (attacker.removeVolatile(move.id)) {
+    //     return;
+    // }
+    let attacker = source_pos;
+    let defender = target_pos;
+
+    // Get move ID - for dive it should be "dive"
+    let move_id = match &battle.current_move {
+        Some(id) => id.clone(),
+        None => return EventResult::Continue,
+    };
+
+    let removed = {
+        let attacker_pokemon = match battle.pokemon_at_mut(attacker.0, attacker.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        attacker_pokemon.remove_volatile(&move_id)
+    };
+
+    if removed {
+        // return;
+        return EventResult::Continue;
+    }
+
+    // if (attacker.hasAbility('gulpmissile') && attacker.species.name === 'Cramorant' && !attacker.transformed) {
+    //     const forme = attacker.hp <= attacker.maxhp / 2 ? 'cramorantgorging' : 'cramorantgulping';
+    //     attacker.formeChange(forme, move);
+    // }
+    let should_forme_change = {
+        let attacker_pokemon = match battle.pokemon_at(attacker.0, attacker.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        attacker_pokemon.has_ability(&ID::from("gulpmissile"), battle) &&
+            attacker_pokemon.species.name == "Cramorant" &&
+            !attacker_pokemon.transformed
+    };
+
+    if should_forme_change {
+        let forme = {
+            let attacker_pokemon = match battle.pokemon_at(attacker.0, attacker.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            if attacker_pokemon.hp <= attacker_pokemon.max_hp / 2 {
+                "cramorantgorging"
+            } else {
+                "cramorantgulping"
+            }
+        };
+
+        let attacker_pokemon = match battle.pokemon_at_mut(attacker.0, attacker.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        attacker_pokemon.forme_change(forme, Some(&move_id));
+    }
+
+    // this.add('-prepare', attacker, move.name);
+    let (attacker_arg, move_name) = {
+        let attacker_pokemon = match battle.pokemon_at(attacker.0, attacker.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        let move_data = battle.dex.get_move_by_id(&move_id);
+        let move_name = move_data.map(|m| m.name.clone()).unwrap_or_else(|| move_id.to_string());
+
+        (crate::battle::Arg::from(attacker_pokemon), move_name)
+    };
+
+    battle.add("-prepare", &[attacker_arg, move_name.into()]);
+
+    // if (!this.runEvent('ChargeMove', attacker, defender, move)) {
+    //     return;
+    // }
+    let charge_result = battle.run_event("ChargeMove", attacker, defender, None);
+    if !charge_result {
+        // return;
+        return EventResult::Continue;
+    }
+
+    // attacker.addVolatile('twoturnmove', defender);
+    let attacker_pokemon = match battle.pokemon_at_mut(attacker.0, attacker.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+    attacker_pokemon.add_volatile(ID::from("twoturnmove"));
+
+    // return null;
+    EventResult::Null
 }
 
 pub mod condition {
@@ -34,7 +125,10 @@ pub mod condition {
     ///     if (type === 'sandstorm' || type === 'hail') return false;
     /// }
     pub fn on_immunity(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // This callback needs additional parameters - the immunity type
+        // The signature doesn't match the TypeScript version which takes (type, pokemon)
+        // TODO: This needs to be called with the type parameter to work correctly
+        // For now, we'll return Continue to not interfere
         EventResult::Continue
     }
 
@@ -45,8 +139,16 @@ pub mod condition {
     ///     return false;
     /// }
     pub fn on_invulnerability(battle: &mut Battle, target_pos: Option<(usize, usize)>, source_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
-        EventResult::Continue
+        // if (['surf', 'whirlpool'].includes(move.id)) {
+        //     return;
+        // }
+        if move_id == "surf" || move_id == "whirlpool" {
+            // return;
+            return EventResult::Continue;
+        }
+
+        // return false;
+        EventResult::Bool(false)
     }
 
     /// onSourceModifyDamage(damage, source, target, move) {
@@ -55,7 +157,14 @@ pub mod condition {
     ///     }
     /// }
     pub fn on_source_modify_damage(battle: &mut Battle, damage: i32, source_pos: Option<(usize, usize)>, target_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // if (move.id === 'surf' || move.id === 'whirlpool') {
+        //     return this.chainModify(2);
+        // }
+        if move_id == "surf" || move_id == "whirlpool" {
+            // return this.chainModify(2);
+            return EventResult::ChainModify(2.0);
+        }
+
         EventResult::Continue
     }
 }
