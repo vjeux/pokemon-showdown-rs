a52f19ab47d40dfacf90fd2843823c0c20a88d74
Developer <dev@example.com>
2025-12-27 22:54:16 +0000

    Implement revivalblessing move callbacks

--- src/data/move_callbacks/revivalblessing.rs
+++ src/data/move_callbacks/revivalblessing.rs
@@ -13,7 +13,23 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let source = source_pos;
+
+    // if (!source.side.pokemon.filter(ally => ally.fainted).length) {
+    //     return false;
+    // }
+    let has_fainted_allies = {
+        let side = match battle.sides.get(source.0) {
+            Some(s) => s,
+            None => return EventResult::Continue,
+        };
+        side.pokemon.iter().any(|p| p.fainted)
+    };
+
+    if !has_fainted_allies {
+        return EventResult::Bool(false);
+    }
+
     EventResult::Continue
 }
 
