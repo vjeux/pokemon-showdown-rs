2de748f68b1b8e861a2f81c75dd2b88bc932bca8
Developer <dev@example.com>
2025-12-27 21:49:10 +0100

    Implement Infernal Parade callback - doubles power if target has status

--- src/data/move_callbacks/infernalparade.rs
+++ src/data/move_callbacks/infernalparade.rs
@@ -12,7 +12,37 @@ use crate::event::EventResult;
 ///     return move.basePower;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (target.status || target.hasAbility('comatose')) return move.basePower * 2;
+    let has_status = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.status.is_some()
+    };
+
+    let has_comatose = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.has_ability(&ID::from("comatose"))
+    };
+
+    if has_status || has_comatose {
+        let base_power = battle.active_move.as_ref().map(|m| m.base_power).unwrap_or(0);
+        return EventResult::Int(base_power * 2);
+    }
+
+    // return move.basePower;
+    let base_power = battle.active_move.as_ref().map(|m| m.base_power).unwrap_or(0);
+    EventResult::Int(base_power)
 }
 
