82ede3f19d01a424f906865f87dafe734d3f6651
Developer <dev@example.com>
2025-12-27 23:20:50 +0000

    Implement skullbash move callback

--- src/data/move_callbacks/skullbash.rs
+++ src/data/move_callbacks/skullbash.rs
@@ -20,7 +20,74 @@ use crate::event::EventResult;
 ///     return null;
 /// }
 pub fn on_try_move(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    // onTryMove(attacker, defender, move) {
+    //     if (attacker.removeVolatile(move.id)) {
+    //         return;
+    //     }
+    //     this.add('-prepare', attacker, move.name);
+    //     this.boost({ def: 1 }, attacker, attacker, move);
+    //     if (!this.runEvent('ChargeMove', attacker, defender, move)) {
+    //         return;
+    //     }
+    //     attacker.addVolatile('twoturnmove', defender);
+    //     return null;
+    // }
+    let attacker = source_pos;
+    let defender = target_pos;
+
+    // if (attacker.removeVolatile(move.id)) {
+    //     return;
+    // }
+    let move_id = {
+        let active_move = match &battle.active_move {
+            Some(m) => m,
+            None => return EventResult::Continue,
+        };
+        active_move.id.clone()
+    };
+
+    let removed = battle.remove_volatile(&move_id, attacker);
+    if removed {
+        return EventResult::Continue;
+    }
+
+    // this.add('-prepare', attacker, move.name);
+    let (attacker_arg, move_name) = {
+        let attacker_pokemon = match battle.pokemon_at(attacker.0, attacker.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        let active_move = match &battle.active_move {
+            Some(m) => m,
+            None => return EventResult::Continue,
+        };
+        (crate::battle::Arg::from(attacker_pokemon), active_move.name.clone())
+    };
+
+    battle.add("-prepare", &[
+        attacker_arg,
+        move_name.into(),
+    ]);
+
+    // this.boost({ def: 1 }, attacker, attacker, move);
+    let mut boosts = std::collections::HashMap::new();
+    boosts.insert("def".to_string(), 1);
+    battle.boost(&boosts, attacker, Some(attacker), Some(&move_id));
+
+    // if (!this.runEvent('ChargeMove', attacker, defender, move)) {
+    //     return;
+    // }
+    let charge_result = battle.run_event("ChargeMove", attacker, defender, None);
+    if !charge_result {
+        return EventResult::Continue;
+    }
+
+    // attacker.addVolatile('twoturnmove', defender);
+    battle.add_volatile(&ID::from("twoturnmove"), attacker, defender, None);
+
+    // return null;
+    EventResult::Null
 }
 
