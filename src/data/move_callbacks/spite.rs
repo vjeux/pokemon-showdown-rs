4e726272e6566ae947c6c141042d87a499ee8346
Developer <dev@example.com>
2025-12-27 23:41:33 +0000

    Implement spite move callback

--- src/data/move_callbacks/spite.rs
+++ src/data/move_callbacks/spite.rs
@@ -11,13 +11,86 @@ use crate::event::EventResult;
 ///     let move: Move | ActiveMove | null = target.lastMove;
 ///     if (!move || move.isZ) return false;
 ///     if (move.isMax && move.baseMove) move = this.dex.moves.get(move.baseMove);
-/// 
+///
 ///     const ppDeducted = target.deductPP(move.id, 4);
 ///     if (!ppDeducted) return false;
 ///     this.add("-activate", target, 'move: Spite', move.name, ppDeducted);
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    // onHit(target) {
+    //     let move: Move | ActiveMove | null = target.lastMove;
+    //     if (!move || move.isZ) return false;
+    //     if (move.isMax && move.baseMove) move = this.dex.moves.get(move.baseMove);
+    //
+    //     const ppDeducted = target.deductPP(move.id, 4);
+    //     if (!ppDeducted) return false;
+    //     this.add("-activate", target, 'move: Spite', move.name, ppDeducted);
+    // }
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // let move: Move | ActiveMove | null = target.lastMove;
+    // if (!move || move.isZ) return false;
+    let (last_move_id, is_z, is_max, base_move) = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        match &target_pokemon.last_move {
+            Some(move_id) => {
+                let move_data = battle.dex.get_move_by_id(move_id);
+                match move_data {
+                    Some(m) => (move_id.clone(), m.is_z, m.is_max, m.base_move.clone()),
+                    None => return EventResult::Bool(false),
+                }
+            }
+            None => return EventResult::Bool(false),
+        }
+    };
+
+    if is_z {
+        return EventResult::Bool(false);
+    }
+
+    // if (move.isMax && move.baseMove) move = this.dex.moves.get(move.baseMove);
+    let move_id = if is_max && base_move.is_some() {
+        base_move.unwrap()
+    } else {
+        last_move_id
+    };
+
+    // const ppDeducted = target.deductPP(move.id, 4);
+    // if (!ppDeducted) return false;
+    let pp_deducted = battle.deduct_pp(target, &move_id, 4);
+    if pp_deducted == 0 {
+        return EventResult::Bool(false);
+    }
+
+    // this.add("-activate", target, 'move: Spite', move.name, ppDeducted);
+    let (target_arg, move_name) = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let move_data = battle.dex.get_move_by_id(&move_id);
+        let move_name = move_data.map(|m| m.name.clone()).unwrap_or_default();
+
+        (crate::battle::Arg::from(target_pokemon), move_name)
+    };
+
+    battle.add("-activate", &[
+        target_arg,
+        "move: Spite".into(),
+        move_name.into(),
+        pp_deducted.to_string().into(),
+    ]);
+
     EventResult::Continue
 }
 
