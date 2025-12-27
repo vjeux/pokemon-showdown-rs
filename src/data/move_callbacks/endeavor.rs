ebd3ba55a8475f905b985aeaeac37a5a2e57ba7f
Developer <dev@example.com>
2025-12-27 12:50:36 +0000

    Implement endeavor onTryImmunity (damageCallback needs getUndynamaxedHP())

--- src/data/move_callbacks/endeavor.rs
+++ src/data/move_callbacks/endeavor.rs
@@ -19,7 +19,22 @@ pub fn damage_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_
 ///     return pokemon.hp < target.hp;
 /// }
 pub fn on_try_immunity(battle: &mut Battle, target_pos: Option<(usize, usize)>, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let target_pos = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    let target = match battle.pokemon_at(target_pos.0, target_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // Only works if pokemon HP is less than target HP
+    EventResult::Bool(pokemon.hp < target.hp)
 }
 
