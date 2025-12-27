cffe3bb19a69d798760087b863272b56f8ad2cd8
Developer <dev@example.com>
2025-12-27 16:33:32 +0000

    Implement happyhour move callback

--- src/data/move_callbacks/happyhour.rs
+++ src/data/move_callbacks/happyhour.rs
@@ -4,14 +4,19 @@
 //!
 //! Generated from data/moves.ts
 
-use crate::battle::Battle;
+use crate::battle::{Arg, Battle};
 use crate::event::EventResult;
 
 /// onTryHit(target, source) {
 ///     this.add('-activate', target, 'move: Happy Hour');
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let target = match battle.pokemon_at(target_pos.0, target_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    battle.add("-activate", &[Arg::Pokemon(target), Arg::Str("move: Happy Hour")]);
     EventResult::Continue
 }
 
