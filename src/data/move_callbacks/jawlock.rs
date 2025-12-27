02c64b8cd848a6f5db5a090963065ee2f62988fe
Developer <dev@example.com>
2025-12-27 21:53:51 +0100

    Implement Jaw Lock callback - mutual trapping

--- src/data/move_callbacks/jawlock.rs
+++ src/data/move_callbacks/jawlock.rs
@@ -12,7 +12,20 @@ use crate::event::EventResult;
 ///     target.addVolatile('trapped', source, move, 'trapper');
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let source = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // source.addVolatile('trapped', target, move, 'trapper');
+    battle.add_volatile_with_source_effect(source, &ID::from("trapped"), Some(target), Some("trapper"));
+
+    // target.addVolatile('trapped', source, move, 'trapper');
+    battle.add_volatile_with_source_effect(target, &ID::from("trapped"), Some(source), Some("trapper"));
+
     EventResult::Continue
 }
 
