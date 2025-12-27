c49bf92a54e4b90c66276d4ed14befeb0571a203
Developer <dev@example.com>
2025-12-27 21:19:12 +0100

    Implement grassknot callbacks - weight-based power and dynamax fail check

--- src/data/move_callbacks/grassknot.rs
+++ src/data/move_callbacks/grassknot.rs
@@ -27,8 +27,46 @@ use crate::event::EventResult;
 ///     return bp;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // const targetWeight = target.getWeight();
+    let target_weight = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.get_weight()
+    };
+
+    // let bp;
+    // if (targetWeight >= 2000) { bp = 120; }
+    // else if (targetWeight >= 1000) { bp = 100; }
+    // else if (targetWeight >= 500) { bp = 80; }
+    // else if (targetWeight >= 250) { bp = 60; }
+    // else if (targetWeight >= 100) { bp = 40; }
+    // else { bp = 20; }
+    let bp = if target_weight >= 2000.0 {
+        120
+    } else if target_weight >= 1000.0 {
+        100
+    } else if target_weight >= 500.0 {
+        80
+    } else if target_weight >= 250.0 {
+        60
+    } else if target_weight >= 100.0 {
+        40
+    } else {
+        20
+    };
+
+    // this.debug(`BP: ${bp}`);
+    battle.debug(&format!("BP: {}", bp));
+
+    // return bp;
+    EventResult::Int(bp)
 }
 
 /// onTryHit(target, source, move) {
@@ -39,7 +77,32 @@ pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), tar
 ///     }
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let target = target_pos;
+    let source = source_pos;
+
+    // if (target.volatiles['dynamax']) {
+    let has_dynamax = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.volatiles.contains_key(&ID::from("dynamax"))
+    };
+
+    if has_dynamax {
+        // this.add('-fail', source, 'move: Grass Knot', '[from] Dynamax');
+        let source_arg = crate::battle::Arg::Pos(source.0, source.1);
+        battle.add("-fail", &[source_arg, "move: Grass Knot".into(), "[from] Dynamax".into()]);
+
+        // this.attrLastMove('[still]');
+        battle.attr_last_move("[still]");
+
+        // return null;
+        return EventResult::Null;
+    }
+
     EventResult::Continue
 }
 
