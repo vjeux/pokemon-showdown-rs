61ced3cf4d1f96fabe8de97c2054d7abe81b805c
Developer <dev@example.com>
2025-12-27 19:40:22 +0100

    Implement chillyreception callbacks - sets up snowscape and switches out

--- src/data/move_callbacks/chillyreception.rs
+++ src/data/move_callbacks/chillyreception.rs
@@ -4,14 +4,22 @@
 //!
 //! Generated from data/moves.ts
 
-use crate::battle::Battle;
+use crate::battle::{Battle, Arg};
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// priorityChargeCallback(source) {
 ///     source.addVolatile('chillyreception');
 /// }
 pub fn priority_charge_callback(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // source.addVolatile('chillyreception');
+    let pokemon = match battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    pokemon.add_volatile(ID::from("chillyreception"));
+
     EventResult::Continue
 }
 
@@ -23,7 +31,28 @@ pub mod condition {
     ///     this.add('-prepare', source, 'Chilly Reception', '[premajor]');
     /// }
     pub fn on_before_move(battle: &mut Battle, source_pos: Option<(usize, usize)>, target_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // if (move.id !== 'chillyreception') return;
+        if move_id != "chillyreception" {
+            // return;
+            return EventResult::Continue;
+        }
+
+        // this.add('-prepare', source, 'Chilly Reception', '[premajor]');
+        let source = match source_pos {
+            Some(pos) => pos,
+            None => return EventResult::Continue,
+        };
+
+        let source_arg = {
+            let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            Arg::from(source_pokemon)
+        };
+
+        battle.add("-prepare", &[source_arg, "Chilly Reception".into(), "[premajor]".into()]);
+
         EventResult::Continue
     }
 }
