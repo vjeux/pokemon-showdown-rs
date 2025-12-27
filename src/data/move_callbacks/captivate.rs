3c6e8ef47f5b8775644a741da354046a96633f94
Developer <dev@example.com>
2025-12-27 19:36:23 +0100

    Implement captivate callback - gender-based immunity check

--- src/data/move_callbacks/captivate.rs
+++ src/data/move_callbacks/captivate.rs
@@ -6,12 +6,37 @@
 
 use crate::battle::Battle;
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// onTryImmunity(pokemon, source) {
 ///     return (pokemon.gender === 'M' && source.gender === 'F') || (pokemon.gender === 'F' && source.gender === 'M');
 /// }
 pub fn on_try_immunity(battle: &mut Battle, pokemon_pos: (usize, usize), source_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
-}
+    // Get the source
+    let source = match source_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
 
+    // return (pokemon.gender === 'M' && source.gender === 'F') || (pokemon.gender === 'F' && source.gender === 'M');
+    let pokemon_gender = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon.gender.clone()
+    };
+
+    let source_gender = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.gender.clone()
+    };
+
+    let result = (pokemon_gender == ID::from("M") && source_gender == ID::from("F")) ||
+                 (pokemon_gender == ID::from("F") && source_gender == ID::from("M"));
+
+    EventResult::Bool(result)
+}
