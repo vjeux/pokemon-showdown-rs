6410c7f1ea951e83532341467884b53b6c9d9a80
Developer <dev@example.com>
2025-12-27 19:32:27 +0000

    Implement expandingforce callbacks - boosted and multi-target in Psychic Terrain

--- src/data/move_callbacks/expandingforce.rs
+++ src/data/move_callbacks/expandingforce.rs
@@ -14,7 +14,31 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_base_power(battle: &mut Battle, base_power: i32, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let source = pokemon_pos;
+
+    // if (this.field.isTerrain('psychicterrain') && source.isGrounded()) {
+    let is_psychic_terrain = battle.field.is_terrain(&ID::from("psychicterrain"));
+
+    if is_psychic_terrain {
+        let is_grounded = {
+            let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            source_pokemon.is_grounded(battle)
+        };
+
+        if is_grounded {
+            // this.debug('terrain buff');
+            battle.debug("terrain buff");
+
+            // return this.chainModify(1.5);
+            return EventResult::ChainModify(1.5);
+        }
+    }
+
     EventResult::Continue
 }
 
@@ -24,7 +48,35 @@ pub fn on_base_power(battle: &mut Battle, base_power: i32, pokemon_pos: (usize, 
 ///     }
 /// }
 pub fn on_modify_move(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let source = pokemon_pos;
+
+    // if (this.field.isTerrain('psychicterrain') && source.isGrounded()) {
+    //     move.target = 'allAdjacentFoes';
+    // }
+    let is_psychic_terrain = battle.field.is_terrain(&ID::from("psychicterrain"));
+
+    if is_psychic_terrain {
+        let is_grounded = {
+            let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            source_pokemon.is_grounded(battle)
+        };
+
+        if is_grounded {
+            // move.target = 'allAdjacentFoes';
+            // We need to modify the current move's target
+            if let Some(ref move_id) = battle.current_move {
+                if let Some(move_data) = battle.dex.get_move_by_id_mut(move_id) {
+                    move_data.target = Some("allAdjacentFoes".to_string());
+                }
+            }
+        }
+    }
+
     EventResult::Continue
 }
 
