6973b5f26fa1aec3ebbb760948cc38c20ac9c19a
Developer <dev@example.com>
2025-12-27 20:19:27 +0100

    Implement electrify callbacks - changes target's next move to Electric type

--- src/data/move_callbacks/electrify.rs
+++ src/data/move_callbacks/electrify.rs
@@ -11,7 +11,24 @@ use crate::event::EventResult;
 ///     if (!this.queue.willMove(target) && target.activeTurns) return false;
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let target = target_pos;
+
+    // if (!this.queue.willMove(target) && target.activeTurns) return false;
+    let will_move = battle.queue.will_move(target);
+
+    let active_turns = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.active_turns
+    };
+
+    if !will_move && active_turns > 0 {
+        // return false;
+        return EventResult::Bool(false);
+    }
+
     EventResult::Continue
 }
 
@@ -22,7 +39,22 @@ pub mod condition {
     ///     this.add('-singleturn', target, 'move: Electrify');
     /// }
     pub fn on_start(battle: &mut Battle, target_pos: Option<(usize, usize)>) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        let target = match target_pos {
+            Some(pos) => pos,
+            None => return EventResult::Continue,
+        };
+
+        // this.add('-singleturn', target, 'move: Electrify');
+        let target_arg = {
+            let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(target_pokemon)
+        };
+
+        battle.add("-singleturn", &[target_arg, "move: Electrify".into()]);
+
         EventResult::Continue
     }
 
@@ -33,7 +65,21 @@ pub mod condition {
     ///     }
     /// }
     pub fn on_modify_type(battle: &mut Battle, move_id: &str) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        use crate::dex_data::ID;
+
+        // if (move.id !== 'struggle') {
+        //     this.debug('Electrify making move type electric');
+        //     move.type = 'Electric';
+        // }
+        if move_id != "struggle" {
+            // this.debug('Electrify making move type electric');
+            battle.debug("Electrify making move type electric");
+
+            // move.type = 'Electric';
+            // Return the Electric type as the modified type
+            return EventResult::ID(ID::from("electric"));
+        }
+
         EventResult::Continue
     }
 }
