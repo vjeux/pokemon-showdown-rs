2c02bc4ce5949ccdf82ff02eb8a6d8f65fc3f0ec
Developer <dev@example.com>
2025-12-27 12:37:46 +0000

    Implement pikapapow and veeveevolley basePowerCallbacks

--- src/data/move_callbacks/veeveevolley.rs
+++ src/data/move_callbacks/veeveevolley.rs
@@ -13,7 +13,15 @@ use crate::event::EventResult;
 ///     return bp;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    let bp = (pokemon.happiness * 10) / 25;
+    let bp = if bp == 0 { 1 } else { bp };
+
+    // TODO: battle.debug(`BP: ${bp}`);
+    EventResult::Number(bp)
 }
 
