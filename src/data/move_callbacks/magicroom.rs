31ac07f690d58389512402c0976dd334315ba624
Developer <dev@example.com>
2025-12-27 22:18:27 +0100

    Implement Magic Room move callbacks
    Implements all 4 callbacks for Magic Room, a field condition that
    suppresses the effects of held items:
    
    - condition::durationCallback: Returns 7 turns if source has Persistent
      ability (with -activate message), otherwise 5 turns
    
    - condition::onFieldStart: Displays -fieldstart message with [persistent]
      tag if source has Persistent ability. Runs End single event on all
      active Pokemon's items to suppress their effects
    
    - condition::onFieldRestart: Removes the magicroom pseudo weather,
      effectively toggling it off when used again
    
    - condition::onFieldEnd: Displays -fieldend message with the source
      Pokemon from effect state

--- src/data/move_callbacks/magicroom.rs
+++ src/data/move_callbacks/magicroom.rs
@@ -19,8 +19,31 @@ pub mod condition {
     ///     return 5;
     /// }
     pub fn duration_callback(battle: &mut Battle, target_pos: Option<(usize, usize)>, source_pos: Option<(usize, usize)>, effect_id: Option<&str>) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
-        EventResult::Continue
+        use crate::dex_data::ID;
+
+        // if (source?.hasAbility('persistent')) {
+        let has_persistent = if let Some(source) = source_pos {
+            let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                Some(p) => p,
+                None => return EventResult::Int(5),
+            };
+            source_pokemon.has_ability(&ID::from("persistent"))
+        } else {
+            false
+        };
+
+        if has_persistent {
+            //     this.add('-activate', source, 'ability: Persistent', '[move] Magic Room');
+            if let Some(source) = source_pos {
+                let source_arg = crate::battle::Arg::Pos(source.0, source.1);
+                battle.add("-activate", &[source_arg, "ability: Persistent".into(), "[move] Magic Room".into()]);
+            }
+            //     return 7;
+            return EventResult::Int(7);
+        }
+
+        // return 5;
+        EventResult::Int(5)
     }
 
     /// onFieldStart(target, source) {
@@ -34,7 +57,50 @@ pub mod condition {
     ///     }
     /// }
     pub fn on_field_start(battle: &mut Battle, target_pos: Option<(usize, usize)>, source_pos: Option<(usize, usize)>) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        use crate::dex_data::ID;
+
+        // if (source?.hasAbility('persistent')) {
+        //     this.add('-fieldstart', 'move: Magic Room', `[of] ${source}`, '[persistent]');
+        // } else {
+        //     this.add('-fieldstart', 'move: Magic Room', `[of] ${source}`);
+        // }
+        let has_persistent = if let Some(source) = source_pos {
+            let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                Some(p) => p,
+                None => false,
+            };
+            source_pokemon.has_ability(&ID::from("persistent"))
+        } else {
+            false
+        };
+
+        if let Some(source) = source_pos {
+            let source_arg = crate::battle::Arg::Pos(source.0, source.1);
+            if has_persistent {
+                battle.add("-fieldstart", &["move: Magic Room".into(), format!("[of] {}", source_arg).into(), "[persistent]".into()]);
+            } else {
+                battle.add("-fieldstart", &["move: Magic Room".into(), format!("[of] {}", source_arg).into()]);
+            }
+        }
+
+        // for (const mon of this.getAllActive()) {
+        //     this.singleEvent('End', mon.getItem(), mon.itemState, mon);
+        // }
+        let all_active = battle.get_all_active();
+        for pokemon_pos in all_active {
+            let item_id = {
+                let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+                    Some(p) => p,
+                    None => continue,
+                };
+                pokemon.item.clone()
+            };
+
+            if let Some(item_id) = item_id {
+                battle.run_single_event("End", Some(&item_id), pokemon_pos, pokemon_pos, None);
+            }
+        }
+
         EventResult::Continue
     }
 
@@ -42,7 +108,11 @@ pub mod condition {
     ///     this.field.removePseudoWeather('magicroom');
     /// }
     pub fn on_field_restart(battle: &mut Battle, target_pos: Option<(usize, usize)>, source_pos: Option<(usize, usize)>) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        use crate::dex_data::ID;
+
+        // this.field.removePseudoWeather('magicroom');
+        battle.remove_pseudo_weather(&ID::from("magicroom"));
+
         EventResult::Continue
     }
 
@@ -50,7 +120,15 @@ pub mod condition {
     ///     this.add('-fieldend', 'move: Magic Room', '[of] ' + this.effectState.source);
     /// }
     pub fn on_field_end(battle: &mut Battle) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // this.add('-fieldend', 'move: Magic Room', '[of] ' + this.effectState.source);
+        let source = match &battle.effect_state {
+            Some(es) => es.source,
+            None => return EventResult::Continue,
+        };
+
+        let source_arg = crate::battle::Arg::Pos(source.0, source.1);
+        battle.add("-fieldend", &["move: Magic Room".into(), format!("[of] {}", source_arg).into()]);
+
         EventResult::Continue
     }
 }
