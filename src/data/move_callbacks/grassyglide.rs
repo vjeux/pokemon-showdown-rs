142112eb9c0c808eeeea37169f6073692d952d69
Developer <dev@example.com>
2025-12-27 21:13:51 +0100

    Implement grassyglide callback - priority boost in Grassy Terrain

--- src/data/move_callbacks/grassyglide.rs
+++ src/data/move_callbacks/grassyglide.rs
@@ -13,7 +13,29 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_modify_priority(battle: &mut Battle, source_pos: Option<(usize, usize)>, target_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let source = match source_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (this.field.isTerrain('grassyterrain') && source.isGrounded()) {
+    let is_grassy_terrain = battle.field.is_terrain(&ID::from("grassyterrain"));
+
+    let is_grounded = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.is_grounded(battle)
+    };
+
+    if is_grassy_terrain && is_grounded {
+        // return priority + 1;
+        return EventResult::Int(1);
+    }
+
     EventResult::Continue
 }
 
