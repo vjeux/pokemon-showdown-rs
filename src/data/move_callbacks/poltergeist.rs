3af22a18e5d450c80577d3ceecfcbdfee43af9d8
Developer <dev@example.com>
2025-12-27 22:20:54 +0000

    Implement poltergeist move callbacks

--- src/data/move_callbacks/poltergeist.rs
+++ src/data/move_callbacks/poltergeist.rs
@@ -11,15 +11,56 @@ use crate::event::EventResult;
 ///     return !!target.item;
 /// }
 pub fn on_try(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Bool(false),
+    };
+
+    // return !!target.item;
+    let has_item = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Bool(false),
+        };
+        target_pokemon.item.is_some()
+    };
+
+    EventResult::Bool(has_item)
 }
 
 /// onTryHit(target, source, move) {
 ///     this.add('-activate', target, 'move: Poltergeist', this.dex.items.get(target.item).name);
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let target = target_pos;
+
+    // this.add('-activate', target, 'move: Poltergeist', this.dex.items.get(target.item).name);
+    let (target_arg, item_name) = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        let target_arg = crate::battle::Arg::from(target_pokemon);
+
+        let item_id = match &target_pokemon.item {
+            Some(id) => id.clone(),
+            None => return EventResult::Continue,
+        };
+
+        let item_data = match battle.dex.get_item_by_id(&item_id) {
+            Some(item) => item,
+            None => return EventResult::Continue,
+        };
+
+        (target_arg, item_data.name.clone())
+    };
+
+    battle.add("-activate", &[
+        target_arg,
+        "move: Poltergeist".into(),
+        item_name.into(),
+    ]);
+
     EventResult::Continue
 }
 
