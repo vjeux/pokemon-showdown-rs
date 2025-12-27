b23098210cf960a92297422f6f6d0a5f72f7bfa2
Developer <dev@example.com>
2025-12-27 23:05:01 +0000

    Implement saltcure move callbacks

--- src/data/move_callbacks/saltcure.rs
+++ src/data/move_callbacks/saltcure.rs
@@ -15,7 +15,20 @@ pub mod condition {
     ///     this.add('-start', pokemon, 'Salt Cure');
     /// }
     pub fn on_start(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // this.add('-start', pokemon, 'Salt Cure');
+        let pokemon_arg = {
+            let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(pokemon)
+        };
+
+        battle.add("-start", &[
+            pokemon_arg,
+            "Salt Cure".into(),
+        ]);
+
         EventResult::Continue
     }
 
@@ -23,7 +36,23 @@ pub mod condition {
     ///     this.damage(pokemon.baseMaxhp / (pokemon.hasType(['Water', 'Steel']) ? 4 : 8));
     /// }
     pub fn on_residual(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        let pokemon = pokemon_pos;
+
+        // this.damage(pokemon.baseMaxhp / (pokemon.hasType(['Water', 'Steel']) ? 4 : 8));
+        let has_water_or_steel = battle.has_type(pokemon, "Water") || battle.has_type(pokemon, "Steel");
+
+        let divisor = if has_water_or_steel { 4 } else { 8 };
+
+        let damage = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.base_maxhp / divisor
+        };
+
+        battle.damage(damage, pokemon, None, None);
+
         EventResult::Continue
     }
 
@@ -31,7 +60,20 @@ pub mod condition {
     ///     this.add('-end', pokemon, 'Salt Cure');
     /// }
     pub fn on_end(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // this.add('-end', pokemon, 'Salt Cure');
+        let pokemon_arg = {
+            let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(pokemon)
+        };
+
+        battle.add("-end", &[
+            pokemon_arg,
+            "Salt Cure".into(),
+        ]);
+
         EventResult::Continue
     }
 }
