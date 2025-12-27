d5cb9404b818871b5389faeff80ac01b38c35303
Developer <dev@example.com>
2025-12-27 18:20:58 +0000

    Implement brickbreak onTryHit - remove screens before hitting

--- src/data/move_callbacks/brickbreak.rs
+++ src/data/move_callbacks/brickbreak.rs
@@ -6,6 +6,7 @@
 
 use crate::battle::Battle;
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// onTryHit(pokemon) {
 ///     // will shatter screens through sub, before you hit
@@ -14,7 +15,19 @@ use crate::event::EventResult;
 ///     pokemon.side.removeSideCondition('auroraveil');
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // will shatter screens through sub, before you hit
+    // pokemon.side.removeSideCondition('reflect');
+    // pokemon.side.removeSideCondition('lightscreen');
+    // pokemon.side.removeSideCondition('auroraveil');
+
+    let side_idx = target_pos.0;
+
+    if side_idx < battle.sides.len() {
+        battle.sides[side_idx].remove_side_condition(&ID::from("reflect"));
+        battle.sides[side_idx].remove_side_condition(&ID::from("lightscreen"));
+        battle.sides[side_idx].remove_side_condition(&ID::from("auroraveil"));
+    }
+
     EventResult::Continue
 }
 
