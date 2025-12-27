bfbd2985374c18e4458ec39775be98410cd65ed2
Developer <dev@example.com>
2025-12-27 17:55:34 +0000

    Implement axekick onMoveFail - crash damage on miss

--- src/data/move_callbacks/axekick.rs
+++ src/data/move_callbacks/axekick.rs
@@ -6,12 +6,30 @@
 
 use crate::battle::Battle;
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// onMoveFail(target, source, move) {
 ///     this.damage(source.baseMaxhp / 2, source, source, this.dex.conditions.get('High Jump Kick'));
 /// }
 pub fn on_move_fail(battle: &mut Battle, target_pos: Option<(usize, usize)>, source_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // Get the source
+    let source = match source_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // Get source pokemon to access base_maxhp
+    let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // this.damage(source.baseMaxhp / 2, source, source, this.dex.conditions.get('High Jump Kick'));
+    let damage_amount = source_pokemon.base_maxhp / 2;
+    let highjumpkick_id = ID::from("highjumpkick");
+
+    battle.damage(damage_amount, Some(source), Some(source), Some(&highjumpkick_id), false);
+
     EventResult::Continue
 }
 
