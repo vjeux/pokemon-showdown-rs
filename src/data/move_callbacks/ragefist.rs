52b6ee734a50052cdb68a212d8cb0f09197459ed
Developer <dev@example.com>
2025-12-27 22:39:27 +0000

    Implement ragefist move callbacks

--- src/data/move_callbacks/ragefist.rs
+++ src/data/move_callbacks/ragefist.rs
@@ -11,7 +11,19 @@ use crate::event::EventResult;
 ///     return Math.min(350, 50 + 50 * pokemon.timesAttacked);
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = pokemon_pos;
+
+    // return Math.min(350, 50 + 50 * pokemon.timesAttacked);
+    let power = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let base_power = 50 + 50 * pokemon_pokemon.times_attacked;
+        std::cmp::min(350, base_power)
+    };
+
+    EventResult::Int(power)
 }
 
