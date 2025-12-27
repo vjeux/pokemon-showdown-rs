bace1f37022a624d94da5d927e1ba1e21cbb7047
Developer <dev@example.com>
2025-12-27 22:11:47 +0000

    Implement painsplit move callback

--- src/data/move_callbacks/painsplit.rs
+++ src/data/move_callbacks/painsplit.rs
@@ -17,7 +17,89 @@ use crate::event::EventResult;
 ///     this.add('-sethp', pokemon, pokemon.getHealth, '[from] move: Pain Split');
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let pokemon = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // const targetHP = target.getUndynamaxedHP();
+    let target_hp = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.max_hp_undynamaxed
+    };
+
+    // const averagehp = Math.floor((targetHP + pokemon.hp) / 2) || 1;
+    let average_hp = {
+        let pokemon_hp = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.hp
+        };
+
+        let avg = (target_hp + pokemon_hp) / 2;
+        if avg == 0 { 1 } else { avg }
+    };
+
+    // const targetChange = targetHP - averagehp;
+    let target_change = target_hp - average_hp;
+
+    // target.sethp(target.hp - targetChange);
+    {
+        let target_current_hp = {
+            let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            target_pokemon.hp
+        };
+        let new_hp = target_current_hp - target_change;
+        battle.set_hp(new_hp, target, None, None);
+    }
+
+    // this.add('-sethp', target, target.getHealth, '[from] move: Pain Split', '[silent]');
+    {
+        let (target_arg, target_health) = {
+            let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            (crate::battle::Arg::from(target_pokemon), target_pokemon.get_health())
+        };
+
+        battle.add("-sethp", &[
+            target_arg,
+            target_health.into(),
+            "[from] move: Pain Split".into(),
+            "[silent]".into(),
+        ]);
+    }
+
+    // pokemon.sethp(averagehp);
+    battle.set_hp(average_hp, pokemon, None, None);
+
+    // this.add('-sethp', pokemon, pokemon.getHealth, '[from] move: Pain Split');
+    {
+        let (pokemon_arg, pokemon_health) = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            (crate::battle::Arg::from(pokemon_pokemon), pokemon_pokemon.get_health())
+        };
+
+        battle.add("-sethp", &[
+            pokemon_arg,
+            pokemon_health.into(),
+            "[from] move: Pain Split".into(),
+        ]);
+    }
+
     EventResult::Continue
 }
 
