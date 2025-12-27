5f42c481e7024a9e761b3e77049ca8cad74039c1
Developer <dev@example.com>
2025-12-27 22:47:44 +0000

    Implement relicsong move callbacks

--- src/data/move_callbacks/relicsong.rs
+++ src/data/move_callbacks/relicsong.rs
@@ -13,7 +13,27 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let pokemon = pokemon_pos;
+
+    // if (pokemon.baseSpecies.baseSpecies === 'Meloetta' && !pokemon.transformed) {
+    //     move.willChangeForme = true;
+    // }
+    let (base_species, transformed) = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        (pokemon_pokemon.base_species.base_species.clone(), pokemon_pokemon.transformed)
+    };
+
+    if base_species == "Meloetta" && !transformed {
+        let active_move = match &mut battle.active_move {
+            Some(m) => m,
+            None => return EventResult::Continue,
+        };
+        active_move.will_change_forme = true;
+    }
+
     EventResult::Continue
 }
 
@@ -24,7 +44,46 @@ pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Opti
 ///     }
 /// }
 pub fn on_after_move_secondary_self(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let pokemon = pokemon_pos;
+
+    // if (move.willChangeForme) {
+    let will_change_forme = {
+        let active_move = match &battle.active_move {
+            Some(m) => m,
+            None => return EventResult::Continue,
+        };
+        active_move.will_change_forme
+    };
+
+    if will_change_forme {
+        // const meloettaForme = pokemon.species.id === 'meloettapirouette' ? '' : '-Pirouette';
+        let meloetta_forme = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            if pokemon_pokemon.species.id == ID::from("meloettapirouette") {
+                ""
+            } else {
+                "-Pirouette"
+            }
+        };
+
+        // pokemon.formeChange('Meloetta' + meloettaForme, this.effect, false, '0', '[msg]');
+        let forme_name = format!("Meloetta{}", meloetta_forme);
+        let effect_id = {
+            let active_move = match &battle.active_move {
+                Some(m) => m,
+                None => return EventResult::Continue,
+            };
+            active_move.id.clone()
+        };
+
+        battle.forme_change(pokemon, &forme_name, Some(&effect_id), false, Some("0"), Some("[msg]"));
+    }
+
     EventResult::Continue
 }
 
