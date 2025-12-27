9ca86c39eb031a0c5a3ffd0bea4a85c346fddd9a
Developer <dev@example.com>
2025-12-27 21:42:22 +0100

    Implement Hyperspace Fury callback - Hoopa-Unbound only move restriction

--- src/data/move_callbacks/hyperspacefury.rs
+++ src/data/move_callbacks/hyperspacefury.rs
@@ -22,7 +22,47 @@ use crate::event::EventResult;
 ///     return null;
 /// }
 pub fn on_try(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let source = source_pos;
+
+    // if (source.species.name === 'Hoopa-Unbound') {
+    //     return;
+    // }
+    let species_name = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.species.name.clone()
+    };
+
+    if species_name == "Hoopa-Unbound" {
+        return EventResult::Continue;
+    }
+
+    // this.hint("Only a Pokemon whose form is Hoopa Unbound can use this move.");
+    battle.hint("Only a Pokemon whose form is Hoopa Unbound can use this move.");
+
+    // if (source.species.name === 'Hoopa') {
+    if species_name == "Hoopa" {
+        // this.attrLastMove('[still]');
+        battle.attr_last_move("[still]");
+
+        // this.add('-fail', source, 'move: Hyperspace Fury', '[forme]');
+        let source_arg = crate::battle::Arg::Pos(source.0, source.1);
+        battle.add("-fail", &[source_arg, "move: Hyperspace Fury".into(), "[forme]".into()]);
+
+        // return null;
+        return EventResult::Null;
+    }
+
+    // this.attrLastMove('[still]');
+    battle.attr_last_move("[still]");
+
+    // this.add('-fail', source, 'move: Hyperspace Fury');
+    let source_arg = crate::battle::Arg::Pos(source.0, source.1);
+    battle.add("-fail", &[source_arg, "move: Hyperspace Fury".into()]);
+
+    // return null;
+    EventResult::Null
 }
 
