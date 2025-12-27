d614e73a870e567a970af27a0e82b8fdd6bf4c58
Developer <dev@example.com>
2025-12-27 16:19:36 +0000

    Implement naturesmadness move callback
    Line-by-line implementation from TypeScript:
    - Deals exactly 50% of target undynamaxed HP
    - Minimum damage is 1
    - Simple fixed damage move

--- src/data/move_callbacks/naturesmadness.rs
+++ src/data/move_callbacks/naturesmadness.rs
@@ -11,7 +11,18 @@ use crate::event::EventResult;
 ///     return this.clampIntRange(Math.floor(target.getUndynamaxedHP() / 2), 1);
 /// }
 pub fn damage_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let target_pos = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    let target = match battle.pokemon_at(target_pos.0, target_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // Deals half of target's undynamaxed HP
+    let damage = target.get_undynamaxed_hp() / 2;
+    EventResult::Number(battle.clamp_int_range(damage, 1))
 }
 
