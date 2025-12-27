36f08be15ded14b40acdb1aa2cb930f0e4578627
Developer <dev@example.com>
2025-12-27 21:04:13 +0100

    Implement glaiverush callbacks - guarantees next hit and doubles damage taken

--- src/data/move_callbacks/glaiverush.rs
+++ src/data/move_callbacks/glaiverush.rs
@@ -15,7 +15,18 @@ pub mod condition {
     ///     this.add('-singlemove', pokemon, 'Glaive Rush', '[silent]');
     /// }
     pub fn on_start(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        let pokemon = pokemon_pos;
+
+        // this.add('-singlemove', pokemon, 'Glaive Rush', '[silent]');
+        let pokemon_arg = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(pokemon_pokemon)
+        };
+        battle.add("-singlemove", &[pokemon_arg, "Glaive Rush".into(), "[silent]".into()]);
+
         EventResult::Continue
     }
 
@@ -23,16 +34,16 @@ pub mod condition {
     ///     return true;
     /// }
     pub fn on_accuracy(battle: &mut Battle) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
-        EventResult::Continue
+        // return true;
+        EventResult::Bool(true)
     }
 
     /// onSourceModifyDamage() {
     ///     return this.chainModify(2);
     /// }
     pub fn on_source_modify_damage(battle: &mut Battle) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
-        EventResult::Continue
+        // return this.chainModify(2);
+        EventResult::ChainModify(2.0)
     }
 
     /// onBeforeMove(pokemon) {
@@ -40,7 +51,20 @@ pub mod condition {
     ///     pokemon.removeVolatile('glaiverush');
     /// }
     pub fn on_before_move(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        use crate::dex_data::ID;
+
+        let pokemon = pokemon_pos;
+
+        // this.debug('removing Glaive Rush drawback before attack');
+        battle.debug("removing Glaive Rush drawback before attack");
+
+        // pokemon.removeVolatile('glaiverush');
+        let pokemon_pokemon = match battle.pokemon_at_mut(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.remove_volatile(&ID::from("glaiverush"));
+
         EventResult::Continue
     }
 }
