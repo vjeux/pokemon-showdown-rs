113446eeb9021446bea9532270eef2d364b6df77
Developer <dev@example.com>
2025-12-27 19:42:45 +0100

    Implement collisioncourse callback - boosts power on super effective hits

--- src/data/move_callbacks/collisioncourse.rs
+++ src/data/move_callbacks/collisioncourse.rs
@@ -6,6 +6,7 @@
 
 use crate::battle::Battle;
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// onBasePower(basePower, source, target, move) {
 ///     if (target.runEffectiveness(move) > 0) {
@@ -15,7 +16,35 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_base_power(battle: &mut Battle, base_power: i32, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // Get the target
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // Get the active move
+    let move_id = match &battle.active_move {
+        Some(id) => id.clone(),
+        None => return EventResult::Continue,
+    };
+
+    // if (target.runEffectiveness(move) > 0) {
+    let effectiveness = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.run_effectiveness(&move_id, battle)
+    };
+
+    if effectiveness > 0 {
+        // this.debug(`collision course super effective buff`);
+        // TODO: debug not yet implemented
+
+        // return this.chainModify([5461, 4096]);
+        let result = battle.chain_modify_fraction(5461, 4096);
+        return EventResult::Int(result);
+    }
+
     EventResult::Continue
 }
-
