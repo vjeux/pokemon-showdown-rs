5bb2b0913f7e2c157cdd783ae0baa4357ab0ac8c
Developer <dev@example.com>
2025-12-27 21:57:16 +0000

    Implement mortalspin move callbacks

--- src/data/move_callbacks/mortalspin.rs
+++ src/data/move_callbacks/mortalspin.rs
@@ -24,7 +24,137 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_after_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let pokemon = source_pos;
+
+    // if (!move.hasSheerForce) {
+    let has_sheer_force = {
+        let active_move = match &battle.active_move {
+            Some(m) => m,
+            None => return EventResult::Continue,
+        };
+        active_move.has_sheer_force
+    };
+
+    if has_sheer_force {
+        return EventResult::Continue;
+    }
+
+    // if (pokemon.hp && pokemon.removeVolatile('leechseed')) {
+    let pokemon_hp = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.hp
+    };
+
+    if pokemon_hp > 0 {
+        let removed_leechseed = {
+            let pokemon_pokemon = match battle.pokemon_at_mut(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.remove_volatile(&ID::from("leechseed"))
+        };
+
+        if removed_leechseed {
+            // this.add('-end', pokemon, 'Leech Seed', '[from] move: Mortal Spin', `[of] ${pokemon}`);
+            let pokemon_arg = {
+                let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                    Some(p) => p,
+                    None => return EventResult::Continue,
+                };
+                crate::battle::Arg::from(pokemon_pokemon)
+            };
+
+            battle.add("-end", &[
+                pokemon_arg.clone(),
+                "Leech Seed".into(),
+                "[from] move: Mortal Spin".into(),
+                format!("[of] {}", pokemon_arg).into(),
+            ]);
+        }
+    }
+
+    // const sideConditions = ['spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge'];
+    let side_conditions = [
+        ID::from("spikes"),
+        ID::from("toxicspikes"),
+        ID::from("stealthrock"),
+        ID::from("stickyweb"),
+        ID::from("gmaxsteelsurge"),
+    ];
+
+    // for (const condition of sideConditions) {
+    for condition in &side_conditions {
+        let pokemon_hp = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.hp
+        };
+
+        if pokemon_hp > 0 {
+            // if (pokemon.hp && pokemon.side.removeSideCondition(condition)) {
+            let removed = battle.remove_side_condition(pokemon.0, condition);
+
+            if removed {
+                // this.add('-sideend', pokemon.side, this.dex.conditions.get(condition).name, '[from] move: Mortal Spin', `[of] ${pokemon}`);
+                let condition_name = battle.dex.get_condition_by_id(condition)
+                    .map(|c| c.name.clone())
+                    .unwrap_or_else(|| condition.to_string());
+
+                let (side_arg, pokemon_arg) = {
+                    let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                        Some(p) => p,
+                        None => return EventResult::Continue,
+                    };
+                    let side_arg = crate::battle::Arg::Side(pokemon.0);
+                    let pokemon_arg = crate::battle::Arg::from(pokemon_pokemon);
+                    (side_arg, pokemon_arg)
+                };
+
+                battle.add("-sideend", &[
+                    side_arg,
+                    condition_name.into(),
+                    "[from] move: Mortal Spin".into(),
+                    format!("[of] {}", pokemon_arg).into(),
+                ]);
+            }
+        }
+    }
+
+    // if (pokemon.hp && pokemon.volatiles['partiallytrapped']) {
+    let pokemon_hp = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.hp
+    };
+
+    if pokemon_hp > 0 {
+        let has_partiallytrapped = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.volatiles.contains_key(&ID::from("partiallytrapped"))
+        };
+
+        if has_partiallytrapped {
+            // pokemon.removeVolatile('partiallytrapped');
+            let pokemon_pokemon = match battle.pokemon_at_mut(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.remove_volatile(&ID::from("partiallytrapped"));
+        }
+    }
+
     EventResult::Continue
 }
 
@@ -45,7 +175,137 @@ pub fn on_after_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos:
 ///     }
 /// }
 pub fn on_after_sub_damage(battle: &mut Battle, damage: i32, target_pos: Option<(usize, usize)>, pokemon_pos: (usize, usize), move_id: &str) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let pokemon = pokemon_pos;
+
+    // if (!move.hasSheerForce) {
+    let has_sheer_force = {
+        let active_move = match &battle.active_move {
+            Some(m) => m,
+            None => return EventResult::Continue,
+        };
+        active_move.has_sheer_force
+    };
+
+    if has_sheer_force {
+        return EventResult::Continue;
+    }
+
+    // if (pokemon.hp && pokemon.removeVolatile('leechseed')) {
+    let pokemon_hp = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.hp
+    };
+
+    if pokemon_hp > 0 {
+        let removed_leechseed = {
+            let pokemon_pokemon = match battle.pokemon_at_mut(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.remove_volatile(&ID::from("leechseed"))
+        };
+
+        if removed_leechseed {
+            // this.add('-end', pokemon, 'Leech Seed', '[from] move: Mortal Spin', `[of] ${pokemon}`);
+            let pokemon_arg = {
+                let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                    Some(p) => p,
+                    None => return EventResult::Continue,
+                };
+                crate::battle::Arg::from(pokemon_pokemon)
+            };
+
+            battle.add("-end", &[
+                pokemon_arg.clone(),
+                "Leech Seed".into(),
+                "[from] move: Mortal Spin".into(),
+                format!("[of] {}", pokemon_arg).into(),
+            ]);
+        }
+    }
+
+    // const sideConditions = ['spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge'];
+    let side_conditions = [
+        ID::from("spikes"),
+        ID::from("toxicspikes"),
+        ID::from("stealthrock"),
+        ID::from("stickyweb"),
+        ID::from("gmaxsteelsurge"),
+    ];
+
+    // for (const condition of sideConditions) {
+    for condition in &side_conditions {
+        let pokemon_hp = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.hp
+        };
+
+        if pokemon_hp > 0 {
+            // if (pokemon.hp && pokemon.side.removeSideCondition(condition)) {
+            let removed = battle.remove_side_condition(pokemon.0, condition);
+
+            if removed {
+                // this.add('-sideend', pokemon.side, this.dex.conditions.get(condition).name, '[from] move: Mortal Spin', `[of] ${pokemon}`);
+                let condition_name = battle.dex.get_condition_by_id(condition)
+                    .map(|c| c.name.clone())
+                    .unwrap_or_else(|| condition.to_string());
+
+                let (side_arg, pokemon_arg) = {
+                    let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                        Some(p) => p,
+                        None => return EventResult::Continue,
+                    };
+                    let side_arg = crate::battle::Arg::Side(pokemon.0);
+                    let pokemon_arg = crate::battle::Arg::from(pokemon_pokemon);
+                    (side_arg, pokemon_arg)
+                };
+
+                battle.add("-sideend", &[
+                    side_arg,
+                    condition_name.into(),
+                    "[from] move: Mortal Spin".into(),
+                    format!("[of] {}", pokemon_arg).into(),
+                ]);
+            }
+        }
+    }
+
+    // if (pokemon.hp && pokemon.volatiles['partiallytrapped']) {
+    let pokemon_hp = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.hp
+    };
+
+    if pokemon_hp > 0 {
+        let has_partiallytrapped = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.volatiles.contains_key(&ID::from("partiallytrapped"))
+        };
+
+        if has_partiallytrapped {
+            // pokemon.removeVolatile('partiallytrapped');
+            let pokemon_pokemon = match battle.pokemon_at_mut(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.remove_volatile(&ID::from("partiallytrapped"));
+        }
+    }
+
     EventResult::Continue
 }
 
