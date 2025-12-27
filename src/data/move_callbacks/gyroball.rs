f25e0de0b952f5c2a3601d85b86168487f98a538
Developer <dev@example.com>
2025-12-27 21:30:13 +0100

    Implement gyroball callback - power based on speed ratio

--- src/data/move_callbacks/gyroball.rs
+++ src/data/move_callbacks/gyroball.rs
@@ -15,7 +15,47 @@ use crate::event::EventResult;
 ///     return power;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // let power = Math.floor(25 * target.getStat('spe') / pokemon.getStat('spe')) + 1;
+    let target_spe = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.get_stat("spe", battle)
+    };
+
+    let pokemon_spe = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.get_stat("spe", battle)
+    };
+
+    let mut power = if pokemon_spe == 0 {
+        1
+    } else {
+        (25 * target_spe / pokemon_spe) + 1
+    };
+
+    // if (!isFinite(power)) power = 1;
+    // Already handled by the division check above
+
+    // if (power > 150) power = 150;
+    if power > 150 {
+        power = 150;
+    }
+
+    // this.debug(`BP: ${power}`);
+    battle.debug(&format!("BP: {}", power));
+
+    // return power;
+    EventResult::Int(power)
 }
 
