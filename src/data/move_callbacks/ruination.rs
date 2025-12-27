a01d749327f99f56155bbb8ea04619de5490c0a3
Developer <dev@example.com>
2025-12-27 23:01:48 +0000

    Implement ruination move callbacks

--- src/data/move_callbacks/ruination.rs
+++ src/data/move_callbacks/ruination.rs
@@ -11,7 +11,22 @@ use crate::event::EventResult;
 ///     return this.clampIntRange(Math.floor(target.getUndynamaxedHP() / 2), 1);
 /// }
 pub fn damage_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // return this.clampIntRange(Math.floor(target.getUndynamaxedHP() / 2), 1);
+    let undynamaxed_hp = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.get_undynamaxed_hp()
+    };
+
+    let damage = (undynamaxed_hp / 2).max(1);
+
+    EventResult::Int(damage)
 }
 
