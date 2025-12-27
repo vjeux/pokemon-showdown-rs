a15db2fee2a5ef03f7d410f0ccc379e0d1e4e3e2
Developer <dev@example.com>
2025-12-27 20:09:40 +0100

    Implement doubleshock callback - requires Electric type

--- src/data/move_callbacks/doubleshock.rs
+++ src/data/move_callbacks/doubleshock.rs
@@ -14,7 +14,39 @@ use crate::event::EventResult;
 ///     return null;
 /// }
 pub fn on_try_move(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    // if (pokemon.hasType('Electric')) return;
+    let pokemon = source_pos;
+
+    let has_electric = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.has_type(&ID::from("electric"), battle)
+    };
+
+    if has_electric {
+        // return;
+        return EventResult::Continue;
+    }
+
+    // this.add('-fail', pokemon, 'move: Double Shock');
+    let pokemon_arg = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        crate::battle::Arg::from(pokemon_pokemon)
+    };
+
+    battle.add("-fail", &[pokemon_arg, "move: Double Shock".into()]);
+
+    // this.attrLastMove('[still]');
+    battle.attr_last_move("[still]");
+
+    // return null;
+    EventResult::Null
 }
 
