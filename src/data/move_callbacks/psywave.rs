7df884cb64343af143d657273d2687789e7ee65f
Developer <dev@example.com>
2025-12-27 22:36:21 +0000

    Implement psywave move callbacks

--- src/data/move_callbacks/psywave.rs
+++ src/data/move_callbacks/psywave.rs
@@ -11,7 +11,21 @@ use crate::event::EventResult;
 ///     return (this.random(50, 151) * pokemon.level) / 100;
 /// }
 pub fn damage_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = pokemon_pos;
+
+    // return (this.random(50, 151) * pokemon.level) / 100;
+    let rand = battle.random_range(50, 151);
+
+    let level = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.level
+    };
+
+    let damage = (rand * level) / 100;
+
+    EventResult::Int(damage)
 }
 
