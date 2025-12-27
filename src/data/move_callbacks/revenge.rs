84c901936fef1ff2d94804423e48ef6f7786faad
Developer <dev@example.com>
2025-12-27 22:52:45 +0000

    Implement revenge move callbacks

--- src/data/move_callbacks/revenge.rs
+++ src/data/move_callbacks/revenge.rs
@@ -18,7 +18,57 @@ use crate::event::EventResult;
 ///     return move.basePower;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // const damagedByTarget = pokemon.attackedBy.some(
+    //     p => p.source === target && p.damage > 0 && p.thisTurn
+    // );
+    let damaged_by_target = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.attacked_by.iter().any(|p| {
+            p.source == Some(target) && p.damage > 0 && p.this_turn
+        })
+    };
+
+    // if (damagedByTarget) {
+    if damaged_by_target {
+        // this.debug(`BP doubled for getting hit by ${target}`);
+        let target_arg = {
+            let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(target_pokemon)
+        };
+        battle.debug(&format!("BP doubled for getting hit by {}", target_arg));
+
+        // return move.basePower * 2;
+        let base_power = {
+            let active_move = match &battle.active_move {
+                Some(m) => m,
+                None => return EventResult::Continue,
+            };
+            active_move.base_power
+        };
+        return EventResult::Int(base_power * 2);
+    }
+
+    // return move.basePower;
+    let base_power = {
+        let active_move = match &battle.active_move {
+            Some(m) => m,
+            None => return EventResult::Continue,
+        };
+        active_move.base_power
+    };
+
+    EventResult::Int(base_power)
 }
 
