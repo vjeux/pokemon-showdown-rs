a3cd2679c1f503a1c5f1939f404ab76fbc02b9c7
Developer <dev@example.com>
2025-12-27 22:11:20 +0100

    Implement Low Kick move callbacks
    Implements both callbacks for Low Kick, a weight-based damage move:
    
    - basePowerCallback: Calculates base power based on target weight:
      >= 2000: 120 BP, >= 1000: 100 BP, >= 500: 80 BP, >= 250: 60 BP,
      >= 100: 40 BP, < 100: 20 BP. Outputs debug message with calculated BP
    
    - onTryHit: Fails if target is Dynamaxed (has dynamax volatile),
      displays -fail message with Dynamax reason, sets last move attribute
      to [still], and returns Null to prevent the move from executing

--- src/data/move_callbacks/lowkick.rs
+++ src/data/move_callbacks/lowkick.rs
@@ -27,8 +27,53 @@ use crate::event::EventResult;
 ///     return bp;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // const targetWeight = target.getWeight();
+    let target_weight = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.get_weight(battle)
+    };
+
+    // let bp;
+    // if (targetWeight >= 2000) {
+    //     bp = 120;
+    // } else if (targetWeight >= 1000) {
+    //     bp = 100;
+    // } else if (targetWeight >= 500) {
+    //     bp = 80;
+    // } else if (targetWeight >= 250) {
+    //     bp = 60;
+    // } else if (targetWeight >= 100) {
+    //     bp = 40;
+    // } else {
+    //     bp = 20;
+    // }
+    let bp = if target_weight >= 2000 {
+        120
+    } else if target_weight >= 1000 {
+        100
+    } else if target_weight >= 500 {
+        80
+    } else if target_weight >= 250 {
+        60
+    } else if target_weight >= 100 {
+        40
+    } else {
+        20
+    };
+
+    // this.debug(`BP: ${bp}`);
+    battle.debug(&format!("BP: {}", bp));
+
+    // return bp;
+    EventResult::Int(bp)
 }
 
 /// onTryHit(target, pokemon, move) {
@@ -39,7 +84,32 @@ pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), tar
 ///     }
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let pokemon = source_pos;
+    let target = target_pos;
+
+    // if (target.volatiles['dynamax']) {
+    let has_dynamax = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.volatiles.contains_key(&ID::from("dynamax"))
+    };
+
+    if has_dynamax {
+        //     this.add('-fail', pokemon, 'Dynamax');
+        let pokemon_arg = crate::battle::Arg::Pos(pokemon.0, pokemon.1);
+        battle.add("-fail", &[pokemon_arg, "Dynamax".into()]);
+
+        //     this.attrLastMove('[still]');
+        battle.attr_last_move("[still]");
+
+        //     return null;
+        return EventResult::Null;
+    }
+
     EventResult::Continue
 }
 
