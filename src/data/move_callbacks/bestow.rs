8d93e636a61295bb00aa81c298cc3cf80a1f160f
Developer <dev@example.com>
2025-12-27 18:10:48 +0000

    Implement bestow onHit - give item to target

--- src/data/move_callbacks/bestow.rs
+++ src/data/move_callbacks/bestow.rs
@@ -4,8 +4,9 @@
 //!
 //! Generated from data/moves.ts
 
-use crate::battle::Battle;
+use crate::battle::{Battle, Arg};
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// onHit(target, source, move) {
 ///     if (target.item) {
@@ -20,7 +21,95 @@ use crate::event::EventResult;
 ///     this.add('-item', target, myItem.name, '[from] move: Bestow', `[of] ${source}`);
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // Get target
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (target.item) {
+    //     return false;
+    // }
+    {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        if !target_pokemon.item.is_empty() {
+            return EventResult::Bool(false);
+        }
+    }
+
+    // const myItem = source.takeItem();
+    let my_item = {
+        let source_pokemon = match battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        source_pokemon.take_item()
+    };
+
+    // if (!myItem) return false;
+    let my_item = match my_item {
+        Some(item) => item,
+        None => return EventResult::Bool(false),
+    };
+
+    // if (!this.singleEvent('TakeItem', myItem, source.itemState, target, source, move, myItem) || !target.setItem(myItem)) {
+    let take_item_event = battle.single_event("TakeItem", &my_item, Some(target), Some(pokemon_pos), None);
+
+    let set_item_success = {
+        let target_pokemon = match battle.pokemon_at_mut(target.0, target.1) {
+            Some(p) => p,
+            None => false,
+        };
+
+        if let Some(target_pokemon) = target_pokemon {
+            target_pokemon.set_item(my_item.clone())
+        } else {
+            false
+        }
+    };
+
+    if !take_item_event || !set_item_success {
+        // source.item = myItem.id;
+        let source_pokemon = match battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.item = my_item;
+
+        // return false;
+        return EventResult::Bool(false);
+    }
+
+    // this.add('-item', target, myItem.name, '[from] move: Bestow', `[of] ${source}`);
+    let (item_name, target_arg, source_arg) = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let source_pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let item_data = battle.dex.get_item_by_id(&my_item);
+        let item_name = item_data.map(|i| i.name.clone()).unwrap_or_else(|| my_item.to_string());
+
+        (item_name, Arg::from(target_pokemon), Arg::from(source_pokemon))
+    };
+
+    battle.add("-item", &[
+        target_arg,
+        item_name.into(),
+        "[from] move: Bestow".into(),
+        format!("[of] {}", source_arg.to_protocol_string()).into(),
+    ]);
+
     EventResult::Continue
 }
 
