73ce5899599f637eaa3d144ab0226ec429b044ad
Developer <dev@example.com>
2025-12-27 16:14:22 +0100

    Implement torment.rs on_disable_move callback

--- src/data/move_callbacks/torment.rs
+++ src/data/move_callbacks/torment.rs
@@ -20,7 +20,9 @@ pub mod condition {
     ///     this.add('-start', pokemon, 'Torment');
     /// }
     pub fn on_start(battle: &mut Battle, pokemon_pos: (usize, usize), source_pos: Option<(usize, usize)>, effect_id: Option<&str>) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // TODO: Check for dynamax volatile
+        // TODO: Check effect_id for gmaxmeltdown
+        // TODO: battle.add('-start', pokemon, 'Torment');
         EventResult::Continue
     }
 
@@ -28,7 +30,7 @@ pub mod condition {
     ///     this.add('-end', pokemon, 'Torment');
     /// }
     pub fn on_end(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // TODO: battle.add('-end', pokemon, 'Torment');
         EventResult::Continue
     }
 
@@ -36,7 +38,24 @@ pub mod condition {
     ///     if (pokemon.lastMove && pokemon.lastMove.id !== 'struggle') pokemon.disableMove(pokemon.lastMove.id);
     /// }
     pub fn on_disable_move(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        // Torment prevents using the same move twice in a row
+        if let Some(ref last_move) = pokemon.last_move {
+            if last_move.as_str() != "struggle" {
+                // Temporarily store last_move to avoid borrow checker issues
+                let last_move_id = last_move.clone();
+                drop(pokemon); // Release borrow
+
+                if let Some(pokemon_mut) = battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+                    pokemon_mut.disable_move(&last_move_id.as_str(), None);
+                }
+            }
+        }
+
         EventResult::Continue
     }
 }
