341eadfac00235f60674347921e56d2a61c6c4e7
Developer <dev@example.com>
2025-12-27 19:37:58 +0100

    Implement celebrate callback - adds activate message

--- src/data/move_callbacks/celebrate.rs
+++ src/data/move_callbacks/celebrate.rs
@@ -4,14 +4,23 @@
 //!
 //! Generated from data/moves.ts
 
-use crate::battle::Battle;
+use crate::battle::{Battle, Arg};
 use crate::event::EventResult;
 
 /// onTryHit(target, source) {
 ///     this.add('-activate', target, 'move: Celebrate');
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // this.add('-activate', target, 'move: Celebrate');
+    let target_arg = {
+        let target = match battle.pokemon_at(target_pos.0, target_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        Arg::from(target)
+    };
+
+    battle.add("-activate", &[target_arg, "move: Celebrate".into()]);
+
     EventResult::Continue
 }
-
