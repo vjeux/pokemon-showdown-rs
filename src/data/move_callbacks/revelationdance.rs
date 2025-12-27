1c75ac54f69001d99d7d41d56241af7de5f05763
Developer <dev@example.com>
2025-12-27 22:51:54 +0000

    Implement revelationdance move callbacks

--- src/data/move_callbacks/revelationdance.rs
+++ src/data/move_callbacks/revelationdance.rs
@@ -15,7 +15,33 @@ use crate::event::EventResult;
 ///     move.type = type;
 /// }
 pub fn on_modify_type(battle: &mut Battle, move_id: &str, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let pokemon = pokemon_pos;
+
+    // const types = pokemon.getTypes();
+    let types = battle.get_types(pokemon, false);
+
+    // let type = types[0];
+    let mut type_str = types.get(0).cloned().unwrap_or_else(|| "Normal".to_string());
+
+    // if (type === 'Bird') type = '???';
+    if type_str == "Bird" {
+        type_str = "???".to_string();
+    }
+
+    // if (type === '???' && types[1]) type = types[1];
+    if type_str == "???" {
+        if let Some(second_type) = types.get(1) {
+            type_str = second_type.clone();
+        }
+    }
+
+    // move.type = type;
+    let active_move = match &mut battle.active_move {
+        Some(m) => m,
+        None => return EventResult::Continue,
+    };
+    active_move.move_type = type_str;
+
     EventResult::Continue
 }
 
