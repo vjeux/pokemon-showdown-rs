9fb1d370eedda486713162acd8eb9cacaa9ed159
Developer <dev@example.com>
2025-12-27 23:28:19 +0000

    Implement sleeptalk move callbacks

--- src/data/move_callbacks/sleeptalk.rs
+++ src/data/move_callbacks/sleeptalk.rs
@@ -11,8 +11,27 @@ use crate::event::EventResult;
 ///     return source.status === 'slp' || source.hasAbility('comatose');
 /// }
 pub fn on_try(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    // onTry(source) {
+    //     return source.status === 'slp' || source.hasAbility('comatose');
+    // }
+    let source = source_pos;
+
+    // return source.status === 'slp' || source.hasAbility('comatose');
+    let has_sleep_or_comatose = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let has_sleep = source_pokemon.status == Some(ID::from("slp"));
+        let has_comatose = battle.has_ability(source, "comatose");
+
+        has_sleep || has_comatose
+    };
+
+    EventResult::Bool(has_sleep_or_comatose)
 }
 
 /// onHit(pokemon) {
@@ -34,7 +53,89 @@ pub fn on_try(battle: &mut Battle, source_pos: (usize, usize), target_pos: Optio
 ///     this.actions.useMove(randomMove, pokemon);
 /// }
 pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    // onHit(pokemon) {
+    //     const moves = [];
+    //     for (const moveSlot of pokemon.moveSlots) {
+    //         const moveid = moveSlot.id;
+    //         if (!moveid) continue;
+    //         const move = this.dex.moves.get(moveid);
+    //         if (move.flags['nosleeptalk'] || move.flags['charge'] || (move.isZ && move.basePower !== 1) || move.isMax) {
+    //             continue;
+    //         }
+    //         moves.push(moveid);
+    //     }
+    //     let randomMove = '';
+    //     if (moves.length) randomMove = this.sample(moves);
+    //     if (!randomMove) {
+    //         return false;
+    //     }
+    //     this.actions.useMove(randomMove, pokemon);
+    // }
+    let pokemon = pokemon_pos;
+
+    // const moves = [];
+    let mut moves = Vec::new();
+
+    // for (const moveSlot of pokemon.moveSlots) {
+    let move_slots = {
+        let pokemon_data = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_data.move_slots.clone()
+    };
+
+    for move_slot in &move_slots {
+        // const moveid = moveSlot.id;
+        let moveid = &move_slot.id;
+
+        // if (!moveid) continue;
+        if moveid.is_empty() {
+            continue;
+        }
+
+        // const move = this.dex.moves.get(moveid);
+        let move_data = battle.dex.get_move_by_id(moveid);
+        let move_data = match move_data {
+            Some(m) => m,
+            None => continue,
+        };
+
+        // if (move.flags['nosleeptalk'] || move.flags['charge'] || (move.isZ && move.basePower !== 1) || move.isMax) {
+        //     continue;
+        // }
+        if move_data.flags.nosleeptalk.unwrap_or(0) != 0 ||
+           move_data.flags.charge.unwrap_or(0) != 0 ||
+           (move_data.is_z && move_data.base_power != 1) ||
+           move_data.is_max {
+            continue;
+        }
+
+        // moves.push(moveid);
+        moves.push(moveid.clone());
+    }
+
+    // let randomMove = '';
+    // if (moves.length) randomMove = this.sample(moves);
+    let random_move = if !moves.is_empty() {
+        battle.sample(&moves)
+    } else {
+        None
+    };
+
+    // if (!randomMove) {
+    //     return false;
+    // }
+    let random_move = match random_move {
+        Some(m) => m,
+        None => return EventResult::Bool(false),
+    };
+
+    // this.actions.useMove(randomMove, pokemon);
+    battle.use_move(&random_move, pokemon);
+
     EventResult::Continue
 }
 
