661ace9b7cdb7fab869b363f72e19856cf630236
Developer <dev@example.com>
2025-12-27 17:58:49 +0000

    Implement barbbarrage onBasePower - doubles power against poisoned targets

--- src/data/move_callbacks/barbbarrage.rs
+++ src/data/move_callbacks/barbbarrage.rs
@@ -6,6 +6,7 @@
 
 use crate::battle::Battle;
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// onBasePower(basePower, pokemon, target) {
 ///     if (target.status === 'psn' || target.status === 'tox') {
@@ -13,7 +14,25 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_base_power(battle: &mut Battle, base_power: i32, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // Get the target
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // Get target pokemon
+    let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // if (target.status === 'psn' || target.status === 'tox') {
+    //     return this.chainModify(2);
+    // }
+    if target_pokemon.status == ID::from("psn") || target_pokemon.status == ID::from("tox") {
+        battle.chain_modify(2.0);
+    }
+
     EventResult::Continue
 }
 
