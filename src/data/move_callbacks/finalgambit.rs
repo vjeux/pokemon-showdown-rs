3a9dcbcc43a80d56ca224dcde45aef2714eda418
Developer <dev@example.com>
2025-12-27 16:17:58 +0000

    Implement finalgambit move callback
    Line-by-line implementation from TypeScript:
    - Damage equals user current HP
    - User faints after using the move (selfdestruct)
    - Returns damage amount to deal to target

--- src/data/move_callbacks/finalgambit.rs
+++ src/data/move_callbacks/finalgambit.rs
@@ -13,7 +13,19 @@ use crate::event::EventResult;
 ///     return damage;
 /// }
 pub fn damage_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // Damage is equal to user's current HP
+    let damage = pokemon.hp;
+
+    // Faint the user (selfdestruct)
+    if let Some(pokemon) = battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+        pokemon.faint();
+    }
+
+    EventResult::Number(damage)
 }
 
