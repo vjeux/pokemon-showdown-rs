274d77991ae0a001998ada99856548872c99f1ba
Developer <dev@example.com>
2025-12-27 23:13:55 +0000

    Implement shelltrap move callbacks

--- src/data/move_callbacks/shelltrap.rs
+++ src/data/move_callbacks/shelltrap.rs
@@ -11,7 +11,15 @@ use crate::event::EventResult;
 ///     pokemon.addVolatile('shelltrap');
 /// }
 pub fn priority_charge_callback(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    // priorityChargeCallback(pokemon) {
+    //     pokemon.addVolatile('shelltrap');
+    // }
+    let pokemon = pokemon_pos;
+
+    battle.add_volatile(&ID::from("shelltrap"), pokemon, None, None);
+
     EventResult::Continue
 }
 
@@ -23,7 +31,54 @@ pub fn priority_charge_callback(battle: &mut Battle, pokemon_pos: (usize, usize)
 ///     }
 /// }
 pub fn on_try_move(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    // onTryMove(pokemon) {
+    //     if (!pokemon.volatiles['shelltrap']?.gotHit) {
+    //         this.attrLastMove('[still]');
+    //         this.add('cant', pokemon, 'Shell Trap', 'Shell Trap');
+    //         return null;
+    //     }
+    // }
+    let pokemon = source_pos;
+
+    // if (!pokemon.volatiles['shelltrap']?.gotHit) {
+    let got_hit = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        if let Some(volatile) = pokemon_pokemon.volatiles.get(&ID::from("shelltrap")) {
+            volatile.got_hit.unwrap_or(false)
+        } else {
+            false
+        }
+    };
+
+    if !got_hit {
+        // this.attrLastMove('[still]');
+        battle.attr_last_move("[still]");
+
+        // this.add('cant', pokemon, 'Shell Trap', 'Shell Trap');
+        let pokemon_arg = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(pokemon_pokemon)
+        };
+
+        battle.add("cant", &[
+            pokemon_arg,
+            "Shell Trap".into(),
+            "Shell Trap".into(),
+        ]);
+
+        // return null;
+        return EventResult::Null;
+    }
+
     EventResult::Continue
 }
 
@@ -34,7 +89,24 @@ pub mod condition {
     ///     this.add('-singleturn', pokemon, 'move: Shell Trap');
     /// }
     pub fn on_start(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // onStart(pokemon) {
+        //     this.add('-singleturn', pokemon, 'move: Shell Trap');
+        // }
+        let pokemon = pokemon_pos;
+
+        let pokemon_arg = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            crate::battle::Arg::from(pokemon_pokemon)
+        };
+
+        battle.add("-singleturn", &[
+            pokemon_arg,
+            "move: Shell Trap".into(),
+        ]);
+
         EventResult::Continue
     }
 
@@ -48,7 +120,49 @@ pub mod condition {
     ///     }
     /// }
     pub fn on_hit(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        use crate::dex_data::ID;
+
+        // onHit(pokemon, source, move) {
+        //     if (!pokemon.isAlly(source) && move.category === 'Physical') {
+        //         this.effectState.gotHit = true;
+        //         const action = this.queue.willMove(pokemon);
+        //         if (action) {
+        //             this.queue.prioritizeAction(action);
+        //         }
+        //     }
+        // }
+        let pokemon = pokemon_pos;
+        let source = match target_pos {
+            Some(pos) => pos,
+            None => return EventResult::Continue,
+        };
+
+        // if (!pokemon.isAlly(source) && move.category === 'Physical') {
+        let is_ally = battle.is_ally(pokemon, source);
+        let is_physical = {
+            let active_move = match &battle.active_move {
+                Some(m) => m,
+                None => return EventResult::Continue,
+            };
+            active_move.category == "Physical"
+        };
+
+        if !is_ally && is_physical {
+            // this.effectState.gotHit = true;
+            if let Some(ref mut effect_state) = battle.effect_state {
+                effect_state.got_hit = Some(true);
+            }
+
+            // const action = this.queue.willMove(pokemon);
+            // if (action) {
+            //     this.queue.prioritizeAction(action);
+            // }
+            let action = battle.queue_will_move(pokemon);
+            if action.is_some() {
+                battle.queue_prioritize_action(pokemon);
+            }
+        }
+
         EventResult::Continue
     }
 }
