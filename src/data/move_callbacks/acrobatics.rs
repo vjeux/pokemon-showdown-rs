87703985a3602ffc62e3c77cf73f7b7565f0646e
Developer <dev@example.com>
2025-12-27 17:27:17 +0000

    Implement acrobatics basePowerCallback - doubles BP when no item held

--- src/data/move_callbacks/acrobatics.rs
+++ src/data/move_callbacks/acrobatics.rs
@@ -15,7 +15,33 @@ use crate::event::EventResult;
 ///     return move.basePower;
 /// }
 pub fn base_power_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    // Get the pokemon
+    let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // Get the active move
+    let move_id = match &battle.active_move {
+        Some(id) => id,
+        None => return EventResult::Continue,
+    };
+
+    // Get the move data
+    let move_data = match battle.dex.get_move_by_id(move_id) {
+        Some(m) => m,
+        None => return EventResult::Continue,
+    };
+
+    // if (!pokemon.item)
+    if pokemon.item.is_empty() {
+        // this.debug("BP doubled for no item");
+        battle.debug("BP doubled for no item");
+        // return move.basePower * 2;
+        return EventResult::Int(move_data.base_power * 2);
+    }
+
+    // return move.basePower;
+    EventResult::Int(move_data.base_power)
 }
 
