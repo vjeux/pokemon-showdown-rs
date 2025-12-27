533536f73ee17581ab1d6467508384acef95861d
Developer <dev@example.com>
2025-12-27 21:46:54 +0000

    Implement minimize move condition callbacks

--- src/data/move_callbacks/minimize.rs
+++ src/data/move_callbacks/minimize.rs
@@ -20,7 +20,31 @@ pub mod condition {
     ///     }
     /// }
     pub fn on_source_modify_damage(battle: &mut Battle, damage: i32, source_pos: Option<(usize, usize)>, target_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        use crate::dex_data::ID;
+
+        // const boostedMoves = [
+        //     'stomp', 'steamroller', 'bodyslam', 'flyingpress', 'dragonrush', 'heatcrash', 'heavyslam', 'maliciousmoonsault', 'supercellslam',
+        // ];
+        let boosted_moves = [
+            ID::from("stomp"),
+            ID::from("steamroller"),
+            ID::from("bodyslam"),
+            ID::from("flyingpress"),
+            ID::from("dragonrush"),
+            ID::from("heatcrash"),
+            ID::from("heavyslam"),
+            ID::from("maliciousmoonsault"),
+            ID::from("supercellslam"),
+        ];
+
+        let current_move_id = ID::from(move_id);
+
+        // if (boostedMoves.includes(move.id)) {
+        if boosted_moves.contains(&current_move_id) {
+            // return this.chainModify(2);
+            return EventResult::ChainModify(2);
+        }
+
         EventResult::Continue
     }
 
@@ -34,7 +58,32 @@ pub mod condition {
     ///     return accuracy;
     /// }
     pub fn on_accuracy(battle: &mut Battle, accuracy: i32, target_pos: Option<(usize, usize)>, source_pos: Option<(usize, usize)>, move_id: &str) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
-        EventResult::Continue
+        use crate::dex_data::ID;
+
+        // const boostedMoves = [
+        //     'stomp', 'steamroller', 'bodyslam', 'flyingpress', 'dragonrush', 'heatcrash', 'heavyslam', 'maliciousmoonsault', 'supercellslam',
+        // ];
+        let boosted_moves = [
+            ID::from("stomp"),
+            ID::from("steamroller"),
+            ID::from("bodyslam"),
+            ID::from("flyingpress"),
+            ID::from("dragonrush"),
+            ID::from("heatcrash"),
+            ID::from("heavyslam"),
+            ID::from("maliciousmoonsault"),
+            ID::from("supercellslam"),
+        ];
+
+        let current_move_id = ID::from(move_id);
+
+        // if (boostedMoves.includes(move.id)) {
+        if boosted_moves.contains(&current_move_id) {
+            // return true;
+            return EventResult::Bool(true);
+        }
+
+        // return accuracy;
+        EventResult::Int(accuracy)
     }
 }
