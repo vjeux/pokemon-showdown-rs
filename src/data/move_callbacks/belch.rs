5257fe548acc238c334531b5e309dfff5a6f46ba
Developer <dev@example.com>
2025-12-27 15:39:54 +0000

    Implement belch move callback
    Line-by-line implementation from TypeScript:
    - Disables belch move if pokemon has not eaten a berry
    - Uses ate_berry field to track berry consumption

--- src/data/move_callbacks/belch.rs
+++ src/data/move_callbacks/belch.rs
@@ -16,8 +16,13 @@ pub fn on_disable_move(battle: &mut Battle, pokemon_pos: (usize, usize)) -> Even
         None => return EventResult::Continue,
     };
 
+    // Only allow belch if pokemon has eaten a berry this battle
     if !pokemon.ate_berry {
-        // TODO: pokemon.disableMove('belch');
+        // Need to get mutable reference to disable the move
+        drop(pokemon);
+        if let Some(pokemon_mut) = battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+            pokemon_mut.disable_move("belch", None);
+        }
     }
 
     EventResult::Continue
