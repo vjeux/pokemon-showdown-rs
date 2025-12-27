892f9a2ca6065c128e04e9504a0b5ac5493c2694
Developer <dev@example.com>
2025-12-27 20:25:50 +0100

    Implement endeavor callbacks - damage based on HP difference

--- src/data/move_callbacks/endeavor.rs
+++ src/data/move_callbacks/endeavor.rs
@@ -11,15 +11,61 @@ use crate::event::EventResult;
 ///     return target.getUndynamaxedHP() - pokemon.hp;
 /// }
 pub fn damage_callback(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // return target.getUndynamaxedHP() - pokemon.hp;
+    let target_hp = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.get_undynamaxed_hp()
+    };
+
+    let pokemon_hp = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.hp
+    };
+
+    let damage = target_hp - pokemon_hp;
+
+    EventResult::Int(damage)
 }
 
 /// onTryImmunity(target, pokemon) {
 ///     return pokemon.hp < target.hp;
 /// }
 pub fn on_try_immunity(battle: &mut Battle, target_pos: Option<(usize, usize)>, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let pokemon = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // return pokemon.hp < target.hp;
+    let pokemon_hp = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_pokemon.hp
+    };
+
+    let target_hp = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.hp
+    };
+
+    EventResult::Bool(pokemon_hp < target_hp)
 }
 
