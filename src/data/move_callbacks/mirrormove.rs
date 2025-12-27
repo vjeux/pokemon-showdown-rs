b27e4d48946696b621ebcf848cad3b0c597a44e9
Developer <dev@example.com>
2025-12-27 21:50:05 +0000

    Implement mirrormove callback

--- src/data/move_callbacks/mirrormove.rs
+++ src/data/move_callbacks/mirrormove.rs
@@ -16,7 +16,47 @@ use crate::event::EventResult;
 ///     return null;
 /// }
 pub fn on_try_hit(battle: &mut Battle, source_pos: (usize, usize), target_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    let pokemon = source_pos;
+    let target = target_pos;
+
+    // const move = target.lastMove;
+    let last_move_id = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Bool(false),
+        };
+        target_pokemon.last_move.clone()
+    };
+
+    // if (!move?.flags['mirror'] || move.isZ || move.isMax) {
+    //     return false;
+    // }
+    let move_id = match last_move_id {
+        Some(id) => id,
+        None => return EventResult::Bool(false),
+    };
+
+    let move_data = match battle.dex.get_move_by_id(&move_id) {
+        Some(m) => m,
+        None => return EventResult::Bool(false),
+    };
+
+    // Check if move has mirror flag
+    if !move_data.flags.contains_key("mirror") {
+        return EventResult::Bool(false);
+    }
+
+    // Check if move is Z or Max
+    if move_data.is_z_or_max_powered {
+        return EventResult::Bool(false);
+    }
+
+    // this.actions.useMove(move.id, pokemon, { target });
+    crate::battle_actions::use_move(battle, &move_id, pokemon, Some(target), None);
+
+    // return null;
+    EventResult::Null
 }
 
