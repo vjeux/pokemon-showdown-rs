efb08e61e96b68bef3c161aaff84ec7705712bb9
Developer <dev@example.com>
2025-12-27 15:49:38 +0100

    Implement hyperspacefury.rs species check for Hoopa forms

--- src/data/move_callbacks/hyperspacefury.rs
+++ src/data/move_callbacks/hyperspacefury.rs
@@ -22,7 +22,26 @@ use crate::event::EventResult;
 ///     return null;
 /// }
 pub fn on_try(battle: &mut Battle, source_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    let source = match battle.pokemon_at(source_pos.0, source_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // Only Hoopa-Unbound can use this move
+    if source.species == "Hoopa-Unbound" {
+        return EventResult::Continue;
+    }
+
+    // TODO: battle.hint("Only a Pokemon whose form is Hoopa Unbound can use this move.");
+
+    if source.species == "Hoopa" {
+        // TODO: battle.attrLastMove('[still]');
+        // TODO: battle.add('-fail', source, 'move: Hyperspace Fury', '[forme]');
+        return EventResult::Null;
+    }
+
+    // TODO: battle.attrLastMove('[still]');
+    // TODO: battle.add('-fail', source, 'move: Hyperspace Fury');
+    EventResult::Null
 }
 
