8133ffd5aadb7d8d9e335be3e590c905c1fbd5f5
Developer <dev@example.com>
2025-12-28 17:19:13 +0000

    Port synthesis.rs on_hit from JS - Weather-based healing (2/3 in sun, 1/4 in rain/sand/snow, 1/2 default)

--- src/data/move_callbacks/synthesis.rs
+++ src/data/move_callbacks/synthesis.rs
@@ -30,10 +30,70 @@ use crate::event::EventResult;
 ///     return success;
 /// }
 pub fn on_hit(
-    _battle: &mut Battle,
-    _pokemon_pos: (usize, usize),
+    battle: &mut Battle,
+    pokemon_pos: (usize, usize),
     _target_pos: Option<(usize, usize)>,
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    let pokemon = pokemon_pos;
+
+    // Get effective weather and determine factor
+    let (numerator, denominator) = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let weather = pokemon_pokemon.effective_weather(battle.field.weather.as_str());
+
+        // let factor = 0.5;
+        // switch (pokemon.effectiveWeather()) {
+        // case 'sunnyday':
+        // case 'desolateland':
+        //     factor = 0.667;
+        //     break;
+        // case 'raindance':
+        // case 'primordialsea':
+        // case 'sandstorm':
+        // case 'hail':
+        // case 'snowscape':
+        //     factor = 0.25;
+        //     break;
+        // }
+        match weather.as_str() {
+            "sunnyday" | "desolateland" => (2, 3), // 0.667
+            "raindance" | "primordialsea" | "sandstorm" | "hail" | "snowscape" => (1, 4), // 0.25
+            _ => (1, 2), // 0.5
+        }
+    };
+
+    // const success = !!this.heal(this.modify(pokemon.maxhp, factor));
+    let heal_amount = {
+        let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        battle.modify(pokemon_pokemon.maxhp, numerator, denominator)
+    };
+
+    let success = battle.heal(heal_amount, Some(pokemon), None, None);
+
+    // if (!success) {
+    //     this.add('-fail', pokemon, 'heal');
+    //     return this.NOT_FAIL;
+    // }
+    if success.is_none() {
+        let pokemon_arg = {
+            let pokemon_pokemon = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_pokemon.get_slot()
+        };
+
+        battle.add("-fail", &[pokemon_arg.into(), "heal".into()]);
+        return EventResult::NotFail;
+    }
+
+    // return success;
     EventResult::Continue
 }
