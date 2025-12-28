683372fab741a947785b0ca902a713f6461dfd67
Developer <dev@example.com>
2025-12-29 00:22:03 +0100

    Implement zygardite onTakeItem callback
    Prevents the item from being taken if the holder is Zygarde (any forme)

--- src/data/item_callbacks/zygardite.rs
+++ src/data/item_callbacks/zygardite.rs
@@ -11,7 +11,37 @@ use crate::event::EventResult;
 ///     if (source.baseSpecies.baseSpecies === 'Zygarde') return false;
 ///     return true;
 /// }
-pub fn on_take_item(battle: &mut Battle, item_pos: Option<(usize, usize)>, pokemon_pos: (usize, usize), source_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+pub fn on_take_item(battle: &mut Battle, _item_pos: Option<(usize, usize)>, pokemon_pos: (usize, usize), source_pos: Option<(usize, usize)>) -> EventResult {
+    // if (source.baseSpecies.baseSpecies === 'Zygarde') return false;
+    // return true;
+
+    // Extract source's base species base species
+    let source = match source_pos {
+        Some(pos) => pos,
+        None => return EventResult::Boolean(true),
+    };
+
+    let source_base_species_base_species = {
+        let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+            Some(p) => p,
+            None => return EventResult::Boolean(true),
+        };
+
+        // Get the source's base species
+        let species = battle.dex.get_species(source_pokemon.base_species.as_str());
+        species
+            .and_then(|s| s.base_species.clone())
+            .unwrap_or_else(|| {
+                // If base_species is None, use the species name itself
+                species.map(|s| s.name.clone()).unwrap_or_default()
+            })
+    };
+
+    // if (source.baseSpecies.baseSpecies === 'Zygarde') return false;
+    if source_base_species_base_species == "Zygarde" {
+        return EventResult::Boolean(false);
+    }
+
+    // return true;
+    EventResult::Boolean(true)
 }
