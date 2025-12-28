d8f4d07fc1edfc57368adbae2fbc17bdcf0ba215
Developer <dev@example.com>
2025-12-28 21:27:14 +0100

    Implement on_base_power for adamantorb

--- src/data/item_callbacks/adamantorb.rs
+++ src/data/item_callbacks/adamantorb.rs
@@ -13,6 +13,28 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_base_power(battle: &mut Battle, base_power: i32, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (user.baseSpecies.num === 483 && (move.type === 'Steel' || move.type === 'Dragon'))
+    let (species_num, move_type) = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let species_num = battle.dex.get_species(pokemon.base_species.as_str())
+            .map(|s| s.num)
+            .unwrap_or(0);
+
+        let move_type = battle.active_move.as_ref()
+            .map(|m| m.move_type.as_str())
+            .unwrap_or("");
+
+        (species_num, move_type.to_string())
+    };
+
+    if species_num == 483 && (move_type == "Steel" || move_type == "Dragon") {
+        // return this.chainModify([4915, 4096]);
+        battle.chain_modify_fraction(4915, 4096);
+    }
+
     EventResult::Continue
 }
