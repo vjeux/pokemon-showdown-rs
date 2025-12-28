5dd56e8cada79a574a73b00e7547b927bf5bf6bf
Developer <dev@example.com>
2025-12-28 17:11:02 +0000

    Port freezeshock.rs on_try_move from JS - Two-turn charging move with removeVolatile, prepare message, ChargeMove event, and addVolatile twoturnmove

--- src/data/move_callbacks/freezeshock.rs
+++ src/data/move_callbacks/freezeshock.rs
@@ -19,10 +19,84 @@ use crate::event::EventResult;
 ///     return null;
 /// }
 pub fn on_try_move(
-    _battle: &mut Battle,
-    _source_pos: (usize, usize),
-    _target_pos: Option<(usize, usize)>,
+    battle: &mut Battle,
+    source_pos: (usize, usize),
+    target_pos: Option<(usize, usize)>,
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    // onTryMove(attacker, defender, move) {
+    //     if (attacker.removeVolatile(move.id)) {
+    //         return;
+    //     }
+    //     this.add('-prepare', attacker, move.name);
+    //     if (!this.runEvent('ChargeMove', attacker, defender, move)) {
+    //         return;
+    //     }
+    //     attacker.addVolatile('twoturnmove', defender);
+    //     return null;
+    // }
+    let attacker = source_pos;
+    let defender = target_pos;
+
+    // if (attacker.removeVolatile(move.id)) {
+    //     return;
+    // }
+    let move_id = {
+        let active_move = match &battle.active_move {
+            Some(active_move) => active_move,
+            None => return EventResult::Continue,
+        };
+        active_move.id.clone()
+    };
+
+    let removed = {
+        let pokemon = match battle.pokemon_at_mut(attacker.0, attacker.1) {
+            Some(p) => p,
+
+            None => return EventResult::Continue,
+        };
+
+        pokemon.remove_volatile(&move_id)
+    };
+    if removed {
+        return EventResult::Continue;
+    }
+
+    // this.add('-prepare', attacker, move.name);
+    let (attacker_arg, move_name) = {
+        let attacker_pokemon = match battle.pokemon_at(attacker.0, attacker.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        let active_move = match &battle.active_move {
+            Some(active_move) => active_move,
+            None => return EventResult::Continue,
+        };
+        (attacker_pokemon.get_slot(), active_move.name.clone())
+    };
+
+    battle.add("-prepare", &[attacker_arg.into(), move_name.into()]);
+
+    // if (!this.runEvent('ChargeMove', attacker, defender, move)) {
+    //     return;
+    // }
+    let charge_result = battle.run_event("ChargeMove", Some(attacker), defender, None, None);
+    if charge_result == Some(0) {
+        return EventResult::Continue;
+    }
+
+    // attacker.addVolatile('twoturnmove', defender);
+    {
+        let pokemon = match battle.pokemon_at_mut(attacker.0, attacker.1) {
+            Some(p) => p,
+
+            None => return EventResult::Continue,
+        };
+
+        pokemon.add_volatile(ID::from("twoturnmove"));
+    }
+
+    // return null;
+    EventResult::Stop
 }
