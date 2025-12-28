d0f05f3463f48c24d894d6d026ea092a1ae6f1f9
Developer <dev@example.com>
2025-12-28 23:55:47 +0100

    Implement metalpowder and quickpowder for Ditto
    - metalpowder: onModifyDef - doubles Defense for untransformed Ditto
    - quickpowder: onModifySpe - doubles Speed for untransformed Ditto

--- src/data/item_callbacks/metalpowder.rs
+++ src/data/item_callbacks/metalpowder.rs
@@ -13,6 +13,24 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_modify_def(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (pokemon.species.name === 'Ditto' && !pokemon.transformed)
+    let (species_name, transformed) = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let species_name = battle.dex.get_species(pokemon.species_id.as_str())
+            .map(|s| s.name.clone())
+            .unwrap_or_default();
+
+        (species_name, pokemon.transformed)
+    };
+
+    if species_name == "Ditto" && !transformed {
+        // return this.chainModify(2);
+        battle.chain_modify(2.0);
+    }
+
     EventResult::Continue
 }
--- src/data/item_callbacks/quickpowder.rs
+++ src/data/item_callbacks/quickpowder.rs
@@ -13,10 +13,24 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_modify_spe(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // if (pokemon.species.name === 'Ditto' && !pokemon.transformed) {
-    //     return this.chainModify(2);
-    // }
-    // TODO: Need pokemon.species.name and pokemon.transformed to check if Ditto
-    // Also need ability to modify speed value by chainModify(2)
+    // if (pokemon.species.name === 'Ditto' && !pokemon.transformed)
+    let (species_name, transformed) = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let species_name = battle.dex.get_species(pokemon.species_id.as_str())
+            .map(|s| s.name.clone())
+            .unwrap_or_default();
+
+        (species_name, pokemon.transformed)
+    };
+
+    if species_name == "Ditto" && !transformed {
+        // return this.chainModify(2);
+        battle.chain_modify(2.0);
+    }
+
     EventResult::Continue
 }
