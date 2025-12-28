44c8133b6bfe8722b1f35124080f14a8db013af4
Developer <dev@example.com>
2025-12-28 16:59:18 +0000

    Port storedpower.rs base_power_callback from JS
    BP = basePower + 20 * positiveBoosts()
    Uses pokemon.positive_boosts() to count all positive stat boosts.

--- src/data/move_callbacks/storedpower.rs
+++ src/data/move_callbacks/storedpower.rs
@@ -13,10 +13,26 @@ use crate::event::EventResult;
 ///     return bp;
 /// }
 pub fn base_power_callback(
-    _battle: &mut Battle,
-    _pokemon_pos: (usize, usize),
+    battle: &mut Battle,
+    pokemon_pos: (usize, usize),
     _target_pos: Option<(usize, usize)>,
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    // Get the pokemon
+    let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // Get the active move
+    let active_move = match &battle.active_move {
+        Some(m) => m,
+        None => return EventResult::Continue,
+    };
+
+    // const bp = move.basePower + 20 * pokemon.positiveBoosts();
+    let bp = active_move.base_power + 20 * pokemon.positive_boosts();
+
+    // Note: JS has this.debug call which we don't have infrastructure for yet
+    // this.debug(`BP: ${bp}`);
+    EventResult::Number(bp)
 }
