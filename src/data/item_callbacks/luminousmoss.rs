bb172ad796929b24d7feb66008fb02f04e6e9043
Developer <dev@example.com>
2025-12-29 00:18:45 +0100

    Implement luminousmoss onDamagingHit callback
    Consumes the item when hit by a Water-type move to boost Special Defense

--- src/data/item_callbacks/luminousmoss.rs
+++ src/data/item_callbacks/luminousmoss.rs
@@ -13,6 +13,22 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_damaging_hit(battle: &mut Battle, damage: i32, target_pos: (usize, usize), source_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (move.type === 'Water') {
+    //     target.useItem();
+    // }
+
+    let is_water_move = match &battle.active_move {
+        Some(active_move) => active_move.move_type == "Water",
+        None => return EventResult::Continue,
+    };
+
+    if is_water_move {
+        let pokemon_mut = match battle.pokemon_at_mut(target_pos.0, target_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_mut.use_item();
+    }
+
     EventResult::Continue
 }
