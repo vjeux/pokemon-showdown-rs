07f86b4d69e2d762741125564ffcaee661e36177
Developer <dev@example.com>
2025-12-28 18:11:07 +0000

    Port steelroller.rs callbacks (on_try, on_hit, on_after_sub_damage)

--- src/data/move_callbacks/steelroller.rs
+++ src/data/move_callbacks/steelroller.rs
@@ -11,11 +11,18 @@ use crate::event::EventResult;
 ///     return !this.field.isTerrain('');
 /// }
 pub fn on_try(
-    _battle: &mut Battle,
+    battle: &mut Battle,
     _source_pos: (usize, usize),
     _target_pos: Option<(usize, usize)>,
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // return !this.field.isTerrain('');
+    // Returns true if there is a terrain active (not empty)
+    let has_terrain = !battle.field.is_terrain("");
+
+    if !has_terrain {
+        return EventResult::NotFail;
+    }
+
     EventResult::Continue
 }
 
@@ -23,18 +30,22 @@ pub fn on_try(
 ///     this.field.clearTerrain();
 /// }
 pub fn on_hit(
-    _battle: &mut Battle,
+    battle: &mut Battle,
     _pokemon_pos: (usize, usize),
     _target_pos: Option<(usize, usize)>,
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // this.field.clearTerrain();
+    battle.field.clear_terrain();
+
     EventResult::Continue
 }
 
 /// onAfterSubDamage() {
 ///     this.field.clearTerrain();
 /// }
-pub fn on_after_sub_damage(_battle: &mut Battle) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_after_sub_damage(battle: &mut Battle) -> EventResult {
+    // this.field.clearTerrain();
+    battle.field.clear_terrain();
+
     EventResult::Continue
 }
