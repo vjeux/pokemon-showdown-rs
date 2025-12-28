b040a38760ba4318c522413022e8dff63d0a7cf1
Developer <dev@example.com>
2025-12-29 00:05:52 +0100

    Implement laxincense onModifyAccuracy callback
    Decreases opponent accuracy using chain_modify_fraction(3686, 4096)

--- src/data/item_callbacks/laxincense.rs
+++ src/data/item_callbacks/laxincense.rs
@@ -13,6 +13,14 @@ use crate::event::EventResult;
 ///     return this.chainModify([3686, 4096]);
 /// }
 pub fn on_modify_accuracy(battle: &mut Battle) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (typeof accuracy !== 'number') return;
+    // this.debug('lax incense - decreasing accuracy');
+    // return this.chainModify([3686, 4096]);
+
+    // Note: In JS, this checks if accuracy is a number (not true/bypassed).
+    // Since this callback is only called when accuracy can be modified,
+    // we can apply the modifier.
+    battle.debug("lax incense - decreasing accuracy");
+    battle.chain_modify_fraction(3686, 4096);
     EventResult::Continue
 }
