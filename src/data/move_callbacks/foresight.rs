ca3a974604ac1f95a08d380da658dce9ac56c3d7
Developer <dev@example.com>
2025-12-28 17:45:10 +0000

    Port foresight.rs on_try_hit and condition::on_start from JS - Checks for miracleeye and adds battle message

--- src/data/move_callbacks/foresight.rs
+++ src/data/move_callbacks/foresight.rs
@@ -11,11 +11,27 @@ use crate::event::EventResult;
 ///     if (target.volatiles['miracleeye']) return false;
 /// }
 pub fn on_try_hit(
-    _battle: &mut Battle,
+    battle: &mut Battle,
     _source_pos: (usize, usize),
-    _target_pos: (usize, usize),
+    target_pos: (usize, usize),
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let target = target_pos;
+
+    // if (target.volatiles['miracleeye']) return false;
+    let has_miracle_eye = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.has_volatile(&ID::from("miracleeye"))
+    };
+
+    if has_miracle_eye {
+        return EventResult::NotFail;
+    }
+
     EventResult::Continue
 }
 
@@ -25,8 +41,23 @@ pub mod condition {
     /// onStart(pokemon) {
     ///     this.add('-start', pokemon, 'Foresight');
     /// }
-    pub fn on_start(_battle: &mut Battle, _pokemon_pos: (usize, usize)) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+    pub fn on_start(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
+        let pokemon = pokemon_pos;
+
+        // this.add('-start', pokemon, 'Foresight');
+        let pokemon_arg = {
+            let pokemon_ref = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_ref.get_slot()
+        };
+
+        battle.add(
+            "-start",
+            &[pokemon_arg.into(), "Foresight".into()],
+        );
+
         EventResult::Continue
     }
 
