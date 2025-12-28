cdcbe0cb440d54ac12d3e38d1e73685f7e6575f2
Developer <dev@example.com>
2025-12-28 23:37:35 +0100

    Implement blacksludge item onResidual callback

--- src/data/item_callbacks/blacksludge.rs
+++ src/data/item_callbacks/blacksludge.rs
@@ -15,6 +15,22 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_residual(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (pokemon.hasType('Poison'))
+    let (has_poison, base_maxhp) = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        (pokemon.has_type("Poison"), pokemon.base_maxhp)
+    };
+
+    if has_poison {
+        // this.heal(pokemon.baseMaxhp / 16);
+        battle.heal(base_maxhp / 16, Some(pokemon_pos), None, None);
+    } else {
+        // this.damage(pokemon.baseMaxhp / 8);
+        battle.damage(base_maxhp / 8, Some(pokemon_pos), None, None, false);
+    }
+
     EventResult::Continue
 }
