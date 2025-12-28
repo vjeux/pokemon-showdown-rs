4014cab7ce9f88cc1188e7c5156adfe769eb6481
Developer <dev@example.com>
2025-12-29 00:02:07 +0100

    Implement all three Choice items
    - choiceband: onStart, onModifyMove, onModifyAtk (1.5x Attack boost)
    - choicescarf: onStart, onModifyMove, onModifySpe (1.5x Speed boost)
    - choicespecs: onStart, onModifyMove, onModifySpA (1.5x Sp.Atk boost)
    
    All three items lock the user into one move via choicelock volatile.
    Boosts don't apply during Dynamax.

--- src/data/item_callbacks/choicespecs.rs
+++ src/data/item_callbacks/choicespecs.rs
@@ -14,7 +14,34 @@ use crate::event::EventResult;
 ///     pokemon.removeVolatile('choicelock');
 /// }
 pub fn on_start(battle: &mut Battle, target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (pokemon.volatiles['choicelock']) {
+    //     this.debug('removing choicelock');
+    // }
+    // pokemon.removeVolatile('choicelock');
+
+    let pokemon_pos = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    let has_choicelock = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon.has_volatile(&crate::dex_data::ID::new("choicelock"))
+    };
+
+    if has_choicelock {
+        battle.debug("removing choicelock");
+    }
+
+    let pokemon_mut = match battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+    pokemon_mut.remove_volatile(&crate::dex_data::ID::new("choicelock"));
+
     EventResult::Continue
 }
 
@@ -22,7 +49,13 @@ pub fn on_start(battle: &mut Battle, target_pos: Option<(usize, usize)>) -> Even
 ///     pokemon.addVolatile('choicelock');
 /// }
 pub fn on_modify_move(battle: &mut Battle, pokemon_pos: (usize, usize), target_pos: Option<(usize, usize)>) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // pokemon.addVolatile('choicelock');
+    let pokemon_mut = match battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+    pokemon_mut.add_volatile(crate::dex_data::ID::new("choicelock"));
+
     EventResult::Continue
 }
 
@@ -31,6 +64,20 @@ pub fn on_modify_move(battle: &mut Battle, pokemon_pos: (usize, usize), target_p
 ///     return this.chainModify(1.5);
 /// }
 pub fn on_modify_sp_a(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (pokemon.volatiles['dynamax']) return;
+    let has_dynamax = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon.has_volatile(&crate::dex_data::ID::new("dynamax"))
+    };
+
+    if has_dynamax {
+        return EventResult::Continue;
+    }
+
+    // return this.chainModify(1.5);
+    battle.chain_modify(1.5);
     EventResult::Continue
 }
