4d5d636986421cc76b200d798b6e7eb4a0642fe2
Developer <dev@example.com>
2025-12-28 18:02:27 +0000

    Port fly.rs callbacks (on_try_move, condition::on_invulnerability, condition::on_source_modify_damage)

--- src/data/move_callbacks/fly.rs
+++ src/data/move_callbacks/fly.rs
@@ -19,12 +19,76 @@ use crate::event::EventResult;
 ///     return null;
 /// }
 pub fn on_try_move(
-    _battle: &mut Battle,
-    _source_pos: (usize, usize),
-    _target_pos: Option<(usize, usize)>,
+    battle: &mut Battle,
+    source_pos: (usize, usize),
+    target_pos: Option<(usize, usize)>,
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    // Get the active move
+    let move_id = match &battle.active_move {
+        Some(active_move) => active_move.id.clone(),
+        None => return EventResult::Continue,
+    };
+
+    // if (attacker.removeVolatile(move.id)) {
+    //     return;
+    // }
+    {
+        let attacker = match battle.pokemon_at_mut(source_pos.0, source_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        if attacker.volatiles.contains_key(&move_id) {
+            attacker.remove_volatile(&move_id);
+            // return;
+            return EventResult::Continue;
+        }
+    }
+
+    // this.add('-prepare', attacker, move.name);
+    let (attacker_ident, move_name) = {
+        let attacker = match battle.pokemon_at(source_pos.0, source_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let move_data = battle.dex.get_move_by_id(&move_id);
+        let move_name = move_data
+            .map(|m| m.name.clone())
+            .unwrap_or_else(|| move_id.to_string());
+
+        (attacker.get_slot(), move_name)
+    };
+
+    battle.add(
+        "-prepare",
+        &[attacker_ident.as_str().into(), move_name.into()],
+    );
+
+    // if (!this.runEvent('ChargeMove', attacker, defender, move)) {
+    //     return;
+    // }
+    if battle
+        .run_event("ChargeMove", Some(source_pos), target_pos, None, None)
+        .unwrap_or(0)
+        == 0
+    {
+        // return;
+        return EventResult::Continue;
+    }
+
+    // attacker.addVolatile('twoturnmove', defender);
+    let attacker = match battle.pokemon_at_mut(source_pos.0, source_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    attacker.add_volatile(ID::from("twoturnmove"));
+
+    // return null;
+    EventResult::Stop
 }
 
 pub mod condition {
@@ -40,10 +104,26 @@ pub mod condition {
         _battle: &mut Battle,
         _target_pos: Option<(usize, usize)>,
         _source_pos: Option<(usize, usize)>,
-        _move_id: &str,
+        move_id: &str,
     ) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
-        EventResult::Continue
+        // if (['gust', 'twister', 'skyuppercut', 'thunder', 'hurricane', 'smackdown', 'thousandarrows'].includes(move.id)) {
+        let valid_moves = [
+            "gust",
+            "twister",
+            "skyuppercut",
+            "thunder",
+            "hurricane",
+            "smackdown",
+            "thousandarrows",
+        ];
+
+        if valid_moves.contains(&move_id) {
+            // return;
+            return EventResult::Continue;
+        }
+
+        // return false;
+        EventResult::Boolean(false)
     }
 
     /// onSourceModifyDamage(damage, source, target, move) {
@@ -52,13 +132,18 @@ pub mod condition {
     ///     }
     /// }
     pub fn on_source_modify_damage(
-        _battle: &mut Battle,
+        battle: &mut Battle,
         _damage: i32,
         _source_pos: Option<(usize, usize)>,
         _target_pos: Option<(usize, usize)>,
-        _move_id: &str,
+        move_id: &str,
     ) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // if (move.id === 'gust' || move.id === 'twister') {
+        if move_id == "gust" || move_id == "twister" {
+            // return this.chainModify(2);
+            return EventResult::Number(battle.chain_modify(2.0));
+        }
+
         EventResult::Continue
     }
 }
