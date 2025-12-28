5f91dcb85cc0c8bc81b6b88e35dfb3d229285a7a
Developer <dev@example.com>
2025-12-28 17:24:24 +0000

    Port venomdrench.rs on_hit from JS - Lowers Atk, SpA, and Spe if target is poisoned

--- src/data/move_callbacks/venomdrench.rs
+++ src/data/move_callbacks/venomdrench.rs
@@ -14,10 +14,38 @@ use crate::event::EventResult;
 ///     return false;
 /// }
 pub fn on_hit(
-    _battle: &mut Battle,
-    _pokemon_pos: (usize, usize),
-    _target_pos: Option<(usize, usize)>,
+    battle: &mut Battle,
+    pokemon_pos: (usize, usize),
+    target_pos: Option<(usize, usize)>,
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    use crate::dex_data::ID;
+
+    let source = pokemon_pos;
+    let target = match target_pos {
+        Some(pos) => pos,
+        None => return EventResult::Continue,
+    };
+
+    // if (target.status === 'psn' || target.status === 'tox')
+    let status = {
+        let target_pokemon = match battle.pokemon_at(target.0, target.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.status.clone()
+    };
+
+    if status == ID::from("psn") || status == ID::from("tox") {
+        // return !!this.boost({ atk: -1, spa: -1, spe: -1 }, target, source, move);
+        let success = battle.boost(&[("atk", -1), ("spa", -1), ("spe", -1)], target, Some(source), None);
+
+        if success {
+            return EventResult::Continue;
+        } else {
+            return EventResult::NotFail;
+        }
+    }
+
+    // return false;
+    EventResult::NotFail
 }
