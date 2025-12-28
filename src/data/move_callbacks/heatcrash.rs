9fc68353243b0a367a0612275125a45103b9ec28
Developer <dev@example.com>
2025-12-28 16:56:18 +0000

    Port heatcrash.rs base_power_callback from JS
    Weight-based BP calculation:
    - 5x+ weight: 120 BP
    - 4x+ weight: 100 BP
    - 3x+ weight: 80 BP
    - 2x+ weight: 60 BP
    - Less: 40 BP

--- src/data/move_callbacks/heatcrash.rs
+++ src/data/move_callbacks/heatcrash.rs
@@ -26,12 +26,45 @@ use crate::event::EventResult;
 ///     return bp;
 /// }
 pub fn base_power_callback(
-    _battle: &mut Battle,
-    _pokemon_pos: (usize, usize),
-    _target_pos: Option<(usize, usize)>,
+    battle: &mut Battle,
+    pokemon_pos: (usize, usize),
+    target_pos: Option<(usize, usize)>,
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
-    EventResult::Continue
+    // Get the pokemon (user of the move)
+    let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+        Some(p) => p,
+        None => return EventResult::Continue,
+    };
+
+    // Get the target
+    let target = match target_pos {
+        Some(pos) => match battle.pokemon_at(pos.0, pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        },
+        None => return EventResult::Continue,
+    };
+
+    // Get weights
+    let pokemon_weight = pokemon.get_weight();
+    let target_weight = target.get_weight();
+
+    // Calculate base power based on weight ratio
+    let bp = if pokemon_weight >= target_weight * 5 {
+        120
+    } else if pokemon_weight >= target_weight * 4 {
+        100
+    } else if pokemon_weight >= target_weight * 3 {
+        80
+    } else if pokemon_weight >= target_weight * 2 {
+        60
+    } else {
+        40
+    };
+
+    // Note: JS has this.debug call which we don't have infrastructure for yet
+    // this.debug(`BP: ${bp}`);
+    EventResult::Number(bp)
 }
 
 /// onTryHit(target, pokemon, move) {
