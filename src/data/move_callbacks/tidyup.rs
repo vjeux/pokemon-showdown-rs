b678dedc88d914813b636bd7926f331993150afd
Developer <dev@example.com>
2025-12-28 18:26:49 +0000

    Implement tidyup move callback (1 callback)
    - on_hit: Removes substitute from all active pokemon, removes hazards from both sides, boosts attack and speed
    - Marked tidyup as complete in MOVES_TODO.md

--- src/data/move_callbacks/tidyup.rs
+++ src/data/move_callbacks/tidyup.rs
@@ -26,10 +26,109 @@ use crate::event::EventResult;
 ///     return !!this.boost({ atk: 1, spe: 1 }, pokemon, pokemon, null, false, true) || success;
 /// }
 pub fn on_hit(
-    _battle: &mut Battle,
-    _pokemon_pos: (usize, usize),
+    battle: &mut Battle,
+    pokemon_pos: (usize, usize),
     _target_pos: Option<(usize, usize)>,
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::ID;
+
+    let pokemon = pokemon_pos;
+
+    // let success = false;
+    let mut success = false;
+
+    // for (const active of this.getAllActive()) {
+    //     if (active.removeVolatile('substitute')) success = true;
+    // }
+    let all_active = battle.get_all_active(false);
+    for active_pos in all_active {
+        let active_mut = match battle.pokemon_at_mut(active_pos.0, active_pos.1) {
+            Some(p) => p,
+            None => continue,
+        };
+
+        if active_mut.has_volatile(&ID::from("substitute")) {
+            active_mut.remove_volatile(&ID::from("substitute"));
+            success = true;
+        }
+    }
+
+    // const removeAll = ['spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge'];
+    let remove_all = [
+        "spikes",
+        "toxicspikes",
+        "stealthrock",
+        "stickyweb",
+        "gmaxsteelsurge",
+    ];
+
+    // const sides = [pokemon.side, ...pokemon.side.foeSidesWithConditions()];
+    let pokemon_side = {
+        let pokemon_ref = match battle.pokemon_at(pokemon.0, pokemon.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_ref.side_index
+    };
+
+    let mut sides_to_check = vec![pokemon_side];
+    let foe_sides = {
+        let pokemon_side_ref = &battle.sides[pokemon_side];
+        pokemon_side_ref.foe_sides_with_conditions(&battle.sides)
+    };
+    for foe_side in foe_sides {
+        sides_to_check.push(foe_side.n);
+    }
+
+    // for (const side of sides) {
+    for side_index in sides_to_check {
+        // for (const sideCondition of removeAll) {
+        for condition_id in remove_all.iter() {
+            // if (side.removeSideCondition(sideCondition)) {
+            let removed = battle.sides[side_index].remove_side_condition(&ID::from(*condition_id));
+
+            if removed {
+                // this.add('-sideend', side, this.dex.conditions.get(sideCondition).name);
+                let side_id = if side_index == 0 { "p1" } else { "p2" };
+                let condition_name = condition_id.to_string();
+
+                battle.add(
+                    "-sideend",
+                    &[
+                        crate::battle::Arg::Str(side_id),
+                        crate::battle::Arg::from(condition_name),
+                    ],
+                );
+                success = true;
+            }
+        }
+    }
+
+    // if (success) this.add('-activate', pokemon, 'move: Tidy Up');
+    if success {
+        let pokemon_slot = {
+            let pokemon_ref = match battle.pokemon_at(pokemon.0, pokemon.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            pokemon_ref.get_slot()
+        };
+
+        battle.add(
+            "-activate",
+            &[
+                crate::battle::Arg::from(pokemon_slot),
+                crate::battle::Arg::from("move: Tidy Up"),
+            ],
+        );
+    }
+
+    // return !!this.boost({ atk: 1, spe: 1 }, pokemon, pokemon, null, false, true) || success;
+    let boost_success = battle.boost(&[("atk", 1), ("spe", 1)], pokemon, Some(pokemon), None);
+
+    if boost_success || success {
+        return EventResult::Continue;
+    }
+
     EventResult::Continue
 }
