d3a6ed533e10d69daa2f8d355fa1769ebbb8102c
Developer <dev@example.com>
2025-12-28 23:57:28 +0100

    Fix type capitalization in plates
    - meadowplate: grass -> Grass
    - mindplate: psychic -> Psychic

--- src/data/item_callbacks/mindplate.rs
+++ src/data/item_callbacks/mindplate.rs
@@ -21,7 +21,7 @@ pub fn on_base_power(battle: &mut Battle, base_power: i32, pokemon_pos: (usize, 
         None => return EventResult::Continue,
     };
 
-    if move_type == "psychic" {
+    if move_type == "Psychic" {
         battle.chain_modify_fraction(4915, 4096);
     }
 
