a818b2439999f8cd97817ca9f26fd1cfead55aa8
Developer <dev@example.com>
2025-12-29 02:07:12 +0100

    Implement leppaberry PP restoration - onUpdate and onEat callbacks

--- src/data/item_callbacks/leppaberry.rs
+++ src/data/item_callbacks/leppaberry.rs
@@ -14,7 +14,30 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_update(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (!pokemon.hp) return;
+    let has_empty_pp_move = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        if pokemon.hp == 0 {
+            return EventResult::Continue;
+        }
+
+        // if (pokemon.moveSlots.some(move => move.pp === 0))
+        pokemon.move_slots.iter().any(|move_slot| move_slot.pp == 0)
+    };
+
+    if has_empty_pp_move {
+        // pokemon.eatItem();
+        let pokemon_mut = match battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon_mut.eat_item(false);
+    }
+
     EventResult::Continue
 }
 
@@ -27,6 +50,54 @@ pub fn on_update(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResul
 ///     this.add('-activate', pokemon, 'item: Leppa Berry', moveSlot.move, '[consumed]');
 /// }
 pub fn on_eat(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // const moveSlot = pokemon.moveSlots.find(move => move.pp === 0) ||
+    //     pokemon.moveSlots.find(move => move.pp < move.maxpp);
+    let (move_slot_index, move_name, pokemon_ident) = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        // Find first move with pp === 0
+        let mut move_slot_idx = pokemon.move_slots.iter()
+            .position(|move_slot| move_slot.pp == 0);
+
+        // If no move with pp === 0, find first move with pp < maxpp
+        if move_slot_idx.is_none() {
+            move_slot_idx = pokemon.move_slots.iter()
+                .position(|move_slot| move_slot.pp < move_slot.maxpp);
+        }
+
+        // if (!moveSlot) return;
+        match move_slot_idx {
+            Some(idx) => {
+                let move_name = pokemon.move_slots[idx].move_name.clone();
+                let ident = pokemon.get_slot();
+                (idx, move_name, ident)
+            },
+            None => return EventResult::Continue,
+        }
+    };
+
+    // moveSlot.pp += 10;
+    // if (moveSlot.pp > moveSlot.maxpp) moveSlot.pp = moveSlot.maxpp;
+    {
+        let pokemon_mut = match battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+
+        let move_slot = &mut pokemon_mut.move_slots[move_slot_index];
+        move_slot.pp = move_slot.pp.saturating_add(10).min(move_slot.maxpp);
+    }
+
+    // this.add('-activate', pokemon, 'item: Leppa Berry', moveSlot.move, '[consumed]');
+    battle.add("-activate", &[
+        pokemon_ident.as_str().into(),
+        "item: Leppa Berry".into(),
+        move_name.into(),
+        "[consumed]".into()
+    ]);
+
     EventResult::Continue
 }
