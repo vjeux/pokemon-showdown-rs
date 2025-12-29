6ae5bda746bb30d802155cbd8ea1bf81c88e81a4
Developer <dev@example.com>
2025-12-29 05:24:45 +0100

    Implement maxstrike onHit - lowers speed of foes if dynamaxed

--- src/data/move_callbacks/mod.rs
+++ src/data/move_callbacks/mod.rs
@@ -236,6 +236,7 @@ pub mod maxquake;
 pub mod maxrockfall;
 pub mod maxsteelspike;
 pub mod maxstarfall;
+pub mod maxstrike;
 pub mod meanlook;
 pub mod mefirst;
 pub mod metalburst;
@@ -830,6 +831,7 @@ pub fn dispatch_on_hit(
         "maxrockfall" => maxrockfall::on_hit(battle, pokemon_pos, target_pos),
         "maxsteelspike" => maxsteelspike::on_hit(battle, pokemon_pos, target_pos),
         "maxstarfall" => maxstarfall::on_hit(battle, pokemon_pos, target_pos),
+        "maxstrike" => maxstrike::on_hit(battle, pokemon_pos, target_pos),
         "meanlook" => meanlook::on_hit(battle, pokemon_pos, target_pos),
         "metronome" => metronome::on_hit(battle, pokemon_pos, target_pos),
         "mimic" => mimic::on_hit(battle, pokemon_pos, target_pos),
