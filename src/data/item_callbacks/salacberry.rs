609ecfc68e9666d3a52c3f18f9ebe739ab4ff96e
Developer <dev@example.com>
2025-12-29 01:14:42 +0100

    Implement liechiberry, salacberry, and lansatberry callbacks

--- src/data/item_callbacks/salacberry.rs
+++ src/data/item_callbacks/salacberry.rs
@@ -14,7 +14,38 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_update(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (pokemon.hp <= pokemon.maxhp / 4 || (pokemon.hp <= pokemon.maxhp / 2 &&
+    //     pokemon.hasAbility('gluttony') && pokemon.abilityState.gluttony)) {
+    //     pokemon.eatItem();
+    // }
+
+    let should_eat = {
+        if let Some(pokemon) = battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+            let quarter_hp = pokemon.maxhp / 4;
+            let half_hp = pokemon.maxhp / 2;
+
+            if pokemon.hp <= quarter_hp {
+                true
+            } else if pokemon.hp <= half_hp {
+                let has_gluttony = pokemon.has_ability(&["gluttony"]);
+                let gluttony_active = pokemon.ability_state.data.get("gluttony")
+                    .and_then(|v| v.as_bool())
+                    .unwrap_or(false);
+                has_gluttony && gluttony_active
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    };
+
+    if should_eat {
+        if let Some(pokemon) = battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+            pokemon.eat_item(false);
+        }
+    }
+
     EventResult::Continue
 }
 
@@ -22,6 +53,7 @@ pub fn on_update(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResul
 ///     this.boost({ spe: 1 });
 /// }
 pub fn on_eat(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // this.boost({ spe: 1 });
+    battle.boost(&[("spe", 1)], pokemon_pos, None, None);
     EventResult::Continue
 }
