03439242c034f935432938ddcf1b8c076874c583
Developer <dev@example.com>
2025-12-29 13:22:01 +0100

    Implement rivalry ability callback (gender-based damage modifier)

--- src/data/ability_callbacks/rivalry.rs
+++ src/data/ability_callbacks/rivalry.rs
@@ -19,7 +19,30 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_base_power(battle: &mut Battle, base_power: i32, attacker_pos: (usize, usize), defender_pos: (usize, usize), move_id: &str) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    use crate::dex_data::Gender;
+
+    let (attacker_gender, defender_gender) = {
+        let attacker = match battle.pokemon_at(attacker_pos.0, attacker_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        let defender = match battle.pokemon_at(defender_pos.0, defender_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        (attacker.gender, defender.gender)
+    };
+
+    if attacker_gender != Gender::None && defender_gender != Gender::None {
+        if attacker_gender == defender_gender {
+            let modified = battle.chain_modify(1.25);
+            return EventResult::Number(modified);
+        } else {
+            let modified = battle.chain_modify(0.75);
+            return EventResult::Number(modified);
+        }
+    }
+
     EventResult::Continue
 }
 
