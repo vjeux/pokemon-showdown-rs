50425c1dd4542c99ea9a9009f36c6b404e7f8f97
Developer <dev@example.com>
2025-12-29 13:10:39 +0100

    Implement marvelscale ability callback
    - on_modify_def: Boost Defense by 1.5x if pokemon has a status condition
    
    1-to-1 port from JavaScript.

--- src/data/ability_callbacks/marvelscale.rs
+++ src/data/ability_callbacks/marvelscale.rs
@@ -13,7 +13,12 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_modify_def(battle: &mut Battle, def: i32, defender_pos: (usize, usize), attacker_pos: (usize, usize), move_id: &str) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    if let Some(pokemon) = battle.pokemon_at(defender_pos.0, defender_pos.1) {
+        if !pokemon.status.is_empty() {
+            let modified = battle.chain_modify(1.5);
+            return EventResult::Number(modified);
+        }
+    }
     EventResult::Continue
 }
 
