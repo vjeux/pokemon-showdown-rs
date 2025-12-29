b82dd3893b6c417280343f01f3261daafb32f689
Developer <dev@example.com>
2025-12-29 04:54:58 +0100

    Implement gmaxmalodor onHit - poisons all foes

--- src/data/move_callbacks/mod.rs
+++ src/data/move_callbacks/mod.rs
@@ -130,6 +130,7 @@ pub mod gmaxcuddle;
 pub mod gmaxfinale;
 pub mod gmaxfoamburst;
 pub mod gmaxgoldrush;
+pub mod gmaxmalodor;
 pub mod gmaxsnooze;
 pub mod gmaxsteelsurge;
 pub mod gmaxsweetness;
@@ -759,6 +760,7 @@ pub fn dispatch_on_hit(
         "gmaxfinale" => gmaxfinale::on_hit(battle, pokemon_pos, target_pos),
         "gmaxfoamburst" => gmaxfoamburst::on_hit(battle, pokemon_pos, target_pos),
         "gmaxgoldrush" => gmaxgoldrush::on_hit(battle, pokemon_pos, target_pos),
+        "gmaxmalodor" => gmaxmalodor::on_hit(battle, pokemon_pos, target_pos),
         "gmaxsnooze" => gmaxsnooze::on_hit(battle, pokemon_pos, target_pos),
         "gmaxsweetness" => gmaxsweetness::on_hit(battle, pokemon_pos, target_pos),
         "gmaxwindrage" => gmaxwindrage::on_hit(battle, pokemon_pos, target_pos),
