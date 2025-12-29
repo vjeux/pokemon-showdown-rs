ecf3006d7aa028b0eb21473d5e6f2c39f29f4051
Developer <dev@example.com>
2025-12-29 05:33:30 +0100

    Implement thief onAfterHit - steals target's item

--- src/data/move_callbacks/thief.rs
+++ src/data/move_callbacks/thief.rs
@@ -5,8 +5,13 @@
 //! Generated from data/moves.ts
 
 use crate::battle::Battle;
+use crate::dex_data::ID;
 use crate::event::EventResult;
 
+/// onAfterHit(target, source, move)
+///
+/// ```text
+/// JS Source (data/moves.ts):
 /// onAfterHit(target, source, move) {
 ///     if (source.item || source.volatiles['gem']) {
 ///         return;
@@ -23,11 +28,117 @@ use crate::event::EventResult;
 ///     this.add('-enditem', target, yourItem, '[silent]', '[from] move: Thief', `[of] ${source}`);
 ///     this.add('-item', source, yourItem, '[from] move: Thief', `[of] ${target}`);
 /// }
+/// ```
 pub fn on_after_hit(
-    _battle: &mut Battle,
-    _source_pos: (usize, usize),
-    _target_pos: (usize, usize),
+    battle: &mut Battle,
+    source_pos: (usize, usize),
+    target_pos: (usize, usize),
 ) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    // if (source.item || source.volatiles['gem']) {
+    //     return;
+    // }
+    let source_has_item_or_gem = {
+        let source_pokemon = match battle.pokemon_at(source_pos.0, source_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        !source_pokemon.item.is_empty() || source_pokemon.volatiles.contains_key(&ID::from("gem"))
+    };
+
+    if source_has_item_or_gem {
+        // return;
+        return EventResult::Continue;
+    }
+
+    // const yourItem = target.takeItem(source);
+    let your_item = {
+        let target_pokemon = match battle.pokemon_at_mut(target_pos.0, target_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.take_item()
+    };
+
+    // if (!yourItem) {
+    //     return;
+    // }
+    let your_item_id = match your_item {
+        Some(id) => id,
+        None => {
+            // return;
+            return EventResult::Continue;
+        }
+    };
+
+    // if (!this.singleEvent('TakeItem', yourItem, target.itemState, source, target, move, yourItem) ||
+    //     !source.setItem(yourItem)) {
+    //     target.item = yourItem.id; // bypass setItem so we don't break choicelock or anything
+    //     return;
+    // }
+    // TODO: Implement singleEvent for TakeItem
+    // For now, we'll just try to set the item directly
+    let set_item_success = {
+        let source_pokemon = match battle.pokemon_at_mut(source_pos.0, source_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        source_pokemon.set_item(your_item_id.clone())
+    };
+
+    if !set_item_success {
+        // target.item = yourItem.id; // bypass setItem so we don't break choicelock or anything
+        let target_pokemon = match battle.pokemon_at_mut(target_pos.0, target_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        target_pokemon.item = your_item_id;
+        // return;
+        return EventResult::Continue;
+    }
+
+    // this.add('-enditem', target, yourItem, '[silent]', '[from] move: Thief', `[of] ${source}`);
+    // this.add('-item', source, yourItem, '[from] move: Thief', `[of] ${target}`);
+    let (source_ident, target_ident, item_name) = {
+        let source_pokemon = match battle.pokemon_at(source_pos.0, source_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        let target_pokemon = match battle.pokemon_at(target_pos.0, target_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        let item_data = battle.dex.get_item_by_id(&your_item_id);
+        let item_name = item_data
+            .map(|i| i.name.clone())
+            .unwrap_or_else(|| your_item_id.to_string());
+
+        (
+            source_pokemon.get_slot(),
+            target_pokemon.get_slot(),
+            item_name,
+        )
+    };
+
+    battle.add(
+        "-enditem",
+        &[
+            target_ident.as_str().into(),
+            item_name.clone().into(),
+            "[silent]".into(),
+            "[from] move: Thief".into(),
+            format!("[of] {}", source_ident).into(),
+        ],
+    );
+
+    battle.add(
+        "-item",
+        &[
+            source_ident.as_str().into(),
+            item_name.into(),
+            "[from] move: Thief".into(),
+            format!("[of] {}", target_ident).into(),
+        ],
+    );
+
     EventResult::Continue
 }
