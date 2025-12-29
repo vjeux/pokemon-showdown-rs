9dea31e3ab2c53cb1a92dcc41abf2eb941f202af
Developer <dev@example.com>
2025-12-29 10:20:31 +0100

    Implement G-Max Depletion move callback
    Added PP depletion logic for G-Max Depletion move that:
    - Deducts 2 PP from each foe's last used move
    - Skips Z-moves (doesn't deduct PP)
    - Handles Max moves by targeting their base move
    - Adds activation message when PP is deducted
    
    Infrastructure used:
    - pokemon.last_move field (pokemon.rs:199)
    - pokemon.deduct_pp() method (pokemon.rs:903)
    - move_data.is_z and is_max checks
    - move_data.base_move for Max move handling

--- src/data/move_callbacks/gmaxdepletion.rs
+++ src/data/move_callbacks/gmaxdepletion.rs
@@ -29,26 +29,97 @@ use crate::event::EventResult;
 /// 		}
 /// ```
 pub fn on_hit(
-    _battle: &mut Battle,
-    _source_pos: (usize, usize),
+    battle: &mut Battle,
+    source_pos: (usize, usize),
     _target_pos: Option<(usize, usize)>,
 ) -> EventResult {
+    use crate::dex_data::ID;
+
     // for (const pokemon of source.foes()) {
-    //     let move: Move | ActiveMove | null = pokemon.lastMove;
-    //     if (!move || move.isZ) continue;
-    //     if (move.isMax && move.baseMove) move = this.dex.moves.get(move.baseMove);
-    //
-    //     const ppDeducted = pokemon.deductPP(move.id, 2);
-    //     if (ppDeducted) {
-    //         this.add("-activate", pokemon, 'move: G-Max Depletion', move.name, ppDeducted);
-    //         // Don't return here because returning early doesn't trigger
-    //         // activation text for the second Pokemon in doubles
+    let all_active = battle.get_all_active(false);
+    let source_side = source_pos.0;
+
+    for pokemon_pos in all_active {
+        // Only process foes (pokemon on different side than source)
+        if pokemon_pos.0 == source_side {
+            continue;
+        }
+
+        //     let move: Move | ActiveMove | null = pokemon.lastMove;
+        //     if (!move || move.isZ) continue;
+        let move_id = {
+            let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+                Some(p) => p,
+                None => continue,
+            };
+
+            match &pokemon.last_move {
+                Some(move_id) => move_id.clone(),
+                None => continue,
+            }
+        };
+
+        // Get move data to check if it's a Z-move or Max move
+        let move_data = match battle.dex.get_move_by_id(&move_id) {
+            Some(m) => m,
+            None => continue,
+        };
+
+        // if (!move || move.isZ) continue;
+        if move_data.is_z.is_some() {
+            continue;
+        }
+
+        //     if (move.isMax && move.baseMove) move = this.dex.moves.get(move.baseMove);
+        let actual_move_id = if move_data.is_max.is_some() {
+            if let Some(ref base_move) = move_data.base_move {
+                base_move.clone()
+            } else {
+                move_id.clone()
+            }
+        } else {
+            move_id.clone()
+        };
+
+        //     const ppDeducted = pokemon.deductPP(move.id, 2);
+        let pp_deducted = {
+            let pokemon = match battle.pokemon_at_mut(pokemon_pos.0, pokemon_pos.1) {
+                Some(p) => p,
+                None => continue,
+            };
+            pokemon.deduct_pp(&actual_move_id, 2)
+        };
+
+        //     if (ppDeducted) {
+        //         this.add("-activate", pokemon, 'move: G-Max Depletion', move.name, ppDeducted);
+        if pp_deducted {
+            let move_name = battle.dex.get_move_by_id(&actual_move_id)
+                .map(|m| m.name.clone())
+                .unwrap_or_else(|| actual_move_id.to_string());
+
+            let pokemon_slot = {
+                let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+                    Some(p) => p,
+                    None => continue,
+                };
+                pokemon.get_slot()
+            };
+
+            battle.add(
+                "-activate",
+                &[
+                    crate::battle::Arg::from(pokemon_slot),
+                    crate::battle::Arg::from("move: G-Max Depletion"),
+                    crate::battle::Arg::from(move_name),
+                    crate::battle::Arg::from("2"),
+                ],
+            );
+        //         // Don't return here because returning early doesn't trigger
+        //         // activation text for the second Pokemon in doubles
+        }
     //     }
     // }
-
-    // TODO: Infrastructure needed - Pokemon::deduct_pp() method
-    // This move deducts 2 PP from each foe's last used move
-    // For now, returning Continue as the infrastructure doesn't exist
+    }
 
     EventResult::Continue
 }
