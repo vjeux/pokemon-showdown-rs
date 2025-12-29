6be2fb8bb716512071d31402552a23a1a3242314
Developer <dev@example.com>
2025-12-29 13:17:39 +0100

    Implement thickfat ability callbacks (weaken Ice and Fire moves)

--- src/data/ability_callbacks/thickfat.rs
+++ src/data/ability_callbacks/thickfat.rs
@@ -14,7 +14,12 @@ use crate::event::EventResult;
 ///     }
 /// }
 pub fn on_source_modify_atk(battle: &mut Battle, move_id: &str) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    if let Some(move_data) = battle.dex.get_move(move_id) {
+        if move_data.move_type == "Ice" || move_data.move_type == "Fire" {
+            let modified = battle.chain_modify(0.5);
+            return EventResult::Number(modified);
+        }
+    }
     EventResult::Continue
 }
 
@@ -25,7 +30,12 @@ pub fn on_source_modify_atk(battle: &mut Battle, move_id: &str) -> EventResult {
 ///     }
 /// }
 pub fn on_source_modify_sp_a(battle: &mut Battle, move_id: &str) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+    if let Some(move_data) = battle.dex.get_move(move_id) {
+        if move_data.move_type == "Ice" || move_data.move_type == "Fire" {
+            let modified = battle.chain_modify(0.5);
+            return EventResult::Number(modified);
+        }
+    }
     EventResult::Continue
 }
 
