f2e25470c04da9bed3b3032af5abfa6ddd69a591
Developer <dev@example.com>
2026-01-03 02:20:19 -0800

    Implement Klutz ability (Batch 116) - onStart calls singleEvent to end item effect

--- src/data/ability_callbacks/klutz.rs
+++ src/data/ability_callbacks/klutz.rs
@@ -5,13 +5,27 @@
 //! Generated from data/abilities.ts
 
 use crate::battle::Battle;
+use crate::dex_data::ID;
 use crate::event::EventResult;
 
 /// onStart(pokemon) {
 ///     this.singleEvent('End', pokemon.getItem(), pokemon.itemState, pokemon);
 /// }
-pub fn on_start(_battle: &mut Battle, _pokemon_pos: (usize, usize)) -> EventResult {
-    // TODO: Implement 1-to-1 from JS
+pub fn on_start(battle: &mut Battle, pokemon_pos: (usize, usize)) -> EventResult {
+    // this.singleEvent('End', pokemon.getItem(), pokemon.itemState, pokemon);
+    let item_id = {
+        let pokemon = match battle.pokemon_at(pokemon_pos.0, pokemon_pos.1) {
+            Some(p) => p,
+            None => return EventResult::Continue,
+        };
+        pokemon.item.clone()
+    };
+
+    // Only call singleEvent if pokemon has an item
+    if !item_id.is_empty() {
+        battle.single_event("End", &ID::from(item_id), Some(pokemon_pos), None, None);
+    }
+
     EventResult::Continue
 }
 
