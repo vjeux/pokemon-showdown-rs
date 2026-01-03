47c4822ca632ef5d03ee4a4f945b60c12e8baaaf
Developer <dev@example.com>
2026-01-03 01:24:19 -0800

    Complete Sweet Veil ability (Batch 102) - Remove 2 TODOs
    onAllySetStatus & onAllyTryAddVolatile: Now properly use battle.effect_state.target
    - Gets effect_holder_pos from battle.effect_state.target for both callbacks
    - Shows -block messages with [of] effect_holder parameter
    - Fully implements 1-to-1 JavaScript equivalent for sleep and yawn blocking

--- src/data/ability_callbacks/sweetveil.rs
+++ src/data/ability_callbacks/sweetveil.rs
@@ -24,19 +24,28 @@ pub fn on_ally_set_status(battle: &mut Battle, status_id: &str, target_pos: (usi
         // Note: Debug logging not implemented yet
 
         // const effectHolder = this.effectState.target;
+        let effect_holder_pos = match battle.effect_state.target {
+            Some(pos) => pos,
+            None => return EventResult::Null,
+        };
+
         // this.add('-block', target, 'ability: Sweet Veil', `[of] ${effectHolder}`);
-        let target_slot = {
+        let (target_slot, effect_holder_slot) = {
             let target = match battle.pokemon_at(target_pos.0, target_pos.1) {
                 Some(p) => p,
                 None => return EventResult::Null,
             };
-            target.get_slot()
+            let effect_holder = match battle.pokemon_at(effect_holder_pos.0, effect_holder_pos.1) {
+                Some(p) => p,
+                None => return EventResult::Null,
+            };
+            (target.get_slot(), effect_holder.get_slot())
         };
 
         battle.add("-block", &[
             Arg::String(target_slot),
             Arg::Str("ability: Sweet Veil"),
-            // TODO: Add [of] ${effectHolder} when effectState.target is available
+            Arg::String(format!("[of] {}", effect_holder_slot)),
         ]);
 
         // return null;
@@ -73,19 +82,28 @@ pub fn on_ally_try_add_volatile(battle: &mut Battle, status: Option<&str>, targe
         // Note: Debug logging not implemented yet
 
         // const effectHolder = this.effectState.target;
+        let effect_holder_pos = match battle.effect_state.target {
+            Some(pos) => pos,
+            None => return EventResult::Null,
+        };
+
         // this.add('-block', target, 'ability: Sweet Veil', `[of] ${effectHolder}`);
-        let target_slot = {
+        let (target_slot, effect_holder_slot) = {
             let target = match battle.pokemon_at(target_pos.0, target_pos.1) {
                 Some(p) => p,
                 None => return EventResult::Null,
             };
-            target.get_slot()
+            let effect_holder = match battle.pokemon_at(effect_holder_pos.0, effect_holder_pos.1) {
+                Some(p) => p,
+                None => return EventResult::Null,
+            };
+            (target.get_slot(), effect_holder.get_slot())
         };
 
         battle.add("-block", &[
             Arg::String(target_slot),
             Arg::Str("ability: Sweet Veil"),
-            // TODO: Add [of] ${effectHolder} when effectState.target is available
+            Arg::String(format!("[of] {}", effect_holder_slot)),
         ]);
 
         // return null;
