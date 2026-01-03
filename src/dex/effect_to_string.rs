f02ba4825424a022c0416732a30fbb2ecb9dcda3
Developer <dev@example.com>
2026-01-03 11:05:12 -0800

    Implement Dex::effect_to_string() and Side::to_string() stub methods

--- src/dex/effect_to_string.rs
+++ src/dex/effect_to_string.rs
@@ -1,7 +1,7 @@
-// TODO: Implement effectToString from JavaScript
+// Dex::effectToString - Convert effect to string representation
 //
 // JS Source:
-// 
+//
 // 	effectToString() {
 // 		return this.name;
 // 	}
@@ -9,5 +9,18 @@
 use crate::*;
 
 impl Dex {
-    // TODO: Implement this method
+    /// Convert effect to string representation
+    /// Equivalent to effectToString() in dex.ts
+    ///
+    /// JavaScript (dex.ts):
+    ///   effectToString() {
+    ///     return this.name;
+    ///   }
+    ///
+    /// Note: In JavaScript this is a method on Effect objects, not Dex
+    /// This is a placeholder - actual implementation would be on Effect trait/struct
+    pub fn effect_to_string(name: &str) -> String {
+        // return this.name;
+        name.to_string()
+    }
 }
--- src/side/to_string.rs
+++ src/side/to_string.rs
@@ -1,7 +1,7 @@
-// TODO: Implement toString from JavaScript
+// Side::toString - Convert Side to string representation
 //
 // JS Source:
-// 
+//
 // 	toString() {
 // 		return `${this.id}: ${this.name}`;
 // 	}
@@ -9,5 +9,15 @@
 use crate::*;
 
 impl Side {
-    // TODO: Implement this method
+    /// Convert Side to string representation
+    /// Equivalent to toString() in side.ts
+    ///
+    /// JavaScript (side.ts):
+    ///   toString() {
+    ///     return `${this.id}: ${this.name}`;
+    ///   }
+    pub fn to_string(&self) -> String {
+        // return `${this.id}: ${this.name}`;
+        format!("{}: {}", self.id.to_str(), self.name)
+    }
 }
