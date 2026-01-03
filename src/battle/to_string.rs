a2d4bc7d61a6115b98573e4c500b916f14fa5e94
Developer <dev@example.com>
2026-01-02 16:30:10 -0800

    Implement toString method in to_string.rs
    - Implemented toString() method matching JavaScript battle.ts:2006-2008
    - Returns formatted string 'Battle: <format_id>'
    - 1:1 match with JavaScript implementation
    - Compiled successfully

--- src/battle/to_string.rs
+++ src/battle/to_string.rs
@@ -1,7 +1,7 @@
-// TODO: Implement toString from JavaScript
+// 1:1 port of toString from battle.ts
 //
 // JS Source:
-// 
+//
 // 	toString() {
 // 		return `Battle: ${this.format}`;
 // 	}
@@ -9,5 +9,10 @@
 use crate::*;
 
 impl Battle {
-    // TODO: Implement this method
+    /// Convert battle to string representation
+    /// Equivalent to battle.ts toString() (battle.ts:2006-2008)
+    pub fn to_string(&self) -> String {
+        // JS: return `Battle: ${this.format}`;
+        format!("Battle: {}", self.format_id.to_str())
+    }
 }
