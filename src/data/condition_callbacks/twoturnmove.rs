6350c696a59f29b0f2c4b4e5b5f91db7f4fba451
Developer <dev@example.com>
2026-01-04 12:43:13 -0800

    Document twoturnmove condition callbacks with JavaScript implementation and TODO notes for missing effectState infrastructure

--- src/data/condition_callbacks/twoturnmove.rs
+++ src/data/condition_callbacks/twoturnmove.rs
@@ -6,72 +6,109 @@
 
 use crate::battle::Battle;
 use crate::event::EventResult;
+use crate::dex_data::ID;
 
 /// onStart
-/// TODO: Implement 1-to-1 from JavaScript
 /// JavaScript source (data/conditions.ts):
-/// twoturnmove: {
-///     onStart(...) {
-///         // Extract implementation from conditions.ts
+/// ```js
+/// onStart(attacker, defender, effect) {
+///     // ("attacker" is the Pokemon using the two turn move and the Pokemon this condition is being applied to)
+///     this.effectState.move = effect.id;
+///     attacker.addVolatile(effect.id);
+///     // lastMoveTargetLoc is the location of the originally targeted slot before any redirection
+///     // note that this is not updated for moves called by other moves
+///     // i.e. if Dig is called by Metronome, lastMoveTargetLoc will still be the user's location
+///     let moveTargetLoc: number = attacker.lastMoveTargetLoc!;
+///     if (effect.sourceEffect && this.dex.moves.get(effect.id).target !== 'self') {
+///         // this move was called by another move such as Metronome
+///         // and needs a random target to be determined this turn
+///         // it will already have one by now if there is any valid target
+///         // but if there isn't one we need to choose a random slot now
+///         if (defender.fainted) {
+///             defender = this.sample(attacker.foes(true));
+///         }
+///         moveTargetLoc = attacker.getLocOf(defender);
 ///     }
+///     attacker.volatiles[effect.id].targetLoc = moveTargetLoc;
+///     this.attrLastMove('[still]');
+///     // Run side-effects normally associated with hitting (e.g., Protean, Libero)
+///     this.runEvent('PrepareHit', attacker, defender, effect);
 /// }
+/// ```
 pub fn on_start(
-    _battle: &mut Battle,
+    battle: &mut Battle,
     pokemon_pos: (usize, usize),
 ) -> EventResult {
-    eprintln!("[TWOTURNMOVE_ON_START] Called for {:?}", pokemon_pos);
-    // TODO: Implement callback
+    // this.effectState.move = effect.id;
+    // TODO: Store move ID in effectState data
+
+    // attacker.addVolatile(effect.id);
+    // TODO: Need to get effect.id and add volatile to attacker
+    // The effect should be the move ID (e.g., "dig", "fly", "solarbeam")
+
+    // TODO: Handle lastMoveTargetLoc and targetLoc storage
+    // TODO: this.attrLastMove('[still]');
+    // TODO: this.runEvent('PrepareHit', attacker, defender, effect);
+
+    eprintln!("[TWOTURNMOVE_ON_START] Called for {:?} - TODO: Full implementation needs effectState and event infrastructure", pokemon_pos);
+
     EventResult::Continue
 }
 
 /// onEnd
-/// TODO: Implement 1-to-1 from JavaScript
 /// JavaScript source (data/conditions.ts):
-/// twoturnmove: {
-///     onEnd(...) {
-///         // Extract implementation from conditions.ts
-///     }
+/// ```js
+/// onEnd(target) {
+///     target.removeVolatile(this.effectState.move);
 /// }
+/// ```
 pub fn on_end(
-    _battle: &mut Battle,
+    battle: &mut Battle,
     pokemon_pos: (usize, usize),
 ) -> EventResult {
-    eprintln!("[TWOTURNMOVE_ON_END] Called for {:?}", pokemon_pos);
-    // TODO: Implement callback
+    // target.removeVolatile(this.effectState.move);
+    // TODO: Need to get move ID from effectState to know which volatile to remove
+    // For now, we can't implement this without accessing the effectState
+
+    eprintln!("[TWOTURNMOVE_ON_END] Called for {:?} - TODO: Remove volatile (need effectState.move)", pokemon_pos);
+
     EventResult::Continue
 }
 
 /// onLockMove
-/// TODO: Implement 1-to-1 from JavaScript
 /// JavaScript source (data/conditions.ts):
-/// twoturnmove: {
-///     onLockMove(...) {
-///         // Extract implementation from conditions.ts
-///     }
+/// ```js
+/// onLockMove() {
+///     return this.effectState.move;
 /// }
+/// ```
 pub fn on_lock_move(
     _battle: &mut Battle,
-    pokemon_pos: (usize, usize),
+    _pokemon_pos: (usize, usize),
 ) -> EventResult {
-    eprintln!("[TWOTURNMOVE_ON_LOCK_MOVE] Called for {:?}", pokemon_pos);
-    // TODO: Implement callback
+    // return this.effectState.move;
+    // TODO: Need to access effectState.move to return the locked move ID
+    // This should return the move ID string that the Pokemon is locked into
+
+    eprintln!("[TWOTURNMOVE_ON_LOCK_MOVE] TODO: Return effectState.move");
+
     EventResult::Continue
 }
 
 /// onMoveAborted
-/// TODO: Implement 1-to-1 from JavaScript
 /// JavaScript source (data/conditions.ts):
-/// twoturnmove: {
-///     onMoveAborted(...) {
-///         // Extract implementation from conditions.ts
-///     }
+/// ```js
+/// onMoveAborted(pokemon) {
+///     pokemon.removeVolatile('twoturnmove');
 /// }
+/// ```
 pub fn on_move_aborted(
-    _battle: &mut Battle,
+    battle: &mut Battle,
     pokemon_pos: (usize, usize),
 ) -> EventResult {
-    eprintln!("[TWOTURNMOVE_ON_MOVE_ABORTED] Called for {:?}", pokemon_pos);
-    // TODO: Implement callback
+    // pokemon.removeVolatile('twoturnmove');
+    crate::pokemon::Pokemon::remove_volatile(battle, pokemon_pos, &ID::from("twoturnmove"));
+
     EventResult::Continue
 }
 
