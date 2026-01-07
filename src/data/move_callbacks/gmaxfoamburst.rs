06ee733cefac6277db79bf9f36ce05ee315d3c92
Developer <dev@example.com>
2026-01-06 20:07:29 -0800

    Implement gmaxfoamburst self::on_hit callback 1-to-1 from JS
    Implemented the self-targeting onHit callback for G-Max Foam Burst move.
    This callback boosts spe:-2 on all foes.

--- src/data/move_callbacks/gmaxfoamburst.rs
+++ src/data/move_callbacks/gmaxfoamburst.rs
@@ -69,11 +69,31 @@ pub mod self_callbacks {
     /// }
     /// ```
     pub fn on_hit(
-        _battle: &mut Battle,
+        battle: &mut Battle,
         _target_pos: (usize, usize),
-        _source_pos: Option<(usize, usize)>,
+        source_pos: Option<(usize, usize)>,
     ) -> EventResult {
-        // TODO: Implement 1-to-1 from JS
+        // for (const pokemon of source.foes()) {
+        //     this.boost({ spe: -2 }, pokemon);
+        // }
+
+        let source = match source_pos {
+            Some(pos) => pos,
+            None => return EventResult::Continue,
+        };
+
+        let foe_positions = {
+            let source_pokemon = match battle.pokemon_at(source.0, source.1) {
+                Some(p) => p,
+                None => return EventResult::Continue,
+            };
+            source_pokemon.foes(battle, false)
+        };
+
+        for foe_pos in foe_positions {
+            battle.boost(&[("spe", -2)], foe_pos, Some(source), None, false, false);
+        }
+
         EventResult::Continue
     }
 }
