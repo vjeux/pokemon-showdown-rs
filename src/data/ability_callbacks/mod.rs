97cf9c675436c3ae1683b94e1029f11c332ab165
Developer <dev@example.com>
2026-01-06 16:43:57 -0800

    Verify dispatch_on_foe_try_eat_item signature matches JavaScript

--- src/data/ability_callbacks/mod.rs
+++ src/data/ability_callbacks/mod.rs
@@ -1091,10 +1091,6 @@ pub fn dispatch_on_foe_trap_pokemon(
         _ => EventResult::Continue,
     }
 }
-// TODO: verify that the list of calls in JavaScript matches the Rust equivalent
-// JavaScript signatures:
-//   onFoeTryEatItem()
-
 /// Dispatch onFoeTryEatItem callbacks
 pub fn dispatch_on_foe_try_eat_item(
     battle: &mut Battle,
